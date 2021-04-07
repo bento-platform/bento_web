6eff8e437cd8a720d8815dfc502dc70efb2b977f
David Lougheed <david.lougheed@gmail.com>
2021-04-07 11:50:21 -0400

    More debugging

--- src/index.js
+++ src/index.js
@@ -19,12 +19,17 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+
     // Handle auth popup callback
     if (window.location.href === `${process.env.CHORD_URL}${POPUP_AUTH_CALLBACK_URL}` && window.opener) {
+        render(<div>Loading...</div>, root);
+
         // We're inside a popup window which has (presumably) successfully
         // re-authenticated the user, meaning we need to close ourself to return
         // focus to the original window.
         window.opener.postMessage("done", (new URL(process.env.CHORD_URL)).origin);
+        console.log("tried to send message to ", (new URL(process.env.CHORD_URL)).origin);
         // TODO: Do we need to pass the Set-Cookie header here?
         // window.close();
     } else {
@@ -34,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     <App/>
                 </BrowserRouter>
             </Provider>,
-            document.getElementById("root")
+            root
         );
     }
 });
