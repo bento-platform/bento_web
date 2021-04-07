594ad2f37cd3492fec2439e38530800a3783758d
David Lougheed <david.lougheed@gmail.com>
2021-04-07 11:30:22 -0400

    Try to fix post message from auth popup

--- src/index.js
+++ src/index.js
@@ -18,22 +18,22 @@ import {POPUP_AUTH_CALLBACK_URL} from "./constants";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-// Handle auth popup callback
-if (window.location.href === `${process.env.CHORD_URL}${POPUP_AUTH_CALLBACK_URL}` && window.opener) {
-    // We're inside a popup window which has (presumably) successfully
-    // re-authenticated the user, meaning we need to close ourself to return
-    // focus to the original window.
-    window.opener.postMessage("done");  // TODO: Do we need to pass the Set-Cookie header here?
-    window.close();
-} else {
-    document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+    // Handle auth popup callback
+    if (window.location.href === `${process.env.CHORD_URL}${POPUP_AUTH_CALLBACK_URL}` && window.opener) {
+        // We're inside a popup window which has (presumably) successfully
+        // re-authenticated the user, meaning we need to close ourself to return
+        // focus to the original window.
+        window.opener.postMessage("done");  // TODO: Do we need to pass the Set-Cookie header here?
+        window.close();
+    } else {
         render(
             <Provider store={store}>
                 <BrowserRouter>
-                    <App />
+                    <App/>
                 </BrowserRouter>
             </Provider>,
             document.getElementById("root")
         );
-    });
-}
+    }
+});
