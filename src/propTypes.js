393d36d7fe36248d764e41d0aa9b8872c9cac11d
David Lougheed <david.lougheed@gmail.com>
2021-07-23 10:38:50 -0400

    some general proptypes work

--- src/propTypes.js
+++ src/propTypes.js
@@ -16,7 +16,7 @@ export const serviceInfoPropTypesShape = PropTypes.shape({
         group: PropTypes.string.isRequired,
         artifact: PropTypes.string.isRequired,
         version: PropTypes.string.isRequired,
-    }).required,
+    }).isRequired,
     description: PropTypes.string,
     organization: PropTypes.shape({
         name: PropTypes.string.isRequired,
@@ -220,12 +220,20 @@ export const biosamplePropTypesShape = PropTypes.shape({
 // Prop types object shape for a single patient individual object from the metadata service.
 export const individualPropTypesShape = PropTypes.shape({
     id: PropTypes.string.isRequired,
+    alternate_ids: PropTypes.arrayOf(PropTypes.string),
+
     date_of_birth: PropTypes.string,
+    age: PropTypes.object,  // TODO: Shape
     sex: PropTypes.oneOf(SEX_VALUES),
     karyotypic_sex: PropTypes.oneOf(KARYOTYPIC_SEX_VALUES),
+    ethnicity: PropTypes.string,
     taxonomy: ontologyShape,
+
     phenopackets: PropTypes.arrayOf(PropTypes.object),  // TODO
     biosamples: PropTypes.arrayOf(biosamplePropTypesShape),
+
+    created: PropTypes.string,  // ISO datetime string
+    updated: PropTypes.string,  // ISO datetime string
 });
 
 // Prop types object shape for a single phenopacket disease object.
@@ -275,18 +283,48 @@ export const experimentPropTypesShape = PropTypes.shape({
     extraction_protocol: PropTypes.string,
     reference_registry_id: PropTypes.string,
 
-    // TODO: FK / M2M Stuff
+    biosample: PropTypes.string,  // Biosample foreign key
+    instrument: PropTypes.shape({
+        identifier: PropTypes.string,
+        platform: PropTypes.string,
+        description: PropTypes.string,
+        model: PropTypes.string,
+    }),
+
+    experiment_results: PropTypes.arrayOf(PropTypes.shape({
+        identifier: PropTypes.string,
+        description: PropTypes.string,
+        filename: PropTypes.string,
+        file_format: PropTypes.oneOf([
+            "SAM",
+            "BAM",
+            "CRAM",
+            "BAI",
+            "CRAI",
+            "VCF",
+            "BCF",
+            "GVCF",
+            "BigWig",
+            "BigBed",
+            "FASTA",
+            "FASTQ",
+            "TAB",
+            "SRA",
+            "SRF",
+            "SFF",
+            "GFF",
+            "TABIX",
+            "UNKNOWN",
+            "OTHER",
+        ]),
+        data_output_type: PropTypes.oneOf(["Raw data", "Derived data"]),
+        usage: PropTypes.string,
+    })),
 
     qc_flags: PropTypes.arrayOf(PropTypes.string),
 
-    extra_properties: PropTypes.object,  // Key-value object
-    // subject: PropTypes.oneOfType([individualPropTypesShape, PropTypes.string]).isRequired,
-    // biosamples: biosamplePropTypesShape.isRequired,
-    // diseases: diseasePropTypesShape.isRequired,
-    // phenotypic_features: phenotypicFeaturePropTypesShape,
-    // meta_data: PropTypes.object.isRequired,  // TODO: Shape
-    // created: PropTypes.string,  // ISO datetime string
-    // updated: PropTypes.string,  // ISO datetime string
+    created: PropTypes.string,  // ISO datetime string
+    updated: PropTypes.string,  // ISO datetime string
 });
 
 export const overviewSummaryPropTypesShape = PropTypes.shape({
