5d4216dec73ca064e50f54faa390f1c6430b9554
Gordon Krieger <gordon.krieger@gmail.com>
2021-07-27 10:45:18 -0300

    fix indents (2 -> 4)

--- src/components/overview/Histogram.js
+++ src/components/overview/Histogram.js
@@ -1,68 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    Tooltip,
-    Label,
-} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Label } from "recharts";
 
 class Histogram extends React.Component {
-  static propTypes = {
-      title: PropTypes.string,
-      data: PropTypes.array,
-      fieldLabel: PropTypes.string,
-      chartHeight: PropTypes.number,
-      chartAspectRatio: PropTypes.number,
-      setAutoQueryPageTransition: PropTypes.func,
-  };
+    static propTypes = {
+        title: PropTypes.string,
+        data: PropTypes.array,
+        fieldLabel: PropTypes.string,
+        chartHeight: PropTypes.number,
+        chartAspectRatio: PropTypes.number,
+        setAutoQueryPageTransition: PropTypes.func,
+    };
 
-  state = {
-      canUpdate: false,
-      activeIndex: undefined,
-      itemSelected: undefined,
-      fieldLabel: undefined,
-  };
+    state = {
+        canUpdate: false,
+        activeIndex: undefined,
+        itemSelected: undefined,
+        fieldLabel: undefined,
+    };
 
-  titleStyle = {
-      fontStyle: "italic",
-      padding: "0",
-      margin: "0",
-  };
+    titleStyle = {
+        fontStyle: "italic",
+        padding: "0",
+        margin: "0",
+    };
 
-  shouldComponentUpdate(props, state) {
-      if (this.state !== state && state.canUpdate) return true;
+    shouldComponentUpdate(props, state) {
+        if (this.state !== state && state.canUpdate) return true;
 
-      return this.props.data !== props.data;
-  }
+        return this.props.data !== props.data;
+    }
 
-  render() {
-      const {title, data, chartHeight, chartAspectRatio} = this.props;
-      const titleHeaderHeight = 31;
+    render() {
+        const { title, data, chartHeight, chartAspectRatio } = this.props;
+        const titleHeaderHeight = 31;
 
-      return (
-      <div>
-        <h2 style={this.titleStyle}>{title}</h2>
-        <BarChart
-          width={(chartHeight- titleHeaderHeight) * chartAspectRatio}
-          height={chartHeight - titleHeaderHeight}
-          data={data}
-          margin={{ top: 50, right: 80, bottom: 30, left: 80 }}
-        >
-          <XAxis dataKey="ageBin" height={20}>
-            <Label value="Subject age (yrs)" offset={-20} position="insideBottom" />
-          </XAxis>
-          <YAxis >
-            <Label value="Count" offset={-10} position="left" angle={270} />
-          </YAxis>
-          <Tooltip />
-          <Bar dataKey="count" fill="#ff0000" isAnimationActive={false} />
-        </BarChart>
-      </div>
-      );
-  }
+        return (
+            <div>
+                <h2 style={this.titleStyle}>{title}</h2>
+                <BarChart
+                    width={(chartHeight - titleHeaderHeight) * chartAspectRatio}
+                    height={chartHeight - titleHeaderHeight}
+                    data={data}
+                    margin={{ top: 50, right: 80, bottom: 30, left: 80 }}
+                >
+                    <XAxis dataKey="ageBin" height={20}>
+                        <Label value="Subject age (yrs)" offset={-20} position="insideBottom" />
+                    </XAxis>
+                    <YAxis>
+                        <Label value="Count" offset={-10} position="left" angle={270} />
+                    </YAxis>
+                    <Tooltip />
+                    <Bar dataKey="count" fill="#ff0000" isAnimationActive={false} />
+                </BarChart>
+            </div>
+        );
+    }
 }
 
 export default Histogram;
