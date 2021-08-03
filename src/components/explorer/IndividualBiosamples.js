98f40e5abdda6c2ab58dea02b5f3a2ab0015975c
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-08-03 15:10:13 -0400

    remodeled IndividualBiosamples for downloading vcf

--- src/components/explorer/IndividualBiosamples.js
+++ src/components/explorer/IndividualBiosamples.js
@@ -1,87 +1,121 @@
-import React from "react";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 
 import {Table} from "antd";
+import {withRouter} from "react-router-dom";
 
 import {EM_DASH} from "../../constants";
 import {renderOntologyTerm} from "./ontologies";
 import {individualPropTypesShape} from "../../propTypes";
 
+import {performDownloadFromDrsIfPossible} from "../../modules/drs/actions"
+
+import PropTypes from "prop-types";
+
 // TODO: Only show biosamples from the relevant dataset, if specified;
 //  highlight those found in search results, if specified
 
-const BIOSAMPLE_COLUMNS = [
-    {
-        title: "ID",
-        key: "id",
-        render: (_, individual) => individual.id,
-        sorter: (a, b) => a.id.localeCompare(b.id),
-        defaultSortOrder: "ascend",
-    },
-    {
-        title: "Sampled Tissue",
-        key: "sampled_tissue",
-        render: (_, individual) => renderOntologyTerm(individual.sampled_tissue),
-    },
-    {
-        title: "Procedure",
-        key: "procedure",
-        render: (_, individual) => <>
-            <strong>Code:</strong> {renderOntologyTerm(individual.procedure.code)}<br />
-            {individual.procedure.body_site
-                ? <><br /><strong>Body Site:</strong> {renderOntologyTerm(individual.procedure.body_site)}</>
-                : null}
-        </>,
-    },
-    {
-        title: "Ind. Age at Collection",
-        key: "individual_age_at_collection",
-        render: (_, individual) => {
-            const age = individual.individual_age_at_collection;
-            return age
-                ? (age.hasOwnProperty("age") ? age.age : `Between ${age.start.age} and ${age.end.age}`)
-                : EM_DASH;
+class IndividualBiosamples extends Component {
+    BIOSAMPLE_COLUMNS = [
+        {
+            title: "ID",
+            key: "id",
+            render: (_, individual) => individual.id,
+            sorter: (a, b) => a.id.localeCompare(b.id),
+            defaultSortOrder: "ascend",
         },
-    },
-    {
-        title: "Extra Properties",
-        key: "extra_properties",
-        render: (_, individual) =>
-            (individual.hasOwnProperty("extra_properties") && Object.keys(individual.extra_properties).length)
-                ?  <div><pre>{JSON.stringify(individual.extra_properties, null, 2)}</pre></div>
-                : EM_DASH,
-    },
-    {
-        title: "Download",
-        key: "extra_properties",
-        render: (_, individual) =>
-            (individual.hasOwnProperty("experiments") && Object.keys(individual.experiments).length)
-                ? (individual.experiments).flatMap(exp => 
-                    (exp.hasOwnProperty("experiment_results") && Object.keys(exp.experiment_results).length) 
-                        ? (exp.experiment_results).flatMap(result => 
-                            (result.hasOwnProperty("filename") && Object.keys(result.filename).length) ? result.filename : EM_DASH)
-                        .filter(isKindOfVcf)
-                        .flatMap(fn => <div><a>{fn}</a></div>)
-                    : EM_DASH)
-                : EM_DASH,
+        {
+            title: "Sampled Tissue",
+            key: "sampled_tissue",
+            render: (_, individual) => renderOntologyTerm(individual.sampled_tissue),
+        },
+        {
+            title: "Procedure",
+            key: "procedure",
+            render: (_, individual) => <>
+                <strong>Code:</strong> {renderOntologyTerm(individual.procedure.code)}<br />
+                {individual.procedure.body_site
+                    ? <><br /><strong>Body Site:</strong> {renderOntologyTerm(individual.procedure.body_site)}</>
+                    : null}
+            </>,
+        },
+        {
+            title: "Ind. Age at Collection",
+            key: "individual_age_at_collection",
+            render: (_, individual) => {
+                const age = individual.individual_age_at_collection;
+                return age
+                    ? (age.hasOwnProperty("age") ? age.age : `Between ${age.start.age} and ${age.end.age}`)
+                    : EM_DASH;
+            },
+        },
+        {
+            title: "Extra Properties",
+            key: "extra_properties",
+            render: (_, individual) =>
+                (individual.hasOwnProperty("extra_properties") && Object.keys(individual.extra_properties).length)
+                    ?  <div><pre>{JSON.stringify(individual.extra_properties, null, 2)}</pre></div>
+                    : EM_DASH,
+        },
+        {
+            title: "Download",
+            key: "extra_properties",
+            render: (_, individual) =>
+                (individual.hasOwnProperty("experiments") && Object.keys(individual.experiments).length)
+                    ? (individual.experiments).flatMap(exp => 
+                        (exp.hasOwnProperty("experiment_results") && Object.keys(exp.experiment_results).length) 
+                            ? (exp.experiment_results).flatMap(result => 
+                                (result.hasOwnProperty("filename") && Object.keys(result.filename).length) ? result.filename : EM_DASH)
+                            .filter(isKindOfVcf)
+                            .flatMap(name => <div><a onClick={async () => this.props.performDownloadFromDrsIfPossible(name)} >{name}</a></div>)
+                        : EM_DASH)
+                    : EM_DASH,
+        }
+    
+    ];
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+        };
+
+        // Ensure user is at the top of the page after transition
+        window.scrollTo(0, 0);
     }
 
-];
 
-const IndividualBiosamples = ({individual}) =>
-    <Table bordered
-           size="middle"
-           pagination={{pageSize: 25}}
-           columns={BIOSAMPLE_COLUMNS}
-           rowKey="id"
-           dataSource={(individual?.phenopackets ?? []).flatMap(p => p.biosamples)} />;
+    render() {
+        return <Table bordered
+            size="middle"
+            pagination={{pageSize: 25}}
+            columns={this.BIOSAMPLE_COLUMNS}
+            rowKey="id"
+            dataSource={(this.props.individual?.phenopackets ?? []).flatMap(p => p.biosamples)} />
+    }
+}
 
 
 function isKindOfVcf(filename) {
     return filename.toLowerCase().includes(".vcf");
-  }
+}
+
 
 IndividualBiosamples.propTypes = {
     individual: individualPropTypesShape,
+    performDownloadFromDrsIfPossible:  PropTypes.func.isRequired,
 };
 
-export default IndividualBiosamples;
+const mapStateToProps = (state, ownProps) => {
+    return {
+
+    };
+};
+
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    performDownloadFromDrsIfPossible: filename => dispatch(performDownloadFromDrsIfPossible(filename)),
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(IndividualBiosamples));
+
---
+++ src/modules/drs/reducers.js
@@ -0,0 +1,58 @@
+import {objectWithoutProp} from "../../utils/misc";
+import FileSaver from "file-saver";
+
+import {
+    PERFORM_SEARCH_BY_FUZZYNAME,
+    PERFORM_OBJECT_DOWNLOAD,
+} from "./actions";
+
+
+
+export const drs = (
+    state = {
+        isFuzzySearching: false,
+        fuzzySearchResponse: {},
+        isFetchingObjectForDownload: false
+    },
+    action
+) => {
+    switch (action.type) {
+        case PERFORM_SEARCH_BY_FUZZYNAME.REQUEST:
+            return {
+                ...state,
+                isFuzzySearching: true
+            };
+        case PERFORM_SEARCH_BY_FUZZYNAME.RECEIVE:
+            return {
+                ...state,
+                isFuzzySearching: false,
+                fuzzySearchResponse: action.data
+            };
+        case PERFORM_SEARCH_BY_FUZZYNAME.FINISH:
+            return {
+                ...state,
+                isFuzzySearching: false
+            };
+
+        case PERFORM_OBJECT_DOWNLOAD.REQUEST:
+            return {
+                ...state,
+                isFetchingObjectForDownload: true
+            };
+        case PERFORM_OBJECT_DOWNLOAD.RECEIVE:
+            FileSaver.saveAs(action.data, "data.vcf.gz"); //new Blob([data], {type: "application/octet-stream"})
+
+            return {
+                ...state,
+                isFetchingObjectForDownload: false,
+            };
+        case PERFORM_OBJECT_DOWNLOAD.FINISH:
+            return {
+                ...state,
+                isFetchingObjectForDownload: false,
+            };
+
+        default:
+            return state;
+    }
+};
