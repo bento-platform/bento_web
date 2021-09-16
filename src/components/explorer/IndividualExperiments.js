30914b0b8e834ada21e5295eed26dcbe1b8cd4ce
Gordon Krieger <gordon.krieger@gmail.com>
2021-09-16 16:45:38 -0400

    add Experiments tab, styling

---
+++ src/components/explorer/IndividualExperiments.js
@@ -0,0 +1,182 @@
+import React, { Component } from "react";
+import { Descriptions, Popover, Table, Typography } from "antd";
+import { EM_DASH } from "../../constants";
+import { individualPropTypesShape } from "../../propTypes";
+import { performDownloadFromDrsIfPossible } from "../../modules/drs/actions";
+import JsonView from "./JsonView";
+
+class IndividualExperiments extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const biosamplesData = (this.props.individual?.phenopackets ?? []).flatMap((p) => p.biosamples);
+        const experimentsData = biosamplesData.flatMap((b) => b?.experiments ?? []);
+
+        console.log({ experimentsData: experimentsData });
+
+        const EXPERIMENT_RESULTS_COLUMNS = [
+            {
+                title: "Result File",
+                key: "result_file",
+                render: (_, result) => result.file_format,
+            },
+            {
+                title: "Creation Date",
+                key: "creation_date",
+                render: (_, result) => result.creation_date,
+            },
+            {
+                title: "Description",
+                key: "description",
+                render: (_, result) => result.description,
+            },
+            {
+                title: "Filename",
+                key: "filename",
+                render: (_, result) => result.filename,
+            },
+            {
+                title: "Download",
+                key: "download",
+                render: (_, result) =>
+                    isDownloadable(result) ? (
+            <div>
+              <a onClick={async () => performDownloadFromDrsIfPossible(result.filename)}>download</a>
+            </div>
+                    ) : (
+                        EM_DASH
+                    ),
+            },
+            {
+                title: "Other Details",
+                key: "other_details",
+                render: (_, result) => (
+          <Popover placement="leftTop" title={"content title"} content={"content content"} trigger="click">
+            Other Details
+          </Popover>
+                ),
+            },
+        ];
+
+        return (
+            <>
+        {experimentsData.map((e) => (
+          <div key={e.id}>
+            <Typography.Title level={4}>
+              {`${e.experiment_type} (Biosample ${e.biosample})`}{" "}
+            </Typography.Title>
+            <div style={{ display: "flex" }}>
+              <div>
+                <Descriptions
+                  layout="vertical"
+                  bordered={true}
+                  colon={false}
+                  column={1}
+                  size="small"
+                  key={e.id}
+                >
+                  <Descriptions.Item>
+                    {e.molecule_ontology.map((mo) => (
+                      <Descriptions
+                        title="Molecule Ontology"
+                        layout="horizontal"
+                        bordered={true}
+                        column={1}
+                        size="small"
+                        key={mo.id}
+                      >
+                        <Descriptions.Item label="id">{mo.id}</Descriptions.Item>
+                        <Descriptions.Item label="label">{mo.label}</Descriptions.Item>
+                      </Descriptions>
+                    ))}
+                  </Descriptions.Item>
+
+                  <Descriptions.Item>
+                    {e.experiment_ontology.map((eo) => (
+                      <Descriptions
+                        title="Experiment Ontology"
+                        layout="horizontal"
+                        bordered={true}
+                        column={1}
+                        size="small"
+                        key={eo.id}
+                      >
+                        <Descriptions.Item label="id">{eo.id}</Descriptions.Item>
+                        <Descriptions.Item label="label">{eo.label}</Descriptions.Item>
+                      </Descriptions>
+                    ))}
+                  </Descriptions.Item>
+                  <Descriptions.Item>
+                    <Descriptions
+                      title="Instrument"
+                      layout="horizontal"
+                      bordered={true}
+                      column={1}
+                      size="small"
+                    >
+                      <Descriptions.Item label="id">{e.instrument.id}</Descriptions.Item>
+                      <Descriptions.Item label="identifier">{e.instrument.identifier}</Descriptions.Item>
+                      <Descriptions.Item label="platform">{e.instrument.platform}</Descriptions.Item>
+                      <Descriptions.Item label="created">{e.instrument.created}</Descriptions.Item>
+                      <Descriptions.Item label="updated">{e.instrument.updated}</Descriptions.Item>
+                    </Descriptions>
+                  </Descriptions.Item>
+                </Descriptions>
+              </div>
+              <Descriptions layout="vertical" bordered={true} column={1} size="small" key={e.id}>
+                <Descriptions.Item>
+                  <Descriptions layout="horizontal" bordered={true} column={1} size="small">
+                    <Descriptions.Item label="Experiment Type">{e.experiment_type}</Descriptions.Item>
+                    <Descriptions.Item label="Study Type">{e.study_type}</Descriptions.Item>
+                    <Descriptions.Item label="Extraction Protocol">{e.extraction_protocol}</Descriptions.Item>
+                    <Descriptions.Item label="Library Layout">{e.library_layout}</Descriptions.Item>
+                    <Descriptions.Item label="Library Selection">{e.library_selection}</Descriptions.Item>
+                    <Descriptions.Item label="Library Source">{e.library_source}</Descriptions.Item>
+                    <Descriptions.Item label="Library Strategy">{e.library_strategy}</Descriptions.Item>
+                  </Descriptions>
+                </Descriptions.Item>
+
+                <Descriptions.Item>
+                  <Descriptions
+                    title="Extra Properties"
+                    layout="horizontal"
+                    bordered={true}
+                    column={1}
+                    size="small"
+                  >
+                    <Descriptions.Item>
+                      <JsonView inputJson={e.extra_properties} />
+                    </Descriptions.Item>
+                  </Descriptions>
+                </Descriptions.Item>
+              </Descriptions>
+            </div>
+            <Typography.Title level={4}>Results </Typography.Title>
+            <Table
+              bordered
+              size="small"
+              pagination={{ pageSize: 25 }}
+              columns={EXPERIMENT_RESULTS_COLUMNS}
+              rowKey="filename"
+              dataSource={e.experiment_results}
+            />
+          </div>
+        ))}
+            </>
+        );
+    }
+}
+
+// expand here accordingly
+function isDownloadable(result) {
+    return result.file_format.toLowerCase() === "vcf" || result.filename.toLowerCase().includes(".vcf");
+}
+
+IndividualExperiments.propTypes = {
+    individual: individualPropTypesShape,
+};
+
+export default IndividualExperiments;
