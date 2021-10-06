5c0da5cae9baef71d62014e29e0318eb550333a0
Gordon Krieger <gordon.krieger@gmail.com>
2021-10-06 16:37:07 -0400

    util function for igv file types

---
+++ src/utils/guessFileType.js
@@ -0,0 +1,15 @@
+// file type guesses for igv files, for cases where this information is missing
+export const guessFileType = (filename) => {
+    if (filename.toLowerCase().endsWith(".vcf.gz")) {
+        return ("vcf");
+    }
+    if (filename.toLowerCase().endsWith(".cram")) {
+        return ("cram");
+    }
+    if (filename.toLowerCase().endsWith(".bw") || filename.toLowerCase().endsWith(".bigwig")) {
+        return "bigWig";
+    }
+
+    // expand here accordingly
+    return null;
+};
