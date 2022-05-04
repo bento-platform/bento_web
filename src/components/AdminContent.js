124c086288ee83d7336131cc1699698901a28c7b
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-04 11:40:18 -0400

    Refactored AdminContent.js

--- src/components/AdminContent.js
+++ src/components/AdminContent.js
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import {Redirect, Switch} from "react-router-dom";
-import {BASE_PATH, withBasePath} from "../utils/url";
+import { Redirect, Switch } from "react-router-dom";
+import { BASE_PATH, withBasePath } from "../utils/url";
 
-import {Layout} from "antd";
+import { Layout } from "antd";
 
 import OwnerRoute from "./OwnerRoute";
 import SitePageHeader from "./SitePageHeader";
@@ -13,33 +13,52 @@ import DataManagerContent from "./DataManagerContent";
 import PeersContent from "./PeersContent";
 import ServiceDetail from "./services/ServiceDetail";
 
-import {SITE_NAME} from "../constants";
-import {nodeInfoDataPropTypesShape, projectPropTypesShape} from "../propTypes";
+import { SITE_NAME } from "../constants";
+import {
+    nodeInfoDataPropTypesShape,
+    projectPropTypesShape,
+} from "../propTypes";
 
-
-class AdminContent extends Component {
-    componentDidMount() {
+const AdminContent = () => {
+    useEffect(() => {
         document.title = `${SITE_NAME} - Admin`;
-    }
+    });
 
-    render() {
-        return <>
+    return (
+        <>
             <SitePageHeader title="Admin" subTitle="Administrative tools" />
             <Layout>
-                <Layout.Content style={{background: "white", padding: "32px 24px 4px"}}>
+                <Layout.Content
+                    style={{ background: "white", padding: "32px 24px 4px" }}
+                >
                     {/* <Typography.Title level={3}>Admin</Typography.Title> */}
                     <Switch>
-                        <OwnerRoute path={withBasePath("admin/services")} component={ServiceContent} />
-                        <OwnerRoute path={withBasePath("admin/services/:artifact")} component={ServiceDetail} />
-                        <OwnerRoute path={withBasePath("admin/data/manager")} component={DataManagerContent} />
-                        <OwnerRoute path={withBasePath("admin/peers")} component={PeersContent} />
-                        <Redirect from={BASE_PATH} to={withBasePath("admin/services")} />
+                        <OwnerRoute
+                            path={withBasePath("admin/services")}
+                            component={ServiceContent}
+                        />
+                        <OwnerRoute
+                            path={withBasePath("admin/services/:artifact")}
+                            component={ServiceDetail}
+                        />
+                        <OwnerRoute
+                            path={withBasePath("admin/data/manager")}
+                            component={DataManagerContent}
+                        />
+                        <OwnerRoute
+                            path={withBasePath("admin/peers")}
+                            component={PeersContent}
+                        />
+                        <Redirect
+                            from={BASE_PATH}
+                            to={withBasePath("admin/services")}
+                        />
                     </Switch>
                 </Layout.Content>
             </Layout>
-        </>;
-    }
-}
+        </>
+    );
+};
 
 AdminContent.propTypes = {
     nodeInfo: nodeInfoDataPropTypesShape,
@@ -52,12 +71,13 @@ AdminContent.propTypes = {
     isFetchingPeers: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     nodeInfo: state.nodeInfo.data,
     isFetchingNodeInfo: state.nodeInfo.isFetching,
 
     projects: state.projects.items,
-    isFetchingProjects: state.auth.isFetchingDependentData || state.projects.isFetching,
+    isFetchingProjects:
+        state.auth.isFetchingDependentData || state.projects.isFetching,
 
     peers: state.peers.items,
     isFetchingPeers: state.auth.isFetchingDependentData,
