2fa562fa2d4dea2cf547aa3cf7474dc9dbf0d1de
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-05 15:12:11 -0400

    refactored NotificationList

--- src/components/notifications/NotificationList.js
+++ src/components/notifications/NotificationList.js
@@ -1,75 +1,94 @@
-import React, {Component} from "react";
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
-import {withRouter} from "react-router-dom";
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 
-import {Button, List} from "antd";
+import { Button, List } from "antd";
 
-import {markNotificationAsRead} from "../../modules/notifications/actions";
+import { markNotificationAsRead } from "../../modules/notifications/actions";
 
-import {NOTIFICATION_WES_RUN_COMPLETED, NOTIFICATION_WES_RUN_FAILED, navigateToWESRun} from "../../utils/notifications";
-import {notificationPropTypesShape} from "../../propTypes";
-
+import {
+    NOTIFICATION_WES_RUN_COMPLETED,
+    NOTIFICATION_WES_RUN_FAILED,
+    navigateToWESRun,
+} from "../../utils/notifications";
+import { notificationPropTypesShape } from "../../propTypes";
 
 const sortNotificationTimestamps = (a, b) => b.timestamp - a.timestamp;
 
-
-class NotificationList extends Component {
-    constructor(props) {
-        super(props);
-        this.getNotificationActions = this.getNotificationActions.bind(this);
-    }
-
-    getNotificationActions(notification) {
+const NotificationList = ({
+    notifications,
+    small,
+    fetchingNotifications,
+    markNotificationAsRead,
+    navigateToWESRun,
+}) => {
+    const getNotificationActions = (notification) => {
         switch (notification.notification_type) {
             case NOTIFICATION_WES_RUN_COMPLETED:
             case NOTIFICATION_WES_RUN_FAILED:
                 return [
-                    <Button key="run-details" onClick={() => this.props.navigateToWESRun(notification.action_target)}>
+                    <Button
+                        key="run-details"
+                        onClick={() =>
+                            navigateToWESRun(notification.action_target)
+                        }
+                    >
                         Run Details
-                    </Button>
+                    </Button>,
                 ];
             default:
                 return [];
         }
-    }
+    };
 
-    render() {
-        const processedNotifications = this.props.notifications.map(n => ({
+    const processedNotifications = notifications
+        .map((n) => ({
             ...n,
-            timestamp: new Date(Date.parse(n.timestamp))
-        })).sort(sortNotificationTimestamps);
+            timestamp: new Date(Date.parse(n.timestamp)),
+        }))
+        .sort(sortNotificationTimestamps);
 
-        const small = this.props.small || false;
+    small = small || false;
 
-        return (
-            <List itemLayout="vertical"
-                  dataSource={processedNotifications}
-                  pagination={{
-                      hideOnSinglePage: small,
-                      pageSize: small ? 5 : 10,
-                      size: small ? "small" : ""
-                  }}
-                  loading={this.props.fetchingNotifications}
-                  renderItem={n => (
-                      <List.Item key={n.id} actions={[
-                          ...this.getNotificationActions(n),
-                          <Button key="mark-as-read"
-                                  type="link"
-                                  icon="read"
-                                  style={{padding: 0}}
-                                  onClick={() => this.props.markNotificationAsRead(n.id)}>
-                              Mark as Read
-                          </Button>
-                      ]}>
-                          <List.Item.Meta title={n.title} description={n.description} style={{marginBottom: "8px"}} />
-                          {n.timestamp.toLocaleString()}
-                      </List.Item>
-                  )} />
-        );
-    }
-}
+    return (
+        <List
+            itemLayout="vertical"
+            dataSource={processedNotifications}
+            pagination={{
+                hideOnSinglePage: small,
+                pageSize: small ? 5 : 10,
+                size: small ? "small" : "",
+            }}
+            loading={fetchingNotifications}
+            renderItem={(n) => (
+                <List.Item
+                    key={n.id}
+                    actions={[
+                        ...getNotificationActions(n),
+                        <Button
+                            key="mark-as-read"
+                            type="link"
+                            icon="read"
+                            style={{ padding: 0 }}
+                            onClick={() => markNotificationAsRead(n.id)}
+                        >
+                            Mark as Read
+                        </Button>,
+                    ]}
+                >
+                    <List.Item.Meta
+                        title={n.title}
+                        description={n.description}
+                        style={{ marginBottom: "8px" }}
+                    />
+                    {n.timestamp.toLocaleString()}
+                </List.Item>
+            )}
+        />
+    );
+};
 
 NotificationList.propTypes = {
     notifications: PropTypes.arrayOf(notificationPropTypesShape),
@@ -81,14 +100,17 @@ NotificationList.propTypes = {
     navigateToWESRun: PropTypes.func,
 };
 
-
-const mapStateToProps = state => ({
-    fetchingNotifications: state.services.isFetchingAll || state.notifications.isFetching,
+const mapStateToProps = (state) => ({
+    fetchingNotifications:
+        state.services.isFetchingAll || state.notifications.isFetching,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    ...bindActionCreators({markNotificationAsRead}, dispatch),
-    navigateToWESRun: target => navigateToWESRun(target, dispatch, ownProps.history),
+    ...bindActionCreators({ markNotificationAsRead }, dispatch),
+    navigateToWESRun: (target) =>
+        navigateToWESRun(target, dispatch, ownProps.history),
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NotificationList));
+export default withRouter(
+    connect(mapStateToProps, mapDispatchToProps)(NotificationList)
+);
