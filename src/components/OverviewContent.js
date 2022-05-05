3014989494b4800e8dc922ebc050ce2f0d72810a
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-05 12:28:56 -0400

    refactored OverviewContent to func based

--- src/components/OverviewContent.js
+++ src/components/OverviewContent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Layout, Divider } from "antd";
 
 import SitePageHeader from "./SitePageHeader";
@@ -7,40 +7,39 @@ import VariantsSummary from "./overview/VariantsSummary";
 import { SITE_NAME } from "../constants";
 import ExperimentsSummary from "./overview/ExperimentsSummary";
 
-class OverviewContent extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
+const OverviewContent = () => {
+    useEffect(() => {
         document.title = `${SITE_NAME} - Overview`;
-    }
+    });
 
-    render() {
-        return (
-            <>
-        <div
-          style={{
-              display: "flex",
-              justifyContent: "space-between",
-              background: "white",
-              borderBottom: "1px solid rgb(232, 232, 232)",
-          }}
-        >
-          <SitePageHeader title="Overview" style={{ border: "none" }} />
-        </div>
-        <Layout>
-          <Layout.Content style={{ background: "white", padding: "32px 24px 4px" }}>
-            <ClinicalSummary  />
-            <Divider />
-            <ExperimentsSummary />
-            <Divider />
-            <VariantsSummary />
-          </Layout.Content>
-        </Layout>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    background: "white",
+                    borderBottom: "1px solid rgb(232, 232, 232)",
+                }}
+            >
+                <SitePageHeader title="Overview" style={{ border: "none" }} />
+            </div>
+            <Layout>
+                <Layout.Content
+                    style={{
+                        background: "white",
+                        padding: "32px 24px 4px",
+                    }}
+                >
+                    <ClinicalSummary />
+                    <Divider />
+                    <ExperimentsSummary />
+                    <Divider />
+                    <VariantsSummary />
+                </Layout.Content>
+            </Layout>
+        </>
+    );
+};
 
 export default OverviewContent;
