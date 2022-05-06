7e51ecda93cb53fa3dd5c58bd92252031ff3f51d
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-06 12:21:59 -0400

    refactored DataUseDisplay

--- src/components/DataUseDisplay.js
+++ src/components/DataUseDisplay.js
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React from "react";
 
-import {Col, List, Icon, Row, Tag, Typography, Popover} from "antd";
+import { Col, List, Icon, Row, Tag, Typography, Popover } from "antd";
 
 import {
     DUO_NOT_FOR_PROFIT_USE_ONLY,
@@ -8,85 +8,126 @@ import {
     DATA_USE_INFO,
     DATA_USE_PROP_TYPE_SHAPE,
     PRIMARY_CONSENT_CODE_INFO,
-    SECONDARY_CONSENT_CODE_INFO
+    SECONDARY_CONSENT_CODE_INFO,
 } from "../duo";
-
 
 const TAG_LABEL_STYLING = {
     fontSize: "0.65rem",
     color: "#777",
     marginTop: "-4px",
-    marginBottom: "4px"
+    marginBottom: "4px",
 };
 
 const TAG_STYLING = {
-    fontFamily: "monospace"
+    fontFamily: "monospace",
 };
 
+const DataUseDisplay = ({ dataUse }) => {
+    const primaryCode = dataUse.consent_code.primary_category.code;
+    const uses = dataUse.data_use_requirements.map((u) => u.code) || [];
 
-class DataUseDisplay extends Component {
-    render() {
-        const primaryCode = this.props.dataUse.consent_code.primary_category.code;
-        const uses = this.props.dataUse.data_use_requirements.map(u => u.code) || [];
-
-        return <>
+    return (
+        <>
             <div>
-                <Typography.Title level={4} style={{fontSize: "20px"}}>
+                <Typography.Title level={4} style={{ fontSize: "20px" }}>
                     Consent Code
                 </Typography.Title>
                 <Row gutter={10} type="flex">
                     <Col>
                         <div style={TAG_LABEL_STYLING}>Primary</div>
-                        <Popover {...PRIMARY_CONSENT_CODE_INFO[primaryCode]}
-                                 overlayStyle={{maxWidth: "576px"}}>
+                        <Popover
+                            {...PRIMARY_CONSENT_CODE_INFO[primaryCode]}
+                            overlayStyle={{ maxWidth: "576px" }}
+                        >
                             <Tag color="blue" style={TAG_STYLING}>
-                                {this.props.dataUse.consent_code.primary_category.code}
+                                {dataUse.consent_code.primary_category.code}
                             </Tag>
                         </Popover>
                     </Col>
                     <Col>
                         <div style={TAG_LABEL_STYLING}>Secondary</div>
-                        {this.props.dataUse.consent_code.secondary_categories.length > 0
-                            ? this.props.dataUse.consent_code.secondary_categories.map(sc =>
-                                <Popover {...SECONDARY_CONSENT_CODE_INFO[sc.code]}
-                                         key={sc.code}
-                                         overlayStyle={{maxWidth: "576px"}}>
-                                    <Tag style={TAG_STYLING}>{sc.code}</Tag>
-                                </Popover>)
-                            : <Tag style={{...TAG_STYLING, background: "white", borderStyle: "dashed"}}>N/A</Tag>}
+                        {dataUse.consent_code.secondary_categories.length >
+                        0 ? (
+                                dataUse.consent_code.secondary_categories.map(
+                                    (sc) => (
+                                    <Popover
+                                        {...SECONDARY_CONSENT_CODE_INFO[
+                                            sc.code
+                                        ]}
+                                        key={sc.code}
+                                        overlayStyle={{ maxWidth: "576px" }}
+                                    >
+                                        <Tag style={TAG_STYLING}>{sc.code}</Tag>
+                                    </Popover>
+                                    )
+                                )
+                            ) : (
+                            <Tag
+                                style={{
+                                    ...TAG_STYLING,
+                                    background: "white",
+                                    borderStyle: "dashed",
+                                }}
+                            >
+                                N/A
+                            </Tag>
+                            )}
                     </Col>
                 </Row>
             </div>
-            <div style={{marginTop: "20px"}}>
+            <div style={{ marginTop: "20px" }}>
                 <Typography.Title level={4}>
                     Restrictions and Conditions
                 </Typography.Title>
                 {/* TODO: Empty display when no restrictions present */}
-                <List itemLayout="horizontal" style={{maxWidth: "600px"}}
-                      dataSource={DATA_USE_KEYS.filter(u => uses.includes(u))}
-                      renderItem={u => (
-                          <List.Item>
-                              <List.Item.Meta avatar={
-                                  u === DUO_NOT_FOR_PROFIT_USE_ONLY ? (
-                                      // Special case for non-profit use; stack two icons (dollar + stop) to
-                                      // create a custom synthetic icon.
-                                      <div style={{opacity: 0.65}}>
-                                          <Icon style={{fontSize: "24px", color: "black"}}
-                                                type={DATA_USE_INFO[u].icon} />
-                                          <Icon style={{fontSize: "24px", marginLeft: "-24px", color: "black"}}
-                                                type="stop" />
-                                      </div>
-                                  ) : <Icon style={{fontSize: "24px"}} type={DATA_USE_INFO[u].icon} />
-                              } title={DATA_USE_INFO[u].title} description={DATA_USE_INFO[u].content} />
-                          </List.Item>
-                      )} />
+                <List
+                    itemLayout="horizontal"
+                    style={{ maxWidth: "600px" }}
+                    dataSource={DATA_USE_KEYS.filter((u) => uses.includes(u))}
+                    renderItem={(u) => (
+                        <List.Item>
+                            <List.Item.Meta
+                                avatar={
+                                    u === DUO_NOT_FOR_PROFIT_USE_ONLY ? (
+                                        // Special case for non-profit use; stack two icons (dollar + stop) to
+                                        // create a custom synthetic icon.
+                                        <div style={{ opacity: 0.65 }}>
+                                            <Icon
+                                                style={{
+                                                    fontSize: "24px",
+                                                    color: "black",
+                                                }}
+                                                type={DATA_USE_INFO[u].icon}
+                                            />
+                                            <Icon
+                                                style={{
+                                                    fontSize: "24px",
+                                                    marginLeft: "-24px",
+                                                    color: "black",
+                                                }}
+                                                type="stop"
+                                            />
+                                        </div>
+                                    ) : (
+                                        <Icon
+                                            style={{ fontSize: "24px" }}
+                                            type={DATA_USE_INFO[u].icon}
+                                        />
+                                    )
+                                }
+                                title={DATA_USE_INFO[u].title}
+                                description={DATA_USE_INFO[u].content}
+                            />
+                        </List.Item>
+                    )}
+                />
             </div>
-        </>;
-    }
-}
+        </>
+    );
+};
 
 DataUseDisplay.propTypes = {
-    dataUse: DATA_USE_PROP_TYPE_SHAPE
+    dataUse: DATA_USE_PROP_TYPE_SHAPE,
 };
 
 export default DataUseDisplay;
