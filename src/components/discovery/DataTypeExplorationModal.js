99ca51d45d25b57ddeb0ee3416946173609b1090
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-06 13:59:12 -0400

    refactored DatatypeExplorationModal

--- src/components/discovery/DataTypeExplorationModal.js
+++ src/components/discovery/DataTypeExplorationModal.js
@@ -1,87 +1,122 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import {Icon, Input, Modal, Radio, Table, Tabs} from "antd";
+import { Icon, Input, Modal, Radio, Table, Tabs } from "antd";
 
 import SchemaTree from "../schema_trees/SchemaTree";
-import {generateSchemaTreeData, generateSchemaTableData} from "../../utils/schema";
-import {nop} from "../../utils/misc";
+import {
+    generateSchemaTreeData,
+    generateSchemaTableData,
+} from "../../utils/schema";
+import { nop } from "../../utils/misc";
 
 // TODO: Add more columns
 const FIELD_COLUMNS = [
-    {title: "Key", dataIndex: "key", render: t =>
-            <span style={{fontFamily: "monospace", fontSize: "12px", whiteSpace: "nowrap"}}>{t}</span>},
-    {title: "JSON Type", dataIndex: "data.type"},
-    {title: "Description", dataIndex: "data.description"},
+    {
+        title: "Key",
+        dataIndex: "key",
+        render: (t) => (
+            <span
+                style={{
+                    fontFamily: "monospace",
+                    fontSize: "12px",
+                    whiteSpace: "nowrap",
+                }}
+            >
+                {t}
+            </span>
+        ),
+    },
+    { title: "JSON Type", dataIndex: "data.type" },
+    { title: "Description", dataIndex: "data.description" },
 ];
 
-class DataTypeExplorationModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            view: "table",
-            filter: ""
-        };
+const DataTypeExplorationModal = ({ dataTypes, visible, onCancel }) => {
+    const [view, setView] = useState("table");
+    const [filter, setFilter] = useState("");
 
-        this.onFilterChange = this.onFilterChange.bind(this);
-        this.applyFilterToTableData = this.applyFilterToTableData.bind(this);
-        this.getTableData = this.getTableData.bind(this);
-    }
+    const onFilterChange = (v) => {
+        setFilter(v.toLocaleLowerCase().trim());
+    };
 
-    onFilterChange(v) {
-        this.setState({filter: v.toLocaleLowerCase().trim()});
-    }
-
-    applyFilterToTableData(l) {
-        return this.state.filter === ""
+    const applyFilterToTableData = (l) => {
+        return filter === ""
             ? l
-            : l.filter(f =>
-                f.key.toLocaleLowerCase().includes(this.state.filter)
-                || (f.data.description || "").toLocaleLowerCase().includes(this.state.filter));
-    }
+            : l.filter(
+                (f) =>
+                    f.key.toLocaleLowerCase().includes(filter) ||
+                      (f.data.description || "")
+                          .toLocaleLowerCase()
+                          .includes(filter)
+            );
+    };
 
-    getTableData(d) {
+    const getTableData = (d) => {
         // TODO: Cache tree data for data type
-        return this.applyFilterToTableData(generateSchemaTableData(generateSchemaTreeData(d.schema)));
-    }
+        return applyFilterToTableData(
+            generateSchemaTableData(generateSchemaTreeData(d.schema))
+        );
+    };
 
-    render() {
-        return <Modal title="Data Types"
-                      visible={this.props.visible}
-                      width={1280}
-                      onCancel={this.props.onCancel || nop}
-                      footer={null}>
-            <Radio.Group value={this.state.view}
-                         onChange={e => this.setState({view: e.target.value})}
-                         buttonStyle="solid"
-                         style={{position: "absolute", top: "73px", right: "24px", zIndex: "50"}}>
-                <Radio.Button value="tree"><Icon type="share-alt" /> Tree View</Radio.Button>
-                <Radio.Button value="table"><Icon type="table" /> Table Detail View</Radio.Button>
+    return (
+        <Modal
+            title="Data Types"
+            visible={visible}
+            width={1280}
+            onCancel={onCancel || nop}
+            footer={null}
+        >
+            <Radio.Group
+                value={view}
+                onChange={(e) => setView(e.target.value)}
+                buttonStyle="solid"
+                style={{
+                    position: "absolute",
+                    top: "73px",
+                    right: "24px",
+                    zIndex: "50",
+                }}
+            >
+                <Radio.Button value="tree">
+                    <Icon type="share-alt" /> Tree View
+                </Radio.Button>
+                <Radio.Button value="table">
+                    <Icon type="table" /> Table Detail View
+                </Radio.Button>
             </Radio.Group>
             <Tabs>
-                {Object.values(this.props.dataTypes).flatMap(ds => (ds.items || []).map(d => (
-                    <Tabs.TabPane tab={d.id} key={d.id}>
-                        {this.state.view === "tree" ? (
-                            <SchemaTree schema={d.schema} />
-                        ) : (
-                            <>
-                                <Input.Search allowClear={true}
-                                              onChange={e => this.onFilterChange(e.target.value)}
-                                              placeholder="Search for a field..." style={{marginBottom: "16px"}} />
-                                <Table bordered={true}
-                                       columns={FIELD_COLUMNS}
-                                       dataSource={this.getTableData(d)} />
-                            </>
-                        )}
-                    </Tabs.TabPane>
-                )))}
+                {Object.values(dataTypes).flatMap((ds) =>
+                    (ds.items || []).map((d) => (
+                        <Tabs.TabPane tab={d.id} key={d.id}>
+                            {view === "tree" ? (
+                                <SchemaTree schema={d.schema} />
+                            ) : (
+                                <>
+                                    <Input.Search
+                                        allowClear={true}
+                                        onChange={(e) =>
+                                            onFilterChange(e.target.value)
+                                        }
+                                        placeholder="Search for a field..."
+                                        style={{ marginBottom: "16px" }}
+                                    />
+                                    <Table
+                                        bordered={true}
+                                        columns={FIELD_COLUMNS}
+                                        dataSource={getTableData(d)}
+                                    />
+                                </>
+                            )}
+                        </Tabs.TabPane>
+                    ))
+                )}
             </Tabs>
-        </Modal>;
-    }
-}
+        </Modal>
+    );
+};
 
 DataTypeExplorationModal.propTypes = {
-    dataTypes: PropTypes.object,  // TODO: Shape
+    dataTypes: PropTypes.object, // TODO: Shape
     visible: PropTypes.bool,
     onCancel: PropTypes.func,
 };
