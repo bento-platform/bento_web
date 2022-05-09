7c7ae1244118044105a2b43dfaeb363a851a571c
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-09 14:34:06 -0400

    refatored SitePageHeader.js

--- src/components/SitePageHeader.js
+++ src/components/SitePageHeader.js
@@ -1,44 +1,42 @@
-import React, {Component} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-import {PageHeader} from "antd";
+import { PageHeader } from "antd";
 
 const PAGE_HEADER_STYLE = {
     borderBottom: "1px solid rgb(232, 232, 232)",
     background: "white",
-    padding: "12px 24px"
+    padding: "12px 24px",
 };
 
 const PAGE_HEADER_TITLE_STYLE = {
     fontSize: "1rem",
     lineHeight: "22px",
-    margin: "5px 0"
+    margin: "5px 0",
 };
 
 const PAGE_HEADER_SUBTITLE_STYLE = {
-    lineHeight: "23px"
+    lineHeight: "23px",
 };
 
-const TAB_BAR_HEADER_STYLING = {borderBottom: "none", paddingBottom: "0"};
+const TAB_BAR_HEADER_STYLING = { borderBottom: "none", paddingBottom: "0" };
 
-class SitePageHeader extends Component {
-    render() {
-        return (
-          <PageHeader
-            {...this.props}
-            title={<div style={PAGE_HEADER_TITLE_STYLE}>{this.props.title || ""}</div>}
-            subTitle={
-              this.props.subTitle ? <span style={PAGE_HEADER_SUBTITLE_STYLE}>{this.props.subTitle}</span> : undefined
-            }
-            style={{
-                ...PAGE_HEADER_STYLE,
-                ...(this.props.withTabBar ? TAB_BAR_HEADER_STYLING : {}),
-                ...(this.props.style || {}),
-            }}
-          />
-        );
-    }
-}
+const SitePageHeader = ({ title, subTitle, withTabBar, style }) => (
+    <PageHeader
+        {...{ title, subTitle, withTabBar, style }}
+        title={<div style={PAGE_HEADER_TITLE_STYLE}>{title || ""}</div>}
+        subTitle={
+            subTitle ? (
+                <span style={PAGE_HEADER_SUBTITLE_STYLE}>{subTitle}</span>
+            ) : undefined
+        }
+        style={{
+            ...PAGE_HEADER_STYLE,
+            ...(withTabBar ? TAB_BAR_HEADER_STYLING : {}),
+            ...(style || {}),
+        }}
+    />
+);
 
 SitePageHeader.propTypes = {
     title: PropTypes.string,
