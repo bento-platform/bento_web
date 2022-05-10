21b79e42d3af26ec008717d731f92e3332d5f334
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-10 10:44:51 -0400

    refactored DatasetForm.js

--- src/components/datasets/DatasetForm.js
+++ src/components/datasets/DatasetForm.js
@@ -1,51 +1,69 @@
-import React, {Component} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-import {Form, Input} from "antd";
+import { Form, Input } from "antd";
 
 import DataUseInput from "../DataUseInput";
 
-import {DATA_USE_PROP_TYPE_SHAPE, INITIAL_DATA_USE_VALUE} from "../../duo";
-import {simpleDeepCopy} from "../../utils/misc";
+import { DATA_USE_PROP_TYPE_SHAPE, INITIAL_DATA_USE_VALUE } from "../../duo";
+import { simpleDeepCopy } from "../../utils/misc";
 
-
-class DatasetForm extends Component {
-    render() {
-        return <Form style={this.props.style || {}}>
+const DatasetForm = ({ style, initialValue, form }) => {
+    return (
+        <Form style={style || {}}>
             <Form.Item label="Title">
-                {this.props.form.getFieldDecorator("title", {
-                    initialValue: (this.props.initialValue || {title: ""}).title || "",
-                    rules: [{required: true}, {min: 3}]
+                {form.getFieldDecorator("title", {
+                    initialValue: (initialValue || { title: "" }).title || "",
+                    rules: [{ required: true }, { min: 3 }],
                 })(<Input placeholder="My Dataset" size="large" />)}
             </Form.Item>
             <Form.Item label="Description">
-                {this.props.form.getFieldDecorator("description", {
-                    initialValue: (this.props.initialValue || {description: ""}).description || "",
-                    rules: [{required: true}]
+                {form.getFieldDecorator("description", {
+                    initialValue:
+                        (initialValue || { description: "" }).description || "",
+                    rules: [{ required: true }],
                 })(<Input.TextArea placeholder="This is a dataset" />)}
             </Form.Item>
             <Form.Item label="Contact Information">
-                {this.props.form.getFieldDecorator("contact_info", {
-                    initialValue: (this.props.initialValue || {contact_info: ""}).contact_info || "",
-                })(<Input.TextArea placeholder={"David Lougheed\ndavid.lougheed@mail.mcgill.ca"} />)}
+                {form.getFieldDecorator("contact_info", {
+                    initialValue:
+                        (initialValue || { contact_info: "" }).contact_info ||
+                        "",
+                })(
+                    <Input.TextArea
+                        placeholder={
+                            "David Lougheed\ndavid.lougheed@mail.mcgill.ca"
+                        }
+                    />
+                )}
             </Form.Item>
             <Form.Item label="Consent Code and Data Use Requirements">
-                {this.props.form.getFieldDecorator("data_use", {
-                    initialValue: ((this.props.initialValue ||
-                        {data_use: simpleDeepCopy(INITIAL_DATA_USE_VALUE)}).data_use ||
-                        simpleDeepCopy(INITIAL_DATA_USE_VALUE)),
-                    rules: [{required: true}, (rule, value, callback) => {
-                        if (!(value.consent_code || {}).primary_category) {
-                            callback(["Please specify one primary consent code"]);
-                            return;
-                        }
-                        callback([]);
-                    }]
+                {form.getFieldDecorator("data_use", {
+                    initialValue:
+                        (
+                            initialValue || {
+                                data_use: simpleDeepCopy(
+                                    INITIAL_DATA_USE_VALUE
+                                ),
+                            }
+                        ).data_use || simpleDeepCopy(INITIAL_DATA_USE_VALUE),
+                    rules: [
+                        { required: true },
+                        (rule, value, callback) => {
+                            if (!(value.consent_code || {}).primary_category) {
+                                callback([
+                                    "Please specify one primary consent code",
+                                ]);
+                                return;
+                            }
+                            callback([]);
+                        },
+                    ],
                 })(<DataUseInput />)}
             </Form.Item>
-        </Form>;
-    }
-}
+        </Form>
+    );
+};
 
 DatasetForm.propTypes = {
     style: PropTypes.object,
@@ -53,8 +71,8 @@ DatasetForm.propTypes = {
         title: PropTypes.string,
         description: PropTypes.string,
         contact_info: PropTypes.string,
-        data_use: DATA_USE_PROP_TYPE_SHAPE,  // TODO: Shared shape for data use
-    })
+        data_use: DATA_USE_PROP_TYPE_SHAPE, // TODO: Shared shape for data use
+    }),
 };
 
-export default Form.create({name: "dataset_form"})(DatasetForm);
+export default Form.create({ name: "dataset_form" })(DatasetForm);
