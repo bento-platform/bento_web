56a634145045dc678511a2e751e62754fe34ac24
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-10 13:37:40 -0400

    refactored Tableform.js

--- src/components/datasets/table/TableForm.js
+++ src/components/datasets/table/TableForm.js
@@ -1,40 +1,45 @@
-import React, {Component} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-import {Form, Input, Select} from "antd";
-
+import { Form, Input, Select } from "antd";
 
 // TODO: Load available data types from store
 
-class TableForm extends Component {
-    render() {
-        const dataTypeOptions = this.props.dataTypes.map(dts =>
-            <Select.Option key={`${dts.a}:${dts.dt.id}`}>{dts.dt.id}</Select.Option>);
+const TableForm = ({ form, dataTypes, initialValue, style }) => {
+    const dataTypeOptions = dataTypes.map((dts) => (
+        <Select.Option key={`${dts.a}:${dts.dt.id}`}>{dts.dt.id}</Select.Option>
+    ));
 
-        return <Form style={this.props.style || {}}>
+    return (
+        <Form style={style || {}}>
             <Form.Item label="Name">
-                {this.props.form.getFieldDecorator("name", {
-                    initialValue: (this.props.initialValue || {name: ""}).name || "",
-                    rules: [{required: true}, {min: 3}]
+                {form.getFieldDecorator("name", {
+                    initialValue: (initialValue || { name: "" }).name || "",
+                    rules: [{ required: true }, { min: 3 }],
                 })(<Input placeholder="My Variant Dataset" size="large" />)}
             </Form.Item>
             <Form.Item label="Data Type">
-                {this.props.form.getFieldDecorator("dataType", {
-                    initialValue: (this.props.initialValue || {dataType: null}).dataType || null,
-                    rules: [{required: true}]
-                })(<Select style={{width: "100%"}}>{dataTypeOptions}</Select>)}
+                {form.getFieldDecorator("dataType", {
+                    initialValue:
+                        (initialValue || { dataType: null }).dataType || null,
+                    rules: [{ required: true }],
+                })(
+                    <Select style={{ width: "100%" }}>{dataTypeOptions}</Select>
+                )}
             </Form.Item>
-        </Form>;
-    }
-}
+        </Form>
+    );
+};
 
 TableForm.propTypes = {
-    dataTypes: PropTypes.arrayOf(PropTypes.shape({
-        dt: PropTypes.object,  // TODO: Shape
-        a: PropTypes.string,
-    })),
+    dataTypes: PropTypes.arrayOf(
+        PropTypes.shape({
+            dt: PropTypes.object, // TODO: Shape
+            a: PropTypes.string,
+        })
+    ),
     initialValue: PropTypes.shape({
         name: PropTypes.string,
         dataType: PropTypes.string,
@@ -42,11 +47,19 @@ TableForm.propTypes = {
     style: PropTypes.object,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     dataTypes: Object.entries(state.serviceDataTypes.dataTypesByServiceArtifact)
-        .filter(([a, _]) => (state.chordServices.itemsByArtifact[a] || {manageable_tables: false}).manageable_tables)
-        .flatMap(([a, dts]) => (dts.items || []).map(dt => ({dt, a})))
+        .filter(
+            ([a, _]) =>
+                (
+                    state.chordServices.itemsByArtifact[a] || {
+                        manageable_tables: false,
+                    }
+                ).manageable_tables
+        )
+        .flatMap(([a, dts]) => (dts.items || []).map((dt) => ({ dt, a }))),
 });
 
-export default connect(mapStateToProps, null, null, {forwardRef: true})(
-    Form.create({name: "table_form"})(TableForm));
+export default connect(mapStateToProps, null, null, { forwardRef: true })(
+    Form.create({ name: "table_form" })(TableForm)
+);
