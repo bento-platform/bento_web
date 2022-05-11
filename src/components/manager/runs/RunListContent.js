e13e03b303f20fe48aa0c84122d205ec9d9d5c86
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-11 15:04:17 -0400

    refactored RunListContent.js

--- src/components/manager/runs/RunListContent.js
+++ src/components/manager/runs/RunListContent.js
@@ -1,50 +1,58 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import {Table, Typography} from "antd";
+import { Table, Typography } from "antd";
 
+import { fetchAllRunDetailsIfNeeded } from "../../../modules/wes/actions";
 
-import {fetchAllRunDetailsIfNeeded} from "../../../modules/wes/actions";
+import { RUN_REFRESH_TIMEOUT, RUN_TABLE_COLUMNS } from "./utils";
 
-import {RUN_REFRESH_TIMEOUT, RUN_TABLE_COLUMNS} from "./utils";
+const RunListContent = ({
+    runs,
+    servicesFetching,
+    runsFetching,
+    fetchAllRunDetailsIfNeeded,
+}) => {
+    let runRefreshTimeout = null;
 
+    const refreshRuns = async () => {
+        await fetchAllRunDetailsIfNeeded();
+        runRefreshTimeout = setTimeout(
+            () => refreshRuns(),
+            RUN_REFRESH_TIMEOUT
+        );
+    };
 
-class RunListContent extends Component {
-    constructor(props) {
-        super(props);
-        this.runRefreshTimeout = null;
-        this.refreshRuns = this.refreshRuns.bind(this);
-    }
+    useEffect(() => {
+        runRefreshTimeout = setTimeout(
+            () => refreshRuns(),
+            RUN_REFRESH_TIMEOUT
+        );
 
-    componentDidMount() {
-        this.runRefreshTimeout = setTimeout(() => this.refreshRuns(), RUN_REFRESH_TIMEOUT);
-    }
-
-    componentWillUnmount() {
-        if (this.runRefreshTimeout) clearTimeout(this.runRefreshTimeout);
-    }
-
-    async refreshRuns() {
-        await this.props.fetchAllRunDetailsIfNeeded();
-        this.runRefreshTimeout = setTimeout(() => this.refreshRuns(), RUN_REFRESH_TIMEOUT);
-    }
+        return () => {
+            if (runRefreshTimeout) clearTimeout(runRefreshTimeout);
+        };
+    });
 
     // TODO: Loading for individual rows
-    render() {
-        return <>
+
+    return (
+        <>
             <Typography.Title level={2}>Workflow Runs</Typography.Title>
-            <Table bordered={true}
-                   columns={RUN_TABLE_COLUMNS}
-                   dataSource={this.props.runs}
-                   loading={this.props.servicesFetching || this.props.runsFetching}
-                   rowKey="run_id" />
-        </>;
-    }
-}
+            <Table
+                bordered={true}
+                columns={RUN_TABLE_COLUMNS}
+                dataSource={runs}
+                loading={servicesFetching || runsFetching}
+                rowKey="run_id"
+            />
+        </>
+    );
+};
 
 RunListContent.propTypes = {
-    runs: PropTypes.arrayOf(PropTypes.object),  // TODO: shape, incorporating additional props included below
+    runs: PropTypes.arrayOf(PropTypes.object), // TODO: shape, incorporating additional props included below
 
     servicesFetching: PropTypes.bool,
     runsFetching: PropTypes.bool,
@@ -52,15 +60,20 @@ RunListContent.propTypes = {
     fetchAllRunDetailsIfNeeded: PropTypes.func,
 };
 
-const mapStateToProps = state => ({
-    runs: state.runs.items.map(r => {
-        const runDetails = (state.runs.itemsByID[r.run_id] || {details: null}).details;
+const mapStateToProps = (state) => ({
+    runs: state.runs.items.map((r) => {
+        const runDetails = (state.runs.itemsByID[r.run_id] || { details: null })
+            .details;
         return {
             ...r,
-            purpose: "Ingestion",  // TODO: Not hard-coded, Ingestion or Analysis
-            name: (runDetails || {run_log: {name: ""}}).run_log.name || "",
-            start_time: (runDetails || {run_log: {start_time: ""}}).run_log.start_time || "",
-            end_time: (runDetails || {run_log: {end_time: ""}}).run_log.end_time || ""
+            purpose: "Ingestion", // TODO: Not hard-coded, Ingestion or Analysis
+            name: (runDetails || { run_log: { name: "" } }).run_log.name || "",
+            start_time:
+                (runDetails || { run_log: { start_time: "" } }).run_log
+                    .start_time || "",
+            end_time:
+                (runDetails || { run_log: { end_time: "" } }).run_log
+                    .end_time || "",
         };
     }),
     servicesFetching: state.services.isFetchingAll,
