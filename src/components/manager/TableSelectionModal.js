6ca78c1fd916ad656e62c859905e7d5da3fb41dd
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-11 10:30:45 -0400

    refactored TableSelectionModal.js

--- src/components/manager/TableSelectionModal.js
+++ src/components/manager/TableSelectionModal.js
@@ -1,34 +1,35 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import {Form, Modal} from "antd";
+import { Form, Modal } from "antd";
 
 import TableTreeSelect from "./TableTreeSelect";
 
-import {nop} from "../../utils/misc";
+import { nop } from "../../utils/misc";
 
-class TableSelectionModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {selected: undefined};
-    }
+const TableSelectionModal = ({ dataType, title, visible, onCancel, onOk }) => {
+    const [selected, setSelected] = useState(undefined);
 
-    render() {
-        return <Modal title={this.props.title || "Select a Table"}
-                      visible={this.props.visible || false}
-                      onCancel={() => (this.props.onCancel || nop)()}
-                      onOk={() => (this.props.onOk || nop)(this.state.selected)}>
+    return (
+        <Modal
+            title={title || "Select a Table"}
+            visible={visible || false}
+            onCancel={() => (onCancel || nop)()}
+            onOk={() => (onOk || nop)(selected)}
+        >
             <Form>
                 <Form.Item label="Table">
-                    <TableTreeSelect style={{width: "100%"}}
-                                     value={this.state.selected}
-                                     dataType={this.props.dataType || null}
-                                     onChange={table => this.setState({selected: table})} />
+                    <TableTreeSelect
+                        style={{ width: "100%" }}
+                        value={selected}
+                        dataType={dataType || null}
+                        onChange={(table) => setSelected(table)}
+                    />
                 </Form.Item>
             </Form>
-        </Modal>;
-    }
-}
+        </Modal>
+    );
+};
 
 TableSelectionModal.propTypes = {
     dataType: PropTypes.string,
@@ -39,3 +40,45 @@ TableSelectionModal.propTypes = {
 };
 
 export default TableSelectionModal;
+
+// import React, {Component} from "react";
+// import PropTypes from "prop-types";
+
+// import {Form, Modal} from "antd";
+
+// import TableTreeSelect from "./TableTreeSelect";
+
+// import {nop} from "../../utils/misc";
+
+// class TableSelectionModal extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {selected: undefined};
+//     }
+
+//     render() {
+//         return <Modal title={this.props.title || "Select a Table"}
+//                       visible={this.props.visible || false}
+//                       onCancel={() => (this.props.onCancel || nop)()}
+//                       onOk={() => (this.props.onOk || nop)(this.state.selected)}>
+//             <Form>
+//                 <Form.Item label="Table">
+//                     <TableTreeSelect style={{width: "100%"}}
+//                                      value={this.state.selected}
+//                                      dataType={this.props.dataType || null}
+//                                      onChange={table => this.setState({selected: table})} />
+//                 </Form.Item>
+//             </Form>
+//         </Modal>;
+//     }
+// }
+
+// TableSelectionModal.propTypes = {
+//     dataType: PropTypes.string,
+//     title: PropTypes.string,
+//     visible: PropTypes.bool,
+//     onCancel: PropTypes.func,
+//     onOk: PropTypes.func,
+// };
+
+// export default TableSelectionModal;
