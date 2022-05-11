2724297c2cd90dac19bb942a4f8201706a05603e
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-11 10:13:43 -0400

    refactored ManagerAccessContent.js

--- src/components/manager/ManagerAccessContent.js
+++ src/components/manager/ManagerAccessContent.js
@@ -1,17 +1,17 @@
-import React, {Component} from "react";
+import React from "react";
 
-import {Layout, Typography} from "antd";
+import { Layout, Typography } from "antd";
 
-import {LAYOUT_CONTENT_STYLE} from "../../styles/layoutContent";
+import { LAYOUT_CONTENT_STYLE } from "../../styles/layoutContent";
 
-class ManagerAccessContent extends Component {
-    render() {
-        return <Layout>
+const ManagerAccessContent = () => {
+    return (
+        <Layout>
             <Layout.Content style={LAYOUT_CONTENT_STYLE}>
                 <Typography.Title level={2}>Access Management</Typography.Title>
             </Layout.Content>
-        </Layout>;
-    }
-}
+        </Layout>
+    );
+};
 
 export default ManagerAccessContent;
