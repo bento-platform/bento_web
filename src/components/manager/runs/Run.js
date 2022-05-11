b7070178ac3e6dbbdc3105c9e0243f2a35aa7046
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-11 15:21:34 -0400

    refactored run.js

--- src/components/manager/runs/Run.js
+++ src/components/manager/runs/Run.js
@@ -1,52 +1,73 @@
-import React, {Component} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-import {PageHeader, Row, Statistic, Tabs, Tag} from "antd";
+import { PageHeader, Row, Statistic, Tabs, Tag } from "antd";
 
 import RunRequest from "./RunRequest";
 import RunLog from "./RunLog";
 import RunTaskLogs from "./RunTaskLogs";
 
-import {renderDate, RUN_STATE_TAG_COLORS} from "./utils";
-import {nop} from "../../../utils/misc";
-import {runPropTypesShape} from "../../../propTypes";
-
+import { renderDate, RUN_STATE_TAG_COLORS } from "./utils";
+import { nop } from "../../../utils/misc";
+import { runPropTypesShape } from "../../../propTypes";
 
 const TABS = {
-    "request": RunRequest,
-    "run_log": RunLog,
-    "task_logs": RunTaskLogs
+    request: RunRequest,
+    run_log: RunLog,
+    task_logs: RunTaskLogs,
 };
 
-
-class Run extends Component {
-    render() {
-        const run = this.props.run ?? {};
-        const tab = this.props.tab ?? "request";
-        const Content = TABS[tab];
-        return <>
-            <PageHeader title={<>Run <span style={{fontFamily: "monospace"}}>{run.run_id}</span></>}
-                        tags={<Tag color={RUN_STATE_TAG_COLORS[run.state]}>{run.state}</Tag>}
-                        footer={
-                            <Tabs activeKey={tab} onChange={this.props.onChangeTab || nop}>
-                                <Tabs.TabPane tab="Request" key="request" />
-                                <Tabs.TabPane tab="Run Log" key="run_log" />
-                                {/*<Tabs.TabPane tab="Task Logs" key="task_logs" /> TODO: Implement v0.2 */}
-                            </Tabs>
-                        }
-                        onBack={this.props.onBack || nop}>
+const Run = ({ tab, run, onBack, onChangeTab }) => {
+    run = run ?? {};
+    tab = tab ?? "request";
+    const Content = TABS[tab];
+    return (
+        <>
+            <PageHeader
+                title={
+                    <>
+                        Run{" "}
+                        <span style={{ fontFamily: "monospace" }}>
+                            {run.run_id}
+                        </span>
+                    </>
+                }
+                tags={
+                    <Tag color={RUN_STATE_TAG_COLORS[run.state]}>
+                        {run.state}
+                    </Tag>
+                }
+                footer={
+                    <Tabs activeKey={tab} onChange={onChangeTab || nop}>
+                        <Tabs.TabPane tab="Request" key="request" />
+                        <Tabs.TabPane tab="Run Log" key="run_log" />
+                        {/*<Tabs.TabPane tab="Task Logs" key="task_logs" /> TODO: Implement v0.2 */}
+                    </Tabs>
+                }
+                onBack={onBack || nop}
+            >
                 <Row type="flex">
-                    <Statistic title="Started" value={renderDate(run.details.run_log.start_time) || "N/A"} />
-                    <Statistic title="Ended" value={renderDate(run.details.run_log.end_time) || "N/A"}
-                               style={{marginLeft: "24px"}} />
+                    <Statistic
+                        title="Started"
+                        value={
+                            renderDate(run.details.run_log.start_time) || "N/A"
+                        }
+                    />
+                    <Statistic
+                        title="Ended"
+                        value={
+                            renderDate(run.details.run_log.end_time) || "N/A"
+                        }
+                        style={{ marginLeft: "24px" }}
+                    />
                 </Row>
             </PageHeader>
-            <div style={{margin: "24px 24px 16px 24px"}}>
+            <div style={{ margin: "24px 24px 16px 24px" }}>
                 <Content run={run} />
             </div>
-        </>;
-    }
-}
+        </>
+    );
+};
 
 Run.propTypes = {
     tab: PropTypes.oneOf(["request", "run_log", "task_logs"]),
