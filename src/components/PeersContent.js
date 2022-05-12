1189150c230eb19c7798088dd2426766e13f7fce
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-12 09:24:08 -0400

    refactored PeersContent.js

--- src/components/PeersContent.js
+++ src/components/PeersContent.js
@@ -1,58 +1,78 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import {Icon, Layout, Table} from "antd";
+import { Icon, Layout, Table } from "antd";
 
 import SitePageHeader from "./SitePageHeader";
 
-import {SITE_NAME} from "../constants";
+import { SITE_NAME } from "../constants";
 
-class PeersContent extends Component {
-    constructor(props) {
-        super(props);
-        this.peerColumns = [
-            {
-                title: "",
-                key: "icon",
-                width: 75,
-                render: (_, peer) => <div style={{width: "100%", textAlign: "center"}}>
-                    <Icon type={this.props.nodeInfo.CHORD_URL === peer.url ? "home" : "global"} />
-                </div>,
-            },
-            {
-                title: "Peer",
-                dataIndex: "url",
-                render: url => <>
-                    <a href={url} target="_blank" rel="noreferrer noopener">{url}</a>
-                    {this.props.nodeInfo.CHORD_URL === url ? <span style={{marginLeft: "0.5em"}}>(current node)</span>
-                        : null}
-                </>,
-                sorter: (a, b) => a.url.localeCompare(b.url),
-                defaultSortOrder: "ascend"
-            }
-        ];
-    }
-
-    componentDidMount() {
+const PeersContent = ({ nodeInfo, peers, isFetchingPeers }) => {
+    useEffect(() => {
         document.title = `${SITE_NAME} - Peers`;
-    }
+    });
 
-    render() {
-        return <>
-            <SitePageHeader title="Peers" subTitle="Other Bento nodes connected to this one" />
+    const peerColumns = [
+        {
+            title: "",
+            key: "icon",
+            width: 75,
+            render: (_, peer) => (
+                <div style={{ width: "100%", textAlign: "center" }}>
+                    <Icon
+                        type={
+                            nodeInfo.CHORD_URL === peer.url ? "home" : "global"
+                        }
+                    />
+                </div>
+            ),
+        },
+        {
+            title: "Peer",
+            dataIndex: "url",
+            render: (url) => (
+                <>
+                    <a href={url} target="_blank" rel="noreferrer noopener">
+                        {url}
+                    </a>
+                    {nodeInfo.CHORD_URL === url ? (
+                        <span style={{ marginLeft: "0.5em" }}>
+                            (current node)
+                        </span>
+                    ) : null}
+                </>
+            ),
+            sorter: (a, b) => a.url.localeCompare(b.url),
+            defaultSortOrder: "ascend",
+        },
+    ];
+
+    return (
+        <>
+            <SitePageHeader
+                title="Peers"
+                subTitle="Other Bento nodes connected to this one"
+            />
             <Layout>
-                <Layout.Content style={{background: "white", padding: "32px 24px 4px"}}>
-                    <Table dataSource={this.props.peers}
-                           columns={this.peerColumns}
-                           loading={this.props.isFetchingPeers}
-                           rowKey="url"
-                           bordered={true}
-                           size="middle" />
+                <Layout.Content
+                    style={{
+                        background: "white",
+                        padding: "32px 24px 4px",
+                    }}
+                >
+                    <Table
+                        dataSource={peers}
+                        columns={peerColumns}
+                        loading={isFetchingPeers}
+                        rowKey="url"
+                        bordered={true}
+                        size="middle"
+                    />
                 </Layout.Content>
             </Layout>
-        </>;
-    }
-}
+        </>
+    );
+};
 
 PeersContent.propTypes = {
     nodeInfo: PropTypes.shape({
@@ -62,9 +82,9 @@ PeersContent.propTypes = {
     isFetchingPeers: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     nodeInfo: state.nodeInfo.data,
-    peers: state.peers.items.map(p => ({url: p})),
+    peers: state.peers.items.map((p) => ({ url: p })),
     isFetchingPeers: state.services.isFetchingAll || state.peers.isFetching,
 });
 
