ba23f0c5d387be5b9ebf7f6188000e9793342a18
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-12 12:01:20 -0400

    refactored DiscoveryDatasetContent.js

--- src/components/discovery/DiscoveryDatasetContent.js
+++ src/components/discovery/DiscoveryDatasetContent.js
@@ -1,53 +1,65 @@
-import React, {Component} from "react";
-import {withRouter} from "react-router-dom";
-import {connect} from "react-redux";
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import {Skeleton} from "antd";
+import { Skeleton } from "antd";
 
 import Dataset from "../datasets/Dataset";
-import {projectPropTypesShape} from "../../propTypes";
+import { projectPropTypesShape } from "../../propTypes";
 
-class DiscoveryDatasetContent extends Component {
-    render() {
-        const datasetId = this.props.match.params.dataset || null;
-        if (!datasetId || this.props.isFetchingUserDependentData) {
-            // TODO: Nicer
-            return <Skeleton />;
-        }
-
-        const project = this.props.projects.find(p => p.datasets.find(d => d.identifier === datasetId));
-        if (!project) return null;  // TODO: 404 or error
-
-        // TODO: Deduplicate with RoutedProject
-        const tables = this.props.serviceTablesByServiceID;
-        const projectTableOwnershipRecords = this.props.projectTablesByProjectID[project.identifier] || [];
-
-        const tableList = projectTableOwnershipRecords
-            .filter(tableOwnership =>
-                (tables[tableOwnership.service_id] || {}).tablesByID.hasOwnProperty(tableOwnership.table_id))
-            .map(tableOwnership => ({
-                ...tableOwnership,
-                ...tables[tableOwnership.service_id].tablesByID[tableOwnership.table_id],
-            }));
-
-        const dataset = {
-            ...project.datasets.find(d => d.identifier === datasetId),
-            tables: tableList.filter(t => t.dataset === datasetId),  // TODO: Filter how?
-        };
-
-        return <Dataset mode="public" value={dataset} project={project} />;
+const DiscoveryDatasetContent = ({
+    match,
+    projects,
+    projectTablesByProjectID,
+    serviceTablesByServiceID,
+    isFetchingUserDependentData,
+}) => {
+    const datasetId = match.params.dataset || null;
+    if (!datasetId || isFetchingUserDependentData) {
+        // TODO: Nicer
+        return <Skeleton />;
     }
-}
+
+    const project = projects.find((p) =>
+        p.datasets.find((d) => d.identifier === datasetId)
+    );
+    if (!project) return null; // TODO: 404 or error
+
+    // TODO: Deduplicate with RoutedProject
+    const tables = serviceTablesByServiceID;
+    const projectTableOwnershipRecords =
+        projectTablesByProjectID[project.identifier] || [];
+
+    const tableList = projectTableOwnershipRecords
+        .filter((tableOwnership) =>
+            (tables[tableOwnership.service_id] || {}).tablesByID.hasOwnProperty(
+                tableOwnership.table_id
+            )
+        )
+        .map((tableOwnership) => ({
+            ...tableOwnership,
+            ...tables[tableOwnership.service_id].tablesByID[
+                tableOwnership.table_id
+            ],
+        }));
+
+    const dataset = {
+        ...project.datasets.find((d) => d.identifier === datasetId),
+        tables: tableList.filter((t) => t.dataset === datasetId), // TODO: Filter how?
+    };
+
+    return <Dataset mode="public" value={dataset} project={project} />;
+};
 
 DiscoveryDatasetContent.propTypes = {
     projects: PropTypes.arrayOf(projectPropTypesShape),
-    projectTablesByProjectID: PropTypes.object,  // TODO: Shape
-    serviceTablesByServiceID: PropTypes.object,  // TODO: Shape
+    projectTablesByProjectID: PropTypes.object, // TODO: Shape
+    serviceTablesByServiceID: PropTypes.object, // TODO: Shape
     isFetchingUserDependentData: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     projects: state.projects.items,
     projectTablesByProjectID: state.projectTables.itemsByProjectID,
     serviceTablesByServiceID: state.serviceTables.itemsByServiceID,
