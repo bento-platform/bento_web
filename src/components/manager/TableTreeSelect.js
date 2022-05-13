23ba0209d2cf8e4592242311ca446708cf06c06c
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-05-13 10:41:28 -0400

    refactored TableTreeSelect.js

--- src/components/manager/TableTreeSelect.js
+++ src/components/manager/TableTreeSelect.js
@@ -1,93 +1,112 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { useState } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import {Spin, Tag, TreeSelect} from "antd";
+import { Spin, Tag, TreeSelect } from "antd";
 
-import {nop} from "../../utils/misc";
+const TableTreeSelect = ({
+    style,
+    value,
+    dataType,
+    onChange,
+    projects,
+    projectTables,
+    tablesByServiceID,
+    servicesLoading,
+    projectsLoading,
+}) => {
+    const [selected, setSelected] = useState(value || undefined);
 
-class TableTreeSelect extends Component {
-    static getDerivedStateFromProps(nextProps) {
-        if ("value" in nextProps) {
-            return {selected: nextProps.value || undefined};
-        }
-        return null;
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {selected: props.value || undefined};
-    }
-
-    onChange(selected) {
+    const onChangeLocal = (selectedNew) => {
         // Set the state directly unless value is bound
-        if (!("value" in this.props)) this.setState({selected});
+        setSelected(selectedNew);
 
         // Update the change handler bound to the component
-        if (this.props.onChange) this.props.onChange(this.state.selected);
-    }
+        if (onChange) onChange(selectedNew);
+    };
 
-    render() {
-        // TODO: Handle table loading better
+    // TODO: Handle table loading better
 
-        const getTableName = (serviceID, tableID) =>
-            this.props.tablesByServiceID[serviceID]?.tablesByID?.[tableID]?.name;
+    const getTableName = (serviceID, tableID) =>
+        tablesByServiceID[serviceID]?.tablesByID?.[tableID]?.name;
 
-        const dataType = this.props.dataType ?? null;
+    dataType = dataType ?? null;
 
-        const selectTreeData = this.props.projects.map(p => ({
-            title: p.title,
+    const selectTreeData = projects.map((p) => ({
+        title: p.title,
+        selectable: false,
+        key: `project:${p.identifier}`,
+        value: `project:${p.identifier}`,
+        data: p,
+        children: p.datasets.map((d) => ({
+            title: d.title,
             selectable: false,
-            key: `project:${p.identifier}`,
-            value: `project:${p.identifier}`,
-            data: p,
-            children: p.datasets.map(d => ({
-                title: d.title,
-                selectable: false,
-                key: `dataset:${d.identifier}`,
-                value: `dataset:${d.identifier}`,
-                data: d,
-                children: (this.props.projectTables[p.identifier] ?? [])
-                    .filter(t => t.dataset === d.identifier &&
-                        ((this.props.tablesByServiceID?.[t.service_id]?.tablesByID ?? {})
-                            .hasOwnProperty(t.table_id)))
-                    .map(t => ({
-                        ...t,
-                        tableName: getTableName(t.service_id, t.table_id) ?? "",
-                        dataType: this.props.tablesByServiceID[t.service_id].tablesByID[t.table_id].data_type,
-                    }))
-                    .map(t => ({
-                        title: <>
-                            <Tag style={{marginRight: "1em"}}>{t.dataType}</Tag>
-                            {t.tableName}&nbsp;
-                            (<span style={{fontFamily: "monospace"}}>{t.table_id}</span>)
-                        </>,
-                        disabled: !(dataType === null || dataType === t.dataType),
-                        isLeaf: true,
-                        key: `${p.identifier}:${t.dataType}:${t.table_id}`,
-                        value: `${p.identifier}:${t.dataType}:${t.table_id}`,
-                        data: t,
-                    })),
-            }))
-        }));
+            key: `dataset:${d.identifier}`,
+            value: `dataset:${d.identifier}`,
+            data: d,
+            children: (projectTables[p.identifier] ?? [])
+                .filter(
+                    (t) =>
+                        t.dataset === d.identifier &&
+                        (
+                            tablesByServiceID?.[t.service_id]?.tablesByID ?? {}
+                        ).hasOwnProperty(t.table_id)
+                )
+                .map((t) => ({
+                    ...t,
+                    tableName: getTableName(t.service_id, t.table_id) ?? "",
+                    dataType:
+                        tablesByServiceID[t.service_id].tablesByID[t.table_id]
+                            .data_type,
+                }))
+                .map((t) => ({
+                    title: (
+                        <>
+                            <Tag style={{ marginRight: "1em" }}>
+                                {t.dataType}
+                            </Tag>
+                            {t.tableName}&nbsp; (
+                            <span style={{ fontFamily: "monospace" }}>
+                                {t.table_id}
+                            </span>
+                            )
+                        </>
+                    ),
+                    disabled: !(dataType === null || dataType === t.dataType),
+                    isLeaf: true,
+                    key: `${p.identifier}:${t.dataType}:${t.table_id}`,
+                    value: `${p.identifier}:${t.dataType}:${t.table_id}`,
+                    data: t,
+                })),
+        })),
+    }));
 
-        return <Spin spinning={this.props.servicesLoading || this.props.projectsLoading}>
-            <TreeSelect style={this.props.style ?? {}}
-                        showSearch={true}
-                        filterTreeNode={(v, n) => {
-                            const filter = v.toLocaleLowerCase().trim();
-                            if (filter === "") return true;
-                            return n.key.toLocaleLowerCase().includes(filter)
-                                || n.props.data.title.toLocaleLowerCase().includes(filter)
-                                || (n.props.data.dataType || "").toLocaleLowerCase().includes(filter);
-                        }}
-                        onChange={this.props.onChange ?? nop}
-                        value={this.state.selected}
-                        treeData={selectTreeData}
-                        treeDefaultExpandAll={true} />
-        </Spin>;
-    }
-}
+    return (
+        <Spin spinning={servicesLoading || projectsLoading}>
+            <TreeSelect
+                style={style ?? {}}
+                showSearch={true}
+                filterTreeNode={(v, n) => {
+                    const filter = v.toLocaleLowerCase().trim();
+                    if (filter === "") return true;
+                    return (
+                        n.key.toLocaleLowerCase().includes(filter) ||
+                        n.props.data.title
+                            .toLocaleLowerCase()
+                            .includes(filter) ||
+                        (n.props.data.dataType || "")
+                            .toLocaleLowerCase()
+                            .includes(filter)
+                    );
+                }}
+                onChange={onChangeLocal}
+                value={selected}
+                treeData={selectTreeData}
+                treeDefaultExpandAll={true}
+            />
+        </Spin>
+    );
+};
 
 TableTreeSelect.propTypes = {
     style: PropTypes.object,
@@ -98,14 +117,14 @@ TableTreeSelect.propTypes = {
     onChange: PropTypes.func,
 
     projects: PropTypes.array,
-    projectTables: PropTypes.object,  // TODO: Shape
-    tablesByServiceID: PropTypes.objectOf(PropTypes.object),  // TODO: Shape
+    projectTables: PropTypes.object, // TODO: Shape
+    tablesByServiceID: PropTypes.objectOf(PropTypes.object), // TODO: Shape
 
     servicesLoading: PropTypes.bool,
     projectsLoading: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     projects: state.projects.items,
     projectTables: state.projectTables.itemsByProjectID,
     tablesByServiceID: state.serviceTables.itemsByServiceID,
