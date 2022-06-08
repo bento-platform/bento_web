6453e66be62c2e854bd85528036d27207bb91614
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-06-08 13:01:25 -0400

    refactored stuff

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {Layout} from "antd";
+import React, { Component } from "react";
+import { Layout } from "antd";
 import BentoLogo from "../images/Bento_final.svg";
 
 const BENTO_LOGO_WIDTH = "125px";
@@ -7,41 +7,46 @@ const BENTO_LOGO_WIDTH = "125px";
 class SiteFooter extends Component {
     render() {
         return (
-      <Layout.Footer>
-        <div
-          style={{
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              alignItems: "center",
-          }}
-        >
-          <div>
-            <div>
-              <p style={{margin: "0"}}>Powered by</p>
-            </div>
+            <Layout.Footer>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <div>
+                        <div>
+                            <p style={{ margin: "0" }}>Powered by</p>
+                        </div>
 
-            <div style={{width: BENTO_LOGO_WIDTH}}>
-              <img src={BentoLogo} alt="Bento logo" />
-            </div>
-          </div>
+                        <div style={{ width: BENTO_LOGO_WIDTH }}>
+                            <img src={BentoLogo} alt="Bento logo" />
+                        </div>
+                    </div>
 
-          <div style={{textAlign: "center"}}>
-            Copyright &copy; 2019-2022 the{" "}
-            <a href="http://computationalgenomics.ca">
-              Canadian Centre for Computational Genomics
-            </a>
-            . <br />
-            <span style={{fontFamily: "monospace"}}>bento_web</span> is licensed
-            under the{" "}
-            <a href="https://github.com/bento-platform/bento_web/blob/master/LICENSE">
-              LGPLv3
-            </a>
-            . The source code is available{" "}
-            <a href="https://github.com/bento-platform/bento_web">on GitHub</a>.
-          </div>
-        </div>
-      </Layout.Footer>
+                    <div style={{ textAlign: "center" }}>
+                        Copyright &copy; 2019-2022 the{" "}
+                        <a href="http://computationalgenomics.ca">
+                            Canadian Centre for Computational Genomics
+                        </a>
+                        . <br />
+                        <span style={{ fontFamily: "monospace" }}>
+                            bento_web
+                        </span>{" "}
+                        is licensed under the{" "}
+                        <a href="https://github.com/bento-platform/bento_web/blob/master/LICENSE">
+                            LGPLv3
+                        </a>
+                        . The source code is available{" "}
+                        <a href="https://github.com/bento-platform/bento_web">
+                            on GitHub
+                        </a>
+                        .
+                    </div>
+                </div>
+            </Layout.Footer>
         );
     }
 }
