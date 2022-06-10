525ce1955c9e06ee42690f40df01bf4ed8ad6c5f
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-06-10 14:40:52 -0400

    converted function based

--- src/components/overview/ClinicalSummary.js
+++ src/components/overview/ClinicalSummary.js
@@ -1,84 +1,74 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import {Col, Row, Spin, Statistic, Typography} from "antd";
+import { Col, Row, Spin, Statistic, Typography } from "antd";
 import CustomPieChart from "./CustomPieChart";
 import Histogram from "./Histogram";
 import { setAutoQueryPageTransition } from "../../modules/explorer/actions";
-import {
-    overviewSummaryPropTypesShape,
-} from "../../propTypes";
-import {mapNameValueFields} from "../../utils/mapNameValueFields";
+import { overviewSummaryPropTypesShape } from "../../propTypes";
+import { mapNameValueFields } from "../../utils/mapNameValueFields";
 
-const mapStateToProps = state => ({
-    overviewSummary: state.overviewSummary,
-    otherThresholdPercentage: state.explorer.otherThresholdPercentage
-});
+const ClinicalSummary = ({
+                             overviewSummary,
+                             setAutoQueryPageTransition,
+                             otherThresholdPercentage,
+                         }) => {
+    const chartHeight = 300;
+    const chartAspectRatio = 1.8;
 
-const actionCreators = {
-    setAutoQueryPageTransition
-};
+    const { data, isFetching } = overviewSummary;
 
-class ClinicalSummary extends Component {
+    const numParticipants = data.data_type_specific?.individuals?.count;
+    const numDiseases = data.data_type_specific?.diseases?.count;
+    const numPhenotypicFeatures =
+        overviewSummary.data?.data_type_specific?.phenotypic_features?.count;
+    const numExperiments =
+        overviewSummary.data?.data_type_specific?.experiments?.count;
 
-    static propTypes = {
-        overviewSummary: PropTypes.shape({
-            isFetching: PropTypes.bool,
-            data: overviewSummaryPropTypesShape
-        }).isRequired,
-        setAutoQueryPageTransition: PropTypes.func, // temp
-        otherThresholdPercentage: PropTypes.number
-    };
+    const biosampleLabels = mapNameValueFields(
+        data.data_type_specific?.biosamples?.sampled_tissue,
+        otherThresholdPercentage / 100
+    );
+    const numBiosamples = data.data_type_specific?.biosamples?.count;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            chartPadding:  "1rem",
-            chartHeight: 300,
-            chartAspectRatio: 1.8,
-        };
-    }
+    const sexLabels = mapNameValueFields(
+        data.data_type_specific?.individuals?.sex,
+        -1
+    );
+    const binnedParticipantAges = binAges(
+        data.data_type_specific?.individuals?.age
+    );
+    const diseaseLabels = mapNameValueFields(
+        data.data_type_specific?.diseases?.term,
+        otherThresholdPercentage / 100
+    );
+    const phenotypicFeatureLabels = mapNameValueFields(
+        data.data_type_specific?.phenotypic_features?.type,
+        otherThresholdPercentage / 100
+    );
+    const autoQueryDataType = "phenopacket";
 
-    render() {
-        const {overviewSummary, otherThresholdPercentage} = this.props;
-        const {data, isFetching} = overviewSummary;
-
-        const numParticipants = data.data_type_specific?.individuals?.count;
-        const numDiseases = data.data_type_specific?.diseases?.count;
-        const numPhenotypicFeatures = overviewSummary.data?.data_type_specific?.phenotypic_features?.count;
-        const numExperiments = overviewSummary.data?.data_type_specific?.experiments?.count;
-
-        const biosampleLabels = mapNameValueFields(
-            data.data_type_specific?.biosamples?.sampled_tissue,
-            otherThresholdPercentage / 100
-        );
-        const numBiosamples = data.data_type_specific?.biosamples?.count;
-
-        const sexLabels = mapNameValueFields(data.data_type_specific?.individuals?.sex, -1);
-        const binnedParticipantAges = binAges(data.data_type_specific?.individuals?.age);
-        const diseaseLabels = mapNameValueFields(
-            data.data_type_specific?.diseases?.term,
-            otherThresholdPercentage / 100
-        );
-        const phenotypicFeatureLabels = mapNameValueFields(
-            data.data_type_specific?.phenotypic_features?.type,
-            otherThresholdPercentage / 100);
-        const autoQueryDataType = "phenopacket";
-
-        return <>
+    return (
+        <>
             <Row>
                 <Typography.Title level={4}>
                     Clinical/Phenotypical Data
                 </Typography.Title>
-                <Row style={{marginBottom: "24px"}} gutter={[0, 16]}>
+                <Row style={{ marginBottom: "24px" }} gutter={[0, 16]}>
                     <Col xl={2} lg={3} md={5} sm={6} xs={10}>
                         <Spin spinning={isFetching}>
-                            <Statistic title="Participants" value={numParticipants} />
+                            <Statistic
+                                title="Participants"
+                                value={numParticipants}
+                            />
                         </Spin>
                     </Col>
                     <Col xl={2} lg={3} md={5} sm={6} xs={10}>
                         <Spin spinning={isFetching}>
-                            <Statistic title="Biosamples" value={numBiosamples} />
+                            <Statistic
+                                title="Biosamples"
+                                value={numBiosamples}
+                            />
                         </Spin>
                     </Col>
                     <Col xl={2} lg={3} md={5} sm={6} xs={10}>
@@ -88,91 +78,130 @@ class ClinicalSummary extends Component {
                     </Col>
                     <Col xl={2} lg={3} md={5} sm={6} xs={10}>
                         <Spin spinning={isFetching}>
-                            <Statistic title="Phenotypic Features" value={numPhenotypicFeatures} />
+                            <Statistic
+                                title="Phenotypic Features"
+                                value={numPhenotypicFeatures}
+                            />
                         </Spin>
                     </Col>
                     <Col xl={2} lg={3} md={5} sm={6} xs={10}>
                         <Spin spinning={isFetching}>
-                            <Statistic title="Experiments" value={numExperiments} />
+                            <Statistic
+                                title="Experiments"
+                                value={numExperiments}
+                            />
                         </Spin>
                     </Col>
                 </Row>
-                <Row style={{display: "flex", flexWrap: "wrap"}} >
-            <Col style={{ textAlign: "center" }}>
-              <Spin spinning={isFetching}>
-                <CustomPieChart
-                  title="Individuals"
-                  data={sexLabels}
-                  chartHeight={this.state.chartHeight}
-                  chartAspectRatio={this.state.chartAspectRatio}
-                  fieldLabel={"[dataset item].subject.sex"}
-                  setAutoQueryPageTransition={this.props.setAutoQueryPageTransition}
-                  autoQueryDataType={autoQueryDataType}
-                />
-              </Spin>
-            </Col>
-            <Col  style={{ textAlign: "center" }}>
-              <Spin spinning={isFetching}>
-                <CustomPieChart
-                  title="Diseases"
-                  data={diseaseLabels}
-                  chartHeight={this.state.chartHeight}
-                  chartAspectRatio={this.state.chartAspectRatio}
-                  fieldLabel={"[dataset item].diseases.[item].term.label"}
-                  setAutoQueryPageTransition={this.props.setAutoQueryPageTransition}
-                  autoQueryDataType={autoQueryDataType}
-                />
-              </Spin>
-            </Col>
-            <Col  style={{ textAlign: "center" }}>
-              <Spin spinning={isFetching}>
-                <Histogram
-                  title="Ages"
-                  data={binnedParticipantAges}
-                  chartAspectRatio={this.state.chartAspectRatio}
-                  chartHeight={this.state.chartHeight}
-                />
-              </Spin>
-            </Col>
-            <Col style={{ textAlign: "center" }}>
-              <Spin spinning={isFetching}>
-                <CustomPieChart
-                  title="Biosamples"
-                  data={biosampleLabels}
-                  chartHeight={this.state.chartHeight}
-                  chartAspectRatio={this.state.chartAspectRatio}
-                  fieldLabel={"[dataset item].biosamples.[item].sampled_tissue.label"}
-                  setAutoQueryPageTransition={this.props.setAutoQueryPageTransition}
-                  autoQueryDataType={autoQueryDataType}
-                />
-              </Spin>
-            </Col>
-            {Boolean(phenotypicFeatureLabels.length) && <Col  style={{ textAlign: "center" }}>
-              <Spin spinning={isFetching}>
-                <CustomPieChart
-                  title="Phenotypic Features"
-                  data={phenotypicFeatureLabels}
-                  chartHeight={this.state.chartHeight}
-                  chartAspectRatio={this.state.chartAspectRatio}
-                  fieldLabel={"[dataset item].phenotypic_features.[item].type.label"}
-                  setAutoQueryPageTransition={this.props.setAutoQueryPageTransition}
-                  autoQueryDataType={autoQueryDataType}
-                />
-              </Spin>
-            </Col>}
-          </Row>
-
+                <Row style={{ display: "flex", flexWrap: "wrap" }}>
+                    <Col style={{ textAlign: "center" }}>
+                        <Spin spinning={isFetching}>
+                            <CustomPieChart
+                                title="Individuals"
+                                data={sexLabels}
+                                chartHeight={chartHeight}
+                                chartAspectRatio={chartAspectRatio}
+                                fieldLabel={"[dataset item].subject.sex"}
+                                setAutoQueryPageTransition={
+                                    setAutoQueryPageTransition
+                                }
+                                autoQueryDataType={autoQueryDataType}
+                            />
+                        </Spin>
+                    </Col>
+                    <Col style={{ textAlign: "center" }}>
+                        <Spin spinning={isFetching}>
+                            <CustomPieChart
+                                title="Diseases"
+                                data={diseaseLabels}
+                                chartHeight={chartHeight}
+                                chartAspectRatio={chartAspectRatio}
+                                fieldLabel={
+                                    "[dataset item].diseases.[item].term.label"
+                                }
+                                setAutoQueryPageTransition={
+                                    setAutoQueryPageTransition
+                                }
+                                autoQueryDataType={autoQueryDataType}
+                            />
+                        </Spin>
+                    </Col>
+                    <Col style={{ textAlign: "center" }}>
+                        <Spin spinning={isFetching}>
+                            <Histogram
+                                title="Ages"
+                                data={binnedParticipantAges}
+                                chartAspectRatio={chartAspectRatio}
+                                chartHeight={chartHeight}
+                            />
+                        </Spin>
+                    </Col>
+                    <Col style={{ textAlign: "center" }}>
+                        <Spin spinning={isFetching}>
+                            <CustomPieChart
+                                title="Biosamples"
+                                data={biosampleLabels}
+                                chartHeight={chartHeight}
+                                chartAspectRatio={chartAspectRatio}
+                                fieldLabel={
+                                    "[dataset item].biosamples.[item].sampled_tissue.label"
+                                }
+                                setAutoQueryPageTransition={
+                                    setAutoQueryPageTransition
+                                }
+                                autoQueryDataType={autoQueryDataType}
+                            />
+                        </Spin>
+                    </Col>
+                    {Boolean(phenotypicFeatureLabels.length) && (
+                        <Col style={{ textAlign: "center" }}>
+                            <Spin spinning={isFetching}>
+                                <CustomPieChart
+                                    title="Phenotypic Features"
+                                    data={phenotypicFeatureLabels}
+                                    chartHeight={chartHeight}
+                                    chartAspectRatio={chartAspectRatio}
+                                    fieldLabel={
+                                        "[dataset item].phenotypic_features.[item].type.label"
+                                    }
+                                    setAutoQueryPageTransition={
+                                        setAutoQueryPageTransition
+                                    }
+                                    autoQueryDataType={autoQueryDataType}
+                                />
+                            </Spin>
+                        </Col>
+                    )}
+                </Row>
             </Row>
-        </>;
-    }
-}
+        </>
+    );
+};
+
+ClinicalSummary.propTypes = {
+    overviewSummary: PropTypes.shape({
+        isFetching: PropTypes.bool,
+        data: overviewSummaryPropTypesShape,
+    }).isRequired,
+    setAutoQueryPageTransition: PropTypes.func, // temp
+    otherThresholdPercentage: PropTypes.number,
+};
+
+const mapStateToProps = (state) => ({
+    overviewSummary: state.overviewSummary,
+    otherThresholdPercentage: state.explorer.otherThresholdPercentage,
+});
+
+const actionCreators = {
+    setAutoQueryPageTransition,
+};
 
 export default connect(mapStateToProps, actionCreators)(ClinicalSummary);
 
 // custom binning function
 // input is object: {age1: count1, age2: count2....}
 // outputs an array [{bin1: bin1count}, {bin2: bin2count}...]
-function binAges (ages) {
+const binAges = (ages) => {
     if (!ages) {
         return null;
     }
@@ -202,7 +231,7 @@ function binAges (ages) {
     }
 
     // return histogram-friendly array
-    return Object.keys(ageBinCounts).map(age => {
-        return {ageBin: age, count: ageBinCounts[age]};
+    return Object.keys(ageBinCounts).map((age) => {
+        return { ageBin: age, count: ageBinCounts[age] };
     });
-}
+};
