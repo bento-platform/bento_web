a2518b7615a776f7010ec305eab5a6e3b888a2e3
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2022-07-20 15:25:51 -0400

    converted to func based and removed network size

--- src/components/ServiceContent.js
+++ src/components/ServiceContent.js
@@ -1,91 +1,56 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import {Col, Layout, Row, Spin, Statistic, Typography} from "antd";
+import { Col, Layout, Row, Spin, Statistic, Typography } from "antd";
 
 import SitePageHeader from "./SitePageHeader";
 import ServiceList from "./ServiceList";
 
-import {SITE_NAME} from "../constants";
-import {nodeInfoDataPropTypesShape, projectPropTypesShape} from "../propTypes";
-import {EM_DASH} from "../constants";
+import { SITE_NAME } from "../constants";
+import { EM_DASH } from "../constants";
 
-
-class ServiceContent extends Component {
-    componentDidMount() {
+const ServiceContent = () => {
+    useEffect(() => {
         document.title = `${SITE_NAME} - Dashboard`;
-    }
+    }, []);
 
-    render() {
-        return <>
+    const nodeInfo = useSelector((state) => state.nodeInfo.data);
+    const isFetchingNodeInfo = useSelector((state) => state.nodeInfo.isFetching);
+    const projects = useSelector((state) => state.projects.items);
+    const isFetchingProjects = useSelector((state) => state.auth.isFetchingDependentData || state.projects.isFetching);
+
+    return (
+        <>
             <SitePageHeader title="Services" subTitle="Node status and health monitor" />
             <Layout>
-                <Layout.Content style={{background: "white", padding: "32px 24px 4px"}}>
-                    <Row style={{marginBottom: "24px"}} gutter={[0, 16]}>
+                <Layout.Content style={{ background: "white", padding: "32px 24px 4px" }}>
+                    <Row style={{ marginBottom: "24px" }} gutter={[0, 16]}>
                         <Col lg={24} xl={12}>
-                            <Spin spinning={this.props.isFetchingNodeInfo}>
-                                <Statistic title="Node URL"
-                                           value={this.props.isFetchingNodeInfo
-                                               ? EM_DASH
-                                               : this.props.nodeInfo.CHORD_URL} />
+                            <Spin spinning={isFetchingNodeInfo}>
+                                <Statistic title="Node URL" value={isFetchingNodeInfo ? EM_DASH : nodeInfo.CHORD_URL} />
                             </Spin>
                         </Col>
                         <Col md={12} lg={8} xl={3}>
-                            <Spin spinning={this.props.isFetchingProjects}>
-                                <Statistic title="Projects"
-                                           value={this.props.isFetchingProjects
-                                               ? EM_DASH
-                                               : this.props.projects.length} />
+                            <Spin spinning={isFetchingProjects}>
+                                <Statistic title="Projects" value={isFetchingProjects ? EM_DASH : projects.length} />
                             </Spin>
                         </Col>
                         <Col md={12} lg={8} xl={3}>
-                            <Spin spinning={this.props.isFetchingProjects}>
-                                <Statistic title="Datasets"
-                                           value={this.props.isFetchingProjects
-                                               ? EM_DASH
-                                               : this.props.projects.flatMap(p => p.datasets).length} />
+                            <Spin spinning={isFetchingProjects}>
+                                <Statistic
+                                    title="Datasets"
+                                    value={isFetchingProjects ? EM_DASH : projects.flatMap((p) => p.datasets).length}
+                                />
                             </Spin>
                         </Col>
                         {/* TODO: Tables */}
-                        <Col md={12} lg={8} xl={3}>
-                            <Spin spinning={this.props.isFetchingPeers}>
-                                {/* Exclude self */}
-                                <Statistic title="Network Size"
-                                           value={this.props.isFetchingPeers
-                                               ? EM_DASH
-                                               : this.props.peers.length} />
-                            </Spin>
-                        </Col>
                     </Row>
                     <Typography.Title level={3}>Services</Typography.Title>
                     <ServiceList />
                 </Layout.Content>
             </Layout>
-        </>;
-    }
-}
-
-ServiceContent.propTypes = {
-    nodeInfo: nodeInfoDataPropTypesShape,
-    isFetchingNodeInfo: PropTypes.bool,
-
-    projects: PropTypes.arrayOf(projectPropTypesShape),
-    isFetchingProjects: PropTypes.bool,
-
-    peers: PropTypes.arrayOf(PropTypes.string),
-    isFetchingPeers: PropTypes.bool,
+        </>
+    );
 };
 
-const mapStateToProps = state => ({
-    nodeInfo: state.nodeInfo.data,
-    isFetchingNodeInfo: state.nodeInfo.isFetching,
-
-    projects: state.projects.items,
-    isFetchingProjects: state.auth.isFetchingDependentData || state.projects.isFetching,
-
-    peers: state.peers.items,
-    isFetchingPeers: state.auth.isFetchingDependentData,
-});
-
-export default connect(mapStateToProps)(ServiceContent);
+export default ServiceContent;
