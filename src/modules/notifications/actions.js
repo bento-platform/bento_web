60824e635354dabe49852e78c016116c17a5462f
David Lougheed <david.lougheed@gmail.com>
2023-01-20 09:43:33 -0500

    chore: rewrite notification drawer to func + make mark all as read button work

--- src/modules/notifications/actions.js
+++ src/modules/notifications/actions.js
@@ -14,19 +14,25 @@ export const addNotification = data => ({
 });
 
 export const FETCH_NOTIFICATIONS = createNetworkActionTypes("FETCH_NOTIFICATIONS");
-
-export const MARK_NOTIFICATION_AS_READ = createNetworkActionTypes("MARK_NOTIFICATION_AS_READ");
-
 export const fetchNotifications = networkAction(() => (dispatch, getState) => ({
     types: FETCH_NOTIFICATIONS,
     url: `${getState().services.notificationService.url}/notifications`,
     err: "Error fetching notifications"
 }));
 
+export const MARK_NOTIFICATION_AS_READ = createNetworkActionTypes("MARK_NOTIFICATION_AS_READ");
 export const markNotificationAsRead = networkAction(notificationID => (dispatch, getState) => ({
     types: MARK_NOTIFICATION_AS_READ,
     params: {notificationID},
     url: `${getState().services.notificationService.url}/notifications/${notificationID}/read`,
     req: {method: "PUT"},
     err: "Error marking notification as read"
+}));
+
+export const MARK_ALL_NOTIFICATIONS_AS_READ = createNetworkActionTypes("MARK_ALL_NOTIFICATIONS_AS_READ");
+export const markAllNotificationsAsRead = networkAction(() => (dispatch, getState) => ({
+    types: MARK_ALL_NOTIFICATIONS_AS_READ,
+    url: `${getState().services.notificationService.url}/notifications/all-read`,
+    req: {method: "PUT"},
+    err: "Error marking all notifications as read"
 }));
