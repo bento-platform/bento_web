b50893cc207060bb9dc249db8d2af9131983d3e7
David Lougheed <david.lougheed@gmail.com>
2023-01-20 09:43:33 -0500

    chore: rewrite notification drawer to func + make mark all as read button work

--- src/components/notifications/NotificationDrawer.js
+++ src/components/notifications/NotificationDrawer.js
@@ -1,61 +1,50 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {withRouter} from "react-router-dom";
-import PropTypes from "prop-types";
+import React, {useCallback, useMemo} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useHistory} from "react-router-dom";
 
 import {Button, Divider, Drawer} from "antd";
 
 import NotificationList from "./NotificationList";
 
-import {hideNotificationDrawer} from "../../modules/notifications/actions";
+import {hideNotificationDrawer, markAllNotificationsAsRead} from "../../modules/notifications/actions";
 import {withBasePath} from "../../utils/url";
-import {notificationPropTypesShape} from "../../propTypes";
 
 
-class NotificationDrawer extends Component {
-    constructor(props) {
-        super(props);
-        this.seeAllNotifications = this.seeAllNotifications.bind(this);
-    }
+const NotificationDrawer = React.memo(() => {
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-    markAllAsRead() {
-        // TODO: Implement
-    }
+    const markAllAsRead = useCallback(() => {
+        dispatch(markAllNotificationsAsRead());
+    }, []);
+    const seeAllNotifications = useCallback(() => {
+        dispatch(hideNotificationDrawer());
+        history.push(withBasePath("notifications"));
+    }, [dispatch, history]);
+    const hideNotificationDrawer_ = useCallback(() => {
+        dispatch(hideNotificationDrawer());
+    }, [dispatch]);
 
-    seeAllNotifications() {
-        this.props.hideNotificationDrawer();
-        this.props.history.push(withBasePath("notifications"));
-    }
+    const notifications = useSelector(state => state.notifications.items);
+    const unreadNotifications = useMemo(() => notifications.filter(n => !n.read), [notifications]);
+    const isMarkingAllAsRead = useSelector(state => state.notifications.isMarkingAllAsRead);
 
-    render() {
-        const unreadNotifications = this.props.notifications.filter(n => !n.read);
-        return <Drawer bodyStyle={{padding: 0}} title="Notifications"
-                       visible={this.props.notificationDrawerVisible}
-                       width="500"
-                       onClose={() => this.props.hideNotificationDrawer()}>
-            <div style={{padding: "16px 24px", display: "flex", gap: "16px"}}>
-                <Button style={{flex: 1}} onClick={() => this.markAllAsRead()}
-                        disabled={!unreadNotifications}>Mark All as Read</Button>
-                <Button style={{flex: 1}} onClick={() => this.seeAllNotifications()}>See All Notifications</Button>
-            </div>
-            <Divider style={{margin: 0}} />
-            <div style={{padding: "0 24px"}}>
-                <NotificationList small={true} notifications={unreadNotifications} />
-            </div>
-        </Drawer>;
-    }
-}
+    const notificationDrawerVisible = useSelector(state => state.notifications.drawerVisible);
 
-NotificationDrawer.propTypes = {
-    notificationDrawerVisible: PropTypes.bool,
-    notifications: PropTypes.arrayOf(notificationPropTypesShape),
-
-    hideNotificationDrawer: PropTypes.func,
-};
-
-const mapStateToProps = state => ({
-    notificationDrawerVisible: state.notifications.drawerVisible,
-    notifications: state.notifications.items,
+    return <Drawer bodyStyle={{padding: 0}} title="Unread Notifications"
+                   visible={notificationDrawerVisible}
+                   width="500"
+                   onClose={hideNotificationDrawer_}>
+        <div style={{padding: "16px 24px", display: "flex", gap: "16px"}}>
+            <Button style={{flex: 1}} onClick={markAllAsRead}
+                    disabled={!unreadNotifications} loading={isMarkingAllAsRead}>Mark All as Read</Button>
+            <Button style={{flex: 1}} onClick={seeAllNotifications}>See All Notifications</Button>
+        </div>
+        <Divider style={{margin: 0}} />
+        <div style={{padding: "0 24px"}}>
+            <NotificationList small={true} notifications={unreadNotifications} />
+        </div>
+    </Drawer>;
 });
 
-export default withRouter(connect(mapStateToProps, {hideNotificationDrawer})(NotificationDrawer));
+export default NotificationDrawer;
