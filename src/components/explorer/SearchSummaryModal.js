e5a49f7acb779c521c02c9dfa2d64993db179f03
Sanjeev Lakhwani <sanjeev.lakhwani@gmail.com>
2023-02-07 11:51:09 -0500

    Added skeleton loading to search summary modal

--- src/components/explorer/SearchSummaryModal.js
+++ src/components/explorer/SearchSummaryModal.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import React, { useEffect, useState } from "react";
-import { Col, Divider, Modal, Row, Statistic, Typography } from "antd";
+import { Col, Divider, Modal, Row, Skeleton, Statistic, Typography } from "antd";
 import CustomPieChart from "../overview/CustomPieChart";
 import Histogram from "../overview/Histogram";
 import { explorerSearchResultsPropTypesShape } from "../../propTypes";
@@ -12,7 +12,6 @@ const MODAL_WIDTH = 1000;
 
 const serializePieChartData = (data) => Object.entries(data).map(([key, value]) => ({ name: key, value }));
 const serializeBarChartData = (data) => Object.entries(data).map(([key, value]) => ({ ageBin: key, count: value }));
-
 
 const SearchSummaryModal = ({ searchResults, ...props }) => {
     const [data, setData] = useState(null);
@@ -44,92 +43,98 @@ const SearchSummaryModal = ({ searchResults, ...props }) => {
             })
             .catch((error) => console.log("error", error));
     }, [searchResults]);
-    // return <div>hi</div>
-    return data ? (
+
+    return (
         <Modal title="Search Results" {...props} width={MODAL_WIDTH} footer={null} style={{ padding: "10px" }}>
-            <Row gutter={16} style={{ display: "flex", flexWrap: "wrap" }}>
-                <Col span={7}>
-                    <Statistic title="Individuals" value={data.individuals.count} />
-                </Col>
-                <Col span={7}>
-                    <Statistic title="Biosamples" value={data.biosamples.count} />
-                </Col>
-                <Col span={7}>
-                    <Statistic title="Experiments" value={data.experiments.count} />
-                </Col>
-            </Row>
-            <Divider />
-            <>
-                <Typography.Title level={4}>Individuals</Typography.Title>
-                <Row gutter={[0, 16]}>
-                    <Col span={12} style={{ textAlign: "center" }}>
-                        <CustomPieChart
-                            title="Sex"
-                            data={serializePieChartData(data.individuals.sex)}
-                            chartHeight={CHART_HEIGHT}
-                            chartAspectRatio={CHART_ASPECT_RATIO}
-                        />
-                    </Col>
-                    <Col span={12} style={{ textAlign: "center" }}>
-                        <CustomPieChart
-                            title="Diseases"
-                            data={serializePieChartData(data.diseases.term)}
-                            chartHeight={CHART_HEIGHT}
-                            chartAspectRatio={CHART_ASPECT_RATIO}
-                        />
-                    </Col>
-                    <Col span={12} style={{ textAlign: "center" }}>
-                        <CustomPieChart
-                            title="Phenotypic Features"
-                            data={serializePieChartData(data.phenotypic_features.type)}
-                            chartHeight={CHART_HEIGHT}
-                            chartAspectRatio={CHART_ASPECT_RATIO}
-                        />
-                    </Col>
-                    <Col span={12} style={{ textAlign: "center" }}>
-                        <Histogram
-                            title="Ages"
-                            data={serializeBarChartData(data.individuals.age)}
-                            chartHeight={CHART_HEIGHT}
-                            chartAspectRatio={CHART_ASPECT_RATIO}
-                        />
-                    </Col>
-                </Row>
-                <Divider />
-                <Typography.Title level={4}>Biosamples</Typography.Title>
-                <Row gutter={[0, 16]}>
-                    <Col span={12} style={{ textAlign: "center" }}>
-                        <CustomPieChart
-                            title="Biosamples by Tissue"
-                            data={serializePieChartData(data.biosamples.sampled_tissue)}
-                            chartHeight={CHART_HEIGHT}
-                            chartAspectRatio={CHART_ASPECT_RATIO}
-                        />
-                    </Col>
-                    <Col span={12} style={{ textAlign: "center" }}>
-                        <CustomPieChart
-                            title="Biosamples by Diagnosis"
-                            data={serializePieChartData(data.biosamples.histological_diagnosis)}
-                            chartHeight={CHART_HEIGHT}
-                            chartAspectRatio={CHART_ASPECT_RATIO}
-                        />
-                    </Col>
-                </Row>
-                <Divider />
-                <Typography.Title level={4}>Experiments</Typography.Title>
-                <Row gutter={[0, 16]}>
-                    <Col span={12} style={{ textAlign: "center" }}>
-                        <CustomPieChart
-                            title="Study Types"
-                            data={serializePieChartData(data.experiments.experiment_type)}
-                            chartHeight={CHART_HEIGHT}
-                            chartAspectRatio={CHART_ASPECT_RATIO}
-                        />
-                    </Col>
-                </Row>
-            </>
+            {data ? (
+                <>
+                    <Row gutter={16} style={{ display: "flex", flexWrap: "wrap" }}>
+                        <Col span={7}>
+                            <Statistic title="Individuals" value={data.individuals.count} />
+                        </Col>
+                        <Col span={7}>
+                            <Statistic title="Biosamples" value={data.biosamples.count} />
+                        </Col>
+                        <Col span={7}>
+                            <Statistic title="Experiments" value={data.experiments.count} />
+                        </Col>
+                    </Row>
+                    <Divider />
+                    <>
+                        <Typography.Title level={4}>Individuals</Typography.Title>
+                        <Row gutter={[0, 16]}>
+                            <Col span={12} style={{ textAlign: "center" }}>
+                                <CustomPieChart
+                                    title="Sex"
+                                    data={serializePieChartData(data.individuals.sex)}
+                                    chartHeight={CHART_HEIGHT}
+                                    chartAspectRatio={CHART_ASPECT_RATIO}
+                                />
+                            </Col>
+                            <Col span={12} style={{ textAlign: "center" }}>
+                                <CustomPieChart
+                                    title="Diseases"
+                                    data={serializePieChartData(data.diseases.term)}
+                                    chartHeight={CHART_HEIGHT}
+                                    chartAspectRatio={CHART_ASPECT_RATIO}
+                                />
+                            </Col>
+                            <Col span={12} style={{ textAlign: "center" }}>
+                                <CustomPieChart
+                                    title="Phenotypic Features"
+                                    data={serializePieChartData(data.phenotypic_features.type)}
+                                    chartHeight={CHART_HEIGHT}
+                                    chartAspectRatio={CHART_ASPECT_RATIO}
+                                />
+                            </Col>
+                            <Col span={12} style={{ textAlign: "center" }}>
+                                <Histogram
+                                    title="Ages"
+                                    data={serializeBarChartData(data.individuals.age)}
+                                    chartHeight={CHART_HEIGHT}
+                                    chartAspectRatio={CHART_ASPECT_RATIO}
+                                />
+                            </Col>
+                        </Row>
+                        <Divider />
+                        <Typography.Title level={4}>Biosamples</Typography.Title>
+                        <Row gutter={[0, 16]}>
+                            <Col span={12} style={{ textAlign: "center" }}>
+                                <CustomPieChart
+                                    title="Biosamples by Tissue"
+                                    data={serializePieChartData(data.biosamples.sampled_tissue)}
+                                    chartHeight={CHART_HEIGHT}
+                                    chartAspectRatio={CHART_ASPECT_RATIO}
+                                />
+                            </Col>
+                            <Col span={12} style={{ textAlign: "center" }}>
+                                <CustomPieChart
+                                    title="Biosamples by Diagnosis"
+                                    data={serializePieChartData(data.biosamples.histological_diagnosis)}
+                                    chartHeight={CHART_HEIGHT}
+                                    chartAspectRatio={CHART_ASPECT_RATIO}
+                                />
+                            </Col>
+                        </Row>
+                        <Divider />
+                        <Typography.Title level={4}>Experiments</Typography.Title>
+                        <Row gutter={[0, 16]}>
+                            <Col span={12} style={{ textAlign: "center" }}>
+                                <CustomPieChart
+                                    title="Study Types"
+                                    data={serializePieChartData(data.experiments.experiment_type)}
+                                    chartHeight={CHART_HEIGHT}
+                                    chartAspectRatio={CHART_ASPECT_RATIO}
+                                />
+                            </Col>
+                        </Row>
+                    </>
+                </>
+            ) : (
+                <Skeleton active />
+            )}
         </Modal>
-    ) : null;
+    );
 };
 
 SearchSummaryModal.propTypes = {
