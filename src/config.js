739e4ef6a237ded8b1608938d7584575445886a0
David Lougheed <david.lougheed@gmail.com>
2023-02-28 15:50:44 -0500

    fix: dev mode cbioportal flag cast

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 
 export const BENTO_URL = BENTO_WEB_CONFIG.BENTO_URL ?? process.env.BENTO_URL ?? process.env.CHORD_URL ?? null;
 export const BENTO_CBIOPORTAL_ENABLED = BENTO_WEB_CONFIG.BENTO_CBIOPORTAL_ENABLED
-    ?? process.env.BENTO_CBIOPORTAL_ENABLED ?? false;
+    ?? ["true", "1", "yes"].includes(process.env.BENTO_CBIOPORTAL_ENABLED || "");
 export const CUSTOM_HEADER = BENTO_WEB_CONFIG.CUSTOM_HEADER ?? process.env.CUSTOM_HEADER ?? null;
