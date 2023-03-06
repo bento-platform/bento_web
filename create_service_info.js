395e7defd51df41b99babfb037f422a6271fa4cd
David Lougheed <david.lougheed@gmail.com>
2023-03-06 16:07:21 -0500

    chore: include environment and git info in service info chore: bump base dev image version to include git

--- create_service_info.js
+++ create_service_info.js
@@ -1,4 +1,7 @@
+const childProcess = require("child_process");
 const packageData = require("./package.json");
+
+const nodeEnv = process.env.NODE_ENV || "production";
 
 const serviceType = {
     group: "ca.c3g.bento",
@@ -8,12 +11,13 @@ const serviceType = {
 
 const serviceID = process.env.SERVICE_ID || `${serviceType.group}:${serviceType.artifact}`;
 
-const serviceInfo = () => ({
+const serviceInfo = {
     id: serviceID,
     name: "Bento Web",
     description: "Private-access web interface for the Bento platform.",
     type: serviceType,
     version: packageData.version,
+    environment: nodeEnv === "production" ? "prod" : "dev",
     organization: {
         name: "C3G",
         url: "https://www.computationalgenomics.ca",
@@ -22,10 +26,21 @@ const serviceInfo = () => ({
     bento: {
         serviceKind: "web",
     },
-});
+};
+
+
+if (nodeEnv === "development") {
+    try {
+        serviceInfo.bento.gitTag = childProcess.execSync("git describe --tags --abbrev=0");
+        serviceInfo.bento.gitBranch = childProcess.execSync("git branch --show-current");
+        serviceInfo.bento.gitCommit = childProcess.execSync("git rev-parse HEAD");
+    } catch (e) {
+        console.warn(`Could not get git information (${e})`);
+    }
+}
 
 if (typeof require !== "undefined" && require.main === module) {
-    console.log(JSON.stringify(serviceInfo(), null, 2));
+    console.log(JSON.stringify(serviceInfo, null, 2));
 }
 
 module.exports = {
