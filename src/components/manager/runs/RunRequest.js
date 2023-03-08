c96925817420131a5429ca114f8610c3711d4e64
David Lougheed <david.lougheed@gmail.com>
2023-03-08 11:21:24 -0500

    chore: rewrite run request as functional component

--- src/components/manager/runs/RunRequest.js
+++ src/components/manager/runs/RunRequest.js
@@ -1,55 +1,55 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React from "react";
+import {useSelector} from "react-redux";
 import PropTypes from "prop-types";
 
 import {Descriptions, List, Tag} from "antd";
 
 import WorkflowListItem from "../WorkflowListItem";
 
-class RunRequest extends Component {
-    render() {
-        const details = (this.props.run || {}).details || {};
+const RunRequest = ({run}) => {
+    const tablesByServiceID = useSelector(state => state.serviceTables.itemsByServiceID);
 
-        const serviceID = details.request.tags.workflow_metadata.serviceID;
-        const tableDataType = details.request.tags.workflow_metadata.data_type;
-        const tableID = details.request.tags.table_id;
-        const tableName = (((this.props.tablesByServiceID[serviceID] || {}).tablesByID || {})[tableID] || {}).name;
+    const details = run?.details ?? {};
 
-        // TODO: Link to some "table" page from the table description item here
+    const serviceID = details.request.tags.workflow_metadata.serviceID;
+    const tableDataType = details.request.tags.workflow_metadata.data_type;
+    const tableID = details.request.tags.table_id;
+    const tableName = tablesByServiceID[serviceID]?.tablesByID?.[tableID]?.name;
 
-        const idFragment = <span style={{fontFamily: "monospace"}}>{tableID}</span>;
+    // TODO: Link to some "table" page from the table description item here
 
-        return <Descriptions bordered>
-            <Descriptions.Item label="Table" span={3}>
-                <Tag>{tableDataType}</Tag> {tableName ? <>{tableName} ({idFragment})</> : idFragment}
-            </Descriptions.Item>
-            <Descriptions.Item label="Parameters" span={3}>
+    const idFragment = <span style={{fontFamily: "monospace"}}>{tableID}</span>;
+
+    return <Descriptions bordered>
+        <Descriptions.Item label="Table" span={3}>
+            <Tag>{tableDataType}</Tag> {tableName ? <>{tableName} ({idFragment})</> : idFragment}
+        </Descriptions.Item>
+        <Descriptions.Item label="Parameters" span={3}>
                 <pre style={{margin: 0}}>{
                     JSON.stringify(details.request.workflow_params, null, 4)
                 }</pre>
-            </Descriptions.Item>
-            <Descriptions.Item label="Workflow Type">
-                {details.request.workflow_type}
-            </Descriptions.Item>
-            <Descriptions.Item label="Workflow Type Version">
-                {details.request.workflow_type_version}
-            </Descriptions.Item>
-            <Descriptions.Item label="Workflow URL">
-                <a href={details.request.workflow_url} target="_blank" rel="noopener noreferrer">
-                    {details.request.workflow_url}
-                </a>
-            </Descriptions.Item>
-            <Descriptions.Item label="Workflow" span={3}>
-                <List itemLayout="vertical">
-                    <WorkflowListItem workflow={details.request.tags.workflow_metadata} />
-                </List>
-            </Descriptions.Item>
-            <Descriptions.Item label="Tags">
-                <pre style={{margin: 0}}>{JSON.stringify(details.request.tags, null, 4)}</pre>
-            </Descriptions.Item>
-        </Descriptions>;
-    }
-}
+        </Descriptions.Item>
+        <Descriptions.Item label="Workflow Type">
+            {details.request.workflow_type}
+        </Descriptions.Item>
+        <Descriptions.Item label="Workflow Type Version">
+            {details.request.workflow_type_version}
+        </Descriptions.Item>
+        <Descriptions.Item label="Workflow URL">
+            <a href={details.request.workflow_url} target="_blank" rel="noopener noreferrer">
+                {details.request.workflow_url}
+            </a>
+        </Descriptions.Item>
+        <Descriptions.Item label="Workflow" span={3}>
+            <List itemLayout="vertical">
+                <WorkflowListItem workflow={details.request.tags.workflow_metadata} />
+            </List>
+        </Descriptions.Item>
+        <Descriptions.Item label="Tags">
+            <pre style={{margin: 0}}>{JSON.stringify(details.request.tags, null, 4)}</pre>
+        </Descriptions.Item>
+    </Descriptions>;
+};
 
 RunRequest.propTypes = {
     run: PropTypes.shape({
@@ -62,11 +62,6 @@ RunRequest.propTypes = {
             })
         })
     }),
-    tablesByServiceID: PropTypes.objectOf(PropTypes.object),  // TODO: Shape
 };
 
-const mapStateToProps = state => ({
-    tablesByServiceID: state.serviceTables.itemsByServiceID,
-});
-
-export default connect(mapStateToProps)(RunRequest);
+export default RunRequest;
