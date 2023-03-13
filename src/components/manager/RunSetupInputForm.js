bd2427f2e4c75c5c705ee4d2c252661b9caa1329
David Lougheed <david.lougheed@gmail.com>
2023-03-13 14:33:39 -0400

    refact: rewrite run setup input form as a func component

--- src/components/manager/RunSetupInputForm.js
+++ src/components/manager/RunSetupInputForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useCallback} from "react";
 import PropTypes from "prop-types";
 
 import {Button, Form, Icon, Input, Select, TreeSelect} from "antd";
@@ -6,8 +6,8 @@ import {Button, Form, Icon, Input, Select, TreeSelect} from "antd";
 import {
     FORM_LABEL_COL,
     FORM_WRAPPER_COL,
-    FORM_BUTTON_COL
-} from "./ingestion";
+    FORM_BUTTON_COL,
+} from "./workflowCommon";
 
 import {nop} from "../../utils/misc";
 import {workflowPropTypesShape} from "../../propTypes";
@@ -26,22 +26,16 @@ const generateFileTree = (directory, valid) => [...directory].sort(sortByName).m
         {entry?.contents ? generateFileTree(entry.contents, valid) : null}
     </TreeSelect.TreeNode>);
 
-class RunSetupInputForm extends Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.getInputComponent = this.getInputComponent.bind(this);
-    }
-
-    handleSubmit(e) {
+const RunSetupInputForm = ({initialValues, form, onSubmit, tree, workflow, onBack}) => {
+    const handleSubmit = useCallback((e) => {
         e.preventDefault();
-        this.props.form.validateFieldsAndScroll((err, values) => {
+        form.validateFieldsAndScroll((err, values) => {
             if (err) return;
-            (this.props.onSubmit || nop)(values);
+            (onSubmit || nop)(values);
         });
-    }
+    }, [form, onSubmit]);
 
-    getInputComponent(input) {
+    const getInputComponent = useCallback((input) => {
         switch (input.type) {
             case "file":
             case "file[]":
@@ -49,7 +43,7 @@ class RunSetupInputForm extends Component {
                 return <TreeSelect showSearch={true} treeDefaultExpandAll={true} multiple={input.type === "file[]"}>
                     <TreeSelect.TreeNode title="Drop Box" key="root">
                         {generateFileTree(
-                            this.props.tree,
+                            tree,
                             entry => entry.hasOwnProperty("contents") ||
                                 input.extensions.find(e => entry.name.endsWith(e)) !== undefined
                         )}
@@ -68,33 +62,29 @@ class RunSetupInputForm extends Component {
             default:
                 return <Input />;
         }
-    }
+    }, [tree]);
 
-    render() {
-        return <Form labelCol={FORM_LABEL_COL} wrapperCol={FORM_WRAPPER_COL} onSubmit={this.handleSubmit}>
-            {[
-                ...this.props.workflow.inputs.map(i => (
-                    <Form.Item label={i.id} key={i.id}>
-                        {this.props.form.getFieldDecorator(i.id, {
-                            initialValue: this.props.initialValues[i.id],  // undefined if not set
-                            // Default to requiring the field unless the "required" property is set on the input
-                            rules: [{required: i.required === undefined ? true : i.required}],
-                        })(this.getInputComponent(i))}
-                    </Form.Item>
-                )),
-
-                <Form.Item key="_submit" wrapperCol={FORM_BUTTON_COL}>
-                    {this.props.onBack
-                        ? <Button icon="left" onClick={() => this.props.onBack()}>Back</Button>
-                        : null}
-                    <Button type="primary" htmlType="submit" style={{float: "right"}}>
-                        Next <Icon type="right" />
-                    </Button>
+    return <Form labelCol={FORM_LABEL_COL} wrapperCol={FORM_WRAPPER_COL} onSubmit={handleSubmit}>
+        {[
+            ...workflow.inputs.map(i => (
+                <Form.Item label={i.id} key={i.id}>
+                    {form.getFieldDecorator(i.id, {
+                        initialValue: initialValues[i.id],  // undefined if not set
+                        // Default to requiring the field unless the "required" property is set on the input
+                        rules: [{required: i.required === undefined ? true : i.required}],
+                    })(getInputComponent(i))}
                 </Form.Item>
-            ]}
-        </Form>;
-    }
-}
+            )),
+
+            <Form.Item key="_submit" wrapperCol={FORM_BUTTON_COL}>
+                {onBack ? <Button icon="left" onClick={() => onBack()}>Back</Button> : null}
+                <Button type="primary" htmlType="submit" style={{float: "right"}}>
+                    Next <Icon type="right" />
+                </Button>
+            </Form.Item>
+        ]}
+    </Form>;
+};
 
 RunSetupInputForm.propTypes = {
     tree: PropTypes.array,
@@ -111,5 +101,5 @@ export default Form.create({
         Object.fromEntries(workflow.inputs.map(i => [i.id, Form.createFormField({...formValues[i.id]})])),
     onFieldsChange: ({onChange}, _, allFields) => {
         onChange({...allFields});
-    }
+    },
 })(RunSetupInputForm);
