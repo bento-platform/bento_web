59bd144f0837c6cefad33383fd43c9bc5183163f
David Lougheed <david.lougheed@gmail.com>
2023-03-13 16:03:48 -0400

    refact: move input display table into independent component

--- src/components/manager/ManagerIngestionContent.js
+++ src/components/manager/ManagerIngestionContent.js
@@ -20,6 +20,7 @@ import {EM_DASH} from "../../constants";
 import {withBasePath} from "../../utils/url";
 import {workflowsStateToPropsMixin} from "../../propTypes";
 import RunSetupWizard from "./RunSetupWizard";
+import RunSetupInputsTable from "./RunSetupInputsTable";
 
 
 const IngestWorkflowSelection = ({values, setValues, handleWorkflowClick}) => {
@@ -87,32 +88,7 @@ const IngestConfirmDisplay = ({selectedTable, selectedWorkflow, inputs, handleRu
                 </List>
             </Form.Item>
             <Form.Item label="Inputs">
-                <Table
-                    size="small"
-                    bordered={true}
-                    showHeader={false}
-                    pagination={false}
-                    columns={[
-                        {
-                            title: "ID",
-                            dataIndex: "id",
-                            render: iID => <span style={{fontWeight: "bold", marginRight: "0.5em"}}>{iID}</span>,
-                        },
-                        {
-                            title: "Value",
-                            dataIndex: "value",
-                            render: value =>
-                                value === undefined
-                                    ? EM_DASH
-                                    : (value instanceof Array
-                                            ? <ul>{value.map(v => <li key={v.toString()}>{v.toString()}</li>)}</ul>
-                                            : value.toString()
-                                    )
-                        }
-                    ]}
-                    rowKey="id"
-                    dataSource={selectedWorkflow.inputs.map(i => ({id: i.id, value: inputs[i.id]}))}
-                />
+                <RunSetupInputsTable selectedWorkflow={selectedWorkflow} inputs={inputs} />
             </Form.Item>
             <Form.Item wrapperCol={FORM_BUTTON_COL}>
                 {/* TODO: Back button like the last one */}
