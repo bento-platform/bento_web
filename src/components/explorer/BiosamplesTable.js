01095a3475479511a015c7f4489857b2d66ee732
Julian <ornatus30@gmail.com>
2023-03-29 15:12:32 -0400

    initial biosampleTable component

--- src/components/explorer/BiosamplesTable.js
+++ src/components/explorer/BiosamplesTable.js
@@ -22,27 +22,88 @@ const biosampleRender = (individual, record) => {
     );
 };
 
+const experimentsRender = (studiesType) => {
+    const experimentCount = studiesType.reduce((acc, study) => {
+        acc[study] = (acc[study] || 0) + 1;
+        return acc;
+    }, {});
+    const formattedExperiments = Object.entries(experimentCount).map(
+        ([study, count]) =>
+            `${count === studiesType.length ? "" : count + " "}${study}${
+                count > 1 && study.slice(-1) !== "s" ? "s" : ""
+            }`
+    );
+    return (
+        <>
+            {studiesType.every((s) => s !== null) ? (
+                <>
+                    {studiesType.length} Experiment{studiesType.length === 1 ? "" : "s"}:{" "}
+                    {formattedExperiments.join(", ")}
+                </>
+            ) : (
+                <>—</>
+            )}
+        </>
+    );
+};
+
+const experimentsSorter = (a, b) => {
+    const countExperiments = (studiesType) => {
+        return studiesType.filter((s) => s !== null).length;
+    };
+
+    return countExperiments(a.studies_type) - countExperiments(b.studies_type);
+};
+
+const sampledTissuesRender = (sampledTissues) => {
+    return sampledTissues.map((m) => m.label)[0];
+};
+
+const sampledTissuesSorter = (a, b) => {
+    if (a.sampled_tissues[0].label && b.sampled_tissues[0].label) {
+        return a.sampled_tissues[0].label.toString().localeCompare(b.sampled_tissues[0].label.toString());
+    }
+    return 0;
+};
+
+const availableExperimentsRender = (experimentsType) => {
+    if (experimentsType.every((s) => s !== null)) {
+        const experimentCount = experimentsType.reduce((acc, experiment) => {
+            acc[experiment] = (acc[experiment] || 0) + 1;
+            return acc;
+        }, {});
+        const formattedExperiments = Object.entries(experimentCount).map(
+            ([experiment, count]) => `${count} ${experiment}`
+        );
+        return formattedExperiments.join(", ");
+    } else {
+        return "—";
+    }
+};
+
+const availableExperimentsSorter = (a, b) => {
+    const highestValue = (formattedExperiments) => {
+        if (formattedExperiments !== "—") {
+            const counts = formattedExperiments.split(", ").map((experiment) => parseInt(experiment.split(" ")[0], 10));
+            return Math.max(...counts);
+        } else {
+            return -Infinity;
+        }
+    };
+
+    const highA = highestValue(availableExperimentsRender(a.experiments_type));
+    const highB = highestValue(availableExperimentsRender(b.experiments_type));
+
+    return highB - highA;
+};
+
 const SEARCH_RESULT_COLUMNS_BIOSAMPLE = [
     {
         title: "Biosample",
         dataIndex: "im_type",
-        //key: "im_type",
-        /* render: (im_type) => (
-            <>
-                <Link
-                    to={(location) => ({
-                        pathname: withBasePath(`data/explorer/biosamples/${im_type}/overview`),
-                        state: { backUrl: location.pathname },
-                    })}
-                >
-                    {im_type}
-                </Link>
-            </>
-        ), */
-        render: (im_type, record) => biosampleRender(im_type, record),
+        render: (bioType, record) => biosampleRender(bioType, record),
         sorter: (a, b) => a.im_type.localeCompare(b.im_type),
         defaultSortOrder: "ascend",
-        //key: (record) => record.im_type,
     },
     {
         title: "Individual",
@@ -54,62 +115,33 @@ const SEARCH_RESULT_COLUMNS_BIOSAMPLE = [
     {
         title: "Experiments",
         dataIndex: "studies_type",
-        render: (studies_type) => (
-            <>
-                {studies_type.every((s) => s !== null) ? (
-                    <>
-                        {studies_type.length} Experiment{studies_type.length === 1 ? "" : "s"}
-                        {studies_type.length ? ": " : ""}
-                        {studies_type.join(", ")}
-                    </>
-                ) : (
-                    <>—</>
-                )}
-            </>
-        ),
-        //render: (studies_type) => <>{studies_type}</>,
-        sorter: (a, b) => a.studies_type.length - b.studies_type.length,
+        render: experimentsRender,
+        sorter: experimentsSorter,
         sortDirections: ["descend", "ascend", "descend"],
     },
     {
         title: "Sampled Tissues",
         dataIndex: "sampled_tissues",
-        // [{id: 'OBI:0000880', label: 'RNA extract'}, {id: 'OBI:0000880', label: 'RNA extract'}]
-        // map to label
-        render: (sampled_tissues) => {
-            return sampled_tissues.map((m) => m.label)[0];
-        },
-        sorter: (a, b) => {
-            if (a.sampled_tissues[0].label && b.sampled_tissues[0].label) {
-                return a.sampled_tissues[0].label.toString().localeCompare(b.sampled_tissues[0].label.toString());
-            }
-            return 0;
-        },
-        defaultSortOrder: "ascend",
+        render: sampledTissuesRender,
+        sorter: sampledTissuesSorter,
+        sortDirections: ["descend", "ascend", "descend"],
     },
     {
         title: "Available Experiments",
         dataIndex: "experiments_type",
-        //  ['RNA-Seq', 'RNA-Seq']
-        render: (experiments_type) => {
-            if (experiments_type.every((s) => s !== null)) {
-                return experiments_type.map((other_thing) => other_thing).join(", ");
-            } else {
-                return "—";
-            }
-        },
-        //render: (other_thingx) => <>{other_thingx}</>,
-        /* sorter: (a, b) => a.other_thingx.localeCompare(b.other_thingx),
-        sortDirections: ["descend", "ascend", "descend"], */
-
-        sorter: (a, b) => a.experiments_type.length - b.experiments_type.length,
+        render: availableExperimentsRender,
+        sorter: availableExperimentsSorter,
         sortDirections: ["descend", "ascend", "descend"],
     },
 ];
 
 const BiosamplesTable = ({ data }) => {
     return (
-        <ExplorerSearchResultsTableComp dataStructure={SEARCH_RESULT_COLUMNS_BIOSAMPLE} type="biosample" data={data} />
+        <ExplorerSearchResultsTableComp
+            dataStructure={SEARCH_RESULT_COLUMNS_BIOSAMPLE}
+            data={data}
+            activeTab="biosamples"
+        />
     );
 };
 
