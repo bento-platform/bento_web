b2ccbf87ccd769ba572f0bc7ec0bb74f89bb9d47
v-rocheleau <victor.rocheleau@mcgill.ca>
2023-05-02 20:41:11 +0000

    Remove proj skeleton from ManagerExtraProperties

--- src/components/manager/projects/ManagerExtraProperties.js
+++ src/components/manager/projects/ManagerExtraProperties.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {Redirect, Route, Switch, useHistory} from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProjectSkeleton from "./ProjectSkeleton";
 import { Button, Empty, Layout, Menu, Typography } from "antd";
@@ -46,43 +46,39 @@ const ManagerExtraProperties = () => {
                             JSON schemas for the new project.
                         </Typography.Paragraph>
                         <Button type="primary" icon="left"
-                                onClick={navigateToProjects}>Project Tab</Button>
+                            onClick={navigateToProjects}>Project Tab</Button>
                     </Empty>
                 </Layout.Content>
             ) : <>
                 <Layout.Sider style={{ background: "white" }} width={256} breakpoint="lg" collapsedWidth={0}>
                     <div style={{ display: "flex", height: "100%", flexDirection: "column" }}>
                         <Menu style={{ flex: 1, paddingTop: "8px" }}
-                              mode="inline"
-                              selectedKeys={matchingMenuKeys(projectMenuItems)}>
+                            mode="inline"
+                            selectedKeys={matchingMenuKeys(projectMenuItems)}>
                             {projectMenuItems.map(renderMenuItem)}
                         </Menu>
                         <div style={{ borderRight: "1px solid #e8e8e8", padding: "24px" }}>
-                        <Button type="primary" icon="left"
+                            <Button type="primary" icon="left"
                                 onClick={navigateToProjects}>Project Tab</Button>
                         </div>
                     </div>
                 </Layout.Sider>
                 <Layout.Content style={LAYOUT_CONTENT_STYLE}>
-                        {projectMenuItems.length > 0 ? (
-                            <Switch>
-                                <Route path={withBasePath("admin/data/manager/schemas/:project")}
-                                       component={RoutedProjectJsonSchema} />
-                                <Redirect from={withBasePath("admin/data/manager/schemas")}
-                                          to={withBasePath(`admin/data/manager/schemas/${projects[0].identifier}`)} />
-                            </Switch>
-                        ) : (
-                            loadingAuthDependentData ? (
-                                <ProjectSkeleton />
-                            ) : (
-                                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}
-                                       description={
-                                            "Select a project from the menu on the" +
-                                            " left to manage its Extra Properties."
-                                        } />
-                            )
-                        )}
-                    </Layout.Content>
+                    {projectMenuItems.length > 0 ? (
+                        <Switch>
+                            <Route path={withBasePath("admin/data/manager/schemas/:project")}
+                                component={RoutedProjectJsonSchema} />
+                            <Redirect from={withBasePath("admin/data/manager/schemas")}
+                                to={withBasePath(`admin/data/manager/schemas/${projects[0].identifier}`)} />
+                        </Switch>
+                    ) : (
+                        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}
+                            description={
+                                "Select a project from the menu on the" +
+                                " left to manage its Extra Properties."
+                            } />
+                    )}
+                </Layout.Content>
             </>}
         </Layout>
     </>);
