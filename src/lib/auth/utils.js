4fcb48daa804b898b65f63fb3ecbf6e77374fdb4
David Lougheed <david.lougheed@gmail.com>
2023-05-18 08:40:05 -0400

    chore: clarify buildUrlEncodedData def

--- src/lib/auth/utils.js
+++ src/lib/auth/utils.js
@@ -1,6 +1,9 @@
 // noinspection JSCheckFunctionSignatures
 export const buildUrlEncodedData = obj =>
-    Object.entries(obj).reduce((params, [k, v]) => params.set(k, v.toString()) || params, new URLSearchParams());
+    Object.entries(obj).reduce((params, [k, v]) => {
+        params.set(k, v.toString());
+        return params;
+    }, new URLSearchParams());
 
 export const getIsAuthenticated = idTokenContents =>
     !!idTokenContents && Math.round((new Date()).getTime() / 1000) < idTokenContents.exp;
