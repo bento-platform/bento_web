08bd70d53c2d5f9918bba59e55ceae034a7f79b7
David Lougheed <david.lougheed@gmail.com>
2023-05-18 10:13:25 -0400

    chore: add note on session worker

--- src/session.worker.js
+++ src/session.worker.js
@@ -3,6 +3,9 @@
 // not active, so we need to move this pinging to a web worker to maintain the
 // session when the tab doesn't have focus.
 
+// When the main page receives the message, it will trigger the REFRESH_TOKENS.*
+// network action to get a new access & refresh token.
+
 setInterval(() => {
     self.postMessage(true);  // Send a window-inactive-resilient ping
 }, 120000);  // every 2 minutes
