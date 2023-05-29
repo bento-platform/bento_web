8fb8456b7536caba6a1b8a06e18578f8b38491ee
David Lougheed <david.lougheed@gmail.com>
2023-05-29 15:07:04 -0400

    refact: rewrite notification list as functional component

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -4,7 +4,7 @@ import {withBasePath} from "./url";
 export const NOTIFICATION_WES_RUN_COMPLETED = "wes_run_completed";
 export const NOTIFICATION_WES_RUN_FAILED = "wes_run_failed";
 
-export const navigateToWESRun = async (target, dispatch, history) => {
+export const navigateToWESRun = (target, history) => async dispatch => {
     await dispatch(hideNotificationDrawer());
     history.push(withBasePath(`admin/data/manager/runs/${target}/request`));
 };
