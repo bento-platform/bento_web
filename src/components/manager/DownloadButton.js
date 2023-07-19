e715d99b41fe339027e64345aba9946d2dc965df
David Lougheed <david.lougheed@gmail.com>
2023-07-19 13:51:23 -0400

    refact: factor out form-based download button component

---
+++ src/components/manager/DownloadButton.js
@@ -0,0 +1,40 @@
+import {useSelector} from "react-redux";
+import React, {useCallback} from "react";
+import {Button} from "antd";
+import PropTypes from "prop-types";
+
+const DownloadButton = ({disabled, uri, children}) => {
+    const {accessToken} = useSelector(state => state.auth);
+
+    const onClick = useCallback(() => {
+        if (!uri) return;
+
+        const form = document.createElement("form");
+        form.method = "post";
+        form.target = "_blank";
+        form.action = uri;
+        form.innerHTML = `<input type="hidden" name="token" value="${accessToken}" />`;
+        document.body.appendChild(form);
+        try {
+            form.submit();
+        } finally {
+            // Even if submit raises for some reason, we still need to clean this up; it has a token in it!
+            document.body.removeChild(form);
+        }
+    }, [uri, accessToken]);
+
+    return <Button key="download" icon="download" disabled={disabled} onClick={onClick}>{children}</Button>;
+};
+
+DownloadButton.defaultProps = {
+    disabled: false,
+    children: "Download",
+};
+
+DownloadButton.propTypes = {
+    disabled: PropTypes.bool,
+    uri: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+};
+
+export default DownloadButton;
