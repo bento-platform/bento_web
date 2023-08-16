8d9122ffafa3635a856e0951700d1b83e97a20f0
Julian <ornatus30@gmail.com>
2023-08-16 09:19:30 -0400

    Revert "Import history hooks"
    This reverts commit 06b1de422aa00acfff0be664563e81efd688a480.

--- src/components/explorer/ExplorerDatasetSearch.js
+++ src/components/explorer/ExplorerDatasetSearch.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {useHistory, useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { Typography, Tabs } from "antd";
 
