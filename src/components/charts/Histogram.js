e4f7a299d9ad6b0e6533c8522fb79c4a1719dbc4
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2023-08-17 11:49:02 -0400

    renamed transformData to transformAgeData

--- src/components/charts/Histogram.js
+++ src/components/charts/Histogram.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 import { BarChart } from "bento-charts";
 import ChartContainer from "./ChartContainer";
 
-const transformData = (data) => data && data.map(({ ageBin, count }) => ({ x: ageBin, y: count }));
+const transformAgeData = (data) => data && data.map(({ ageBin, count }) => ({ x: ageBin, y: count }));
 
 const Histogram = ({ title = "Histogram", data = [], chartHeight = 300, unit = "" }) => {
-    const transformedData = useMemo(() => transformData(data), [data]);
+    const transformedData = useMemo(() => transformAgeData(data), [data]);
 
     return (
         <ChartContainer title={title} empty={!Array.isArray(data) || !data.length}>
