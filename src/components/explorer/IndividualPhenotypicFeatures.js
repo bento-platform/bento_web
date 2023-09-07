d47ef536d3e55b39aa170e6b2fc469b1559f25e8
David Lougheed <david.lougheed@gmail.com>
2023-09-07 16:42:05 -0400

    feat(explorer): deduplicate phenotypic features in table

--- src/components/explorer/IndividualPhenotypicFeatures.js
+++ src/components/explorer/IndividualPhenotypicFeatures.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 
 import {Table} from "antd";
 
@@ -8,35 +8,57 @@ import {individualPropTypesShape} from "../../propTypes";
 const P_FEATURES_COLUMNS = [
     {
         title: "Type",
-        key: "type",
-        render: (_, individual) => <span>
-            <strong>{individual?.type?.label ?? EM_DASH} </strong>
-            {individual?.type?.id ?? EM_DASH}
+        dataIndex: "type",
+        render: (type) => <span>
+            <strong>{type?.label ?? EM_DASH} </strong>
+            {type?.id ?? EM_DASH}
         </span>,
     },
     {
         title: "Negated",
-        key: "negated",
-        render: (_, individual) => (individual?.negated ?? "false").toString(),
+        dataIndex: "negated",
+        render: (negated) => (negated ?? "false").toString(),
     },
     {
         title: "Extra Properties",
-        key: "extra_properties",
-        render: (_, individual) =>
-            (individual.hasOwnProperty("extra_properties") && Object.keys(individual.extra_properties).length)
-                ?  <div><pre>{JSON.stringify(individual.extra_properties, null, 2)}</pre></div>
+        dataIndex: "extra_properties",
+        render: (extraProperties) =>
+            (Object.keys(extraProperties ?? {}).length)
+                ?  <div><pre>{JSON.stringify(extraProperties ?? {}, null, 2)}</pre></div>
                 : EM_DASH,
     },
 
 ];
 
-const IndividualPhenotypicFeatures = ({individual}) =>
-    <Table bordered
-           size="middle"
-           pagination={{pageSize: 25}}
-           columns={P_FEATURES_COLUMNS}
-           rowKey="id"
-           dataSource={individual?.phenopackets.flatMap(p => (p.phenotypic_features ?? []))} />;
+const IndividualPhenotypicFeatures = ({individual}) => {
+    // TODO: this logic might be technically incorrect with different versions of the same resource
+    //  across multiple phenopackets
+    const phenotypicFeatures = useMemo(
+        () =>
+            Object.values(
+                Object.fromEntries(
+                    (individual?.phenopackets ?? [])
+                        .flatMap(p => (p.phenotypic_features ?? []))
+                        .map(pf => {
+                            const pfID = `${pf.type.id}:${pf.negated}`;
+                            return [pfID, {...pf, id: pfID}];
+                        })
+                )
+            ),
+        [individual],
+    );
+
+    return (
+        <Table
+            bordered
+            size="middle"
+            pagination={{pageSize: 25}}
+            columns={P_FEATURES_COLUMNS}
+            rowKey="id"
+            dataSource={phenotypicFeatures}
+        />
+    );
+}
 
 IndividualPhenotypicFeatures.propTypes = {
     individual: individualPropTypesShape,
