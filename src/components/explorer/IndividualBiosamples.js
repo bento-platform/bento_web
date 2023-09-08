90f735c34f765c439f5b160541a35ab36a11e084
David Lougheed <david.lougheed@gmail.com>
2023-09-08 10:52:48 -0400

    chore: use expandable table for individual biosamples

--- src/components/explorer/IndividualBiosamples.js
+++ src/components/explorer/IndividualBiosamples.js
@@ -1,20 +1,114 @@
-import React, {useCallback, useMemo} from "react";
+import React, { Fragment, useCallback, useMemo } from "react";
 import PropTypes from "prop-types";
-import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 
-import {Button, Descriptions, Tabs} from "antd";
+import { Button, Descriptions, Table } from "antd";
 
 import { EM_DASH } from "../../constants";
 import { renderOntologyTerm } from "./ontologies";
-import { individualPropTypesShape } from "../../propTypes";
+import {
+    biosamplePropTypesShape,
+    experimentPropTypesShape,
+    individualPropTypesShape,
+    ontologyShape,
+} from "../../propTypes";
 import JsonView from "./JsonView";
 import { useRouteMatch, useParams } from "react-router-dom/cjs/react-router-dom";
+
+import "./explorer.css";
 
 // TODO: Only show biosamples from the relevant dataset, if specified;
 //  highlight those found in search results, if specified
 
-const BiosampleTabs = ({ individual, handleBiosampleClick, handleExperimentClick }) => {
+const BiosampleProcedure = ({procedure}) => (
+    <div>
+        <strong>Code:</strong>{" "}
+        {renderOntologyTerm(procedure.code)}
+        {procedure.body_site ? (
+            <div>
+                <strong>Body Site:</strong>{" "}
+                {renderOntologyTerm(procedure.body_site)}
+            </div>
+        ) : null}
+    </div>
+);
+BiosampleProcedure.propTypes = {
+    procedure: PropTypes.shape({
+        code: ontologyShape.isRequired,
+        body_site: ontologyShape,
+    }),
+};
+
+const ExperimentsClickList = ({ experiments, handleExperimentClick }) => {
+    if (!experiments?.length) return EM_DASH;
+    return (
+        <>
+            {(experiments ?? []).map((e, i) => (
+                <Fragment key={i}>
+                    <Button onClick={() => handleExperimentClick(e.id)}>
+                        {e.experiment_type}
+                    </Button>
+                    {" "}
+                </Fragment>
+            ))}
+        </>
+    );
+};
+ExperimentsClickList.propTypes = {
+    experiments: PropTypes.arrayOf(experimentPropTypesShape),
+    handleExperimentClick: PropTypes.func,
+};
+
+const BiosampleDetail = ({ biosample, handleExperimentClick }) => {
+    return (
+        <Descriptions bordered={true} column={1} size="small" style={{maxWidth: 800}}>
+            <Descriptions.Item label="ID">
+                {biosample.id}
+            </Descriptions.Item>
+            <Descriptions.Item label="Sampled Tissue">
+                {renderOntologyTerm(biosample.sampled_tissue)}
+            </Descriptions.Item>
+            <Descriptions.Item label="Procedure">
+                <BiosampleProcedure procedure={biosample.procedure} />
+            </Descriptions.Item>
+            <Descriptions.Item label="Histological Diagnosis">
+                {renderOntologyTerm(biosample.histological_diagnosis)}
+            </Descriptions.Item>
+            <Descriptions.Item label="Ind. Age At Collection">
+                {biosample.individual_age_at_collection
+                    ? biosample.individual_age_at_collection.age ??
+                    `Between ${biosample.individual_age_at_collection.start.age}` +
+                    `and ${biosample.individual_age_at_collection.end.age}`
+                    : EM_DASH}
+            </Descriptions.Item>
+            <Descriptions.Item label="Extra Properties">
+                {biosample.hasOwnProperty("extra_properties") &&
+                    Object.keys(biosample.extra_properties).length ? (
+                        <JsonView inputJson={biosample.extra_properties} />
+                    ) : (
+                        EM_DASH
+                    )}
+            </Descriptions.Item>
+            <Descriptions.Item label="Available Experiments">
+                <ExperimentsClickList
+                    experiments={biosample.experiments}
+                    handleExperimentClick={handleExperimentClick}
+                />
+            </Descriptions.Item>
+        </Descriptions>
+    );
+};
+BiosampleDetail.propTypes = {
+    biosample: biosamplePropTypesShape,
+    handleExperimentClick: PropTypes.func,
+};
+
+const Biosamples = ({ individual, handleBiosampleClick, handleExperimentClick }) => {
     const { selectedBiosample } = useParams();
+    const selectedRowKeys = useMemo(
+        () => selectedBiosample ? [selectedBiosample] : [],
+        [selectedBiosample],
+    );
 
     const biosamples = useMemo(
         () => Object.values(
@@ -26,65 +120,56 @@ const BiosampleTabs = ({ individual, handleBiosampleClick, handleExperimentClick
         ),
         [individual]);
 
-    return <Tabs type="card" activeKey={selectedBiosample} onChange={handleBiosampleClick}>
-        {biosamples.map(biosample => (
-            <Tabs.TabPane key={biosample.id} tab={`Biosample ${biosample.id}`}>
-                <Descriptions bordered={true} column={1} size="small" style={{maxWidth: 800}}>
-                    <Descriptions.Item label="ID">
-                        {biosample.id}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Sampled Tissue">
-                        {renderOntologyTerm(biosample.sampled_tissue)}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Procedure">
-                        <div>
-                            <strong>Code:</strong>{" "}
-                            {renderOntologyTerm(biosample.procedure.code)}
-                            {biosample.procedure.body_site ? (
-                                <div>
-                                    <strong>Body Site:</strong>{" "}
-                                    {renderOntologyTerm(
-                                        biosample.procedure.body_site,
-                                    )}
-                                </div>
-                            ) : null}
-                        </div>
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Histological Diagnosis">
-                        {renderOntologyTerm(biosample.histological_diagnosis)}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Ind. Age At Collection">
-                        {biosample.individual_age_at_collection
-                            ? biosample.individual_age_at_collection.age ??
-                            `Between ${biosample.individual_age_at_collection.start.age}` +
-                            `and ${biosample.individual_age_at_collection.end.age}`
-                            : EM_DASH}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Extra Properties">
-                        {biosample.hasOwnProperty("extra_properties") &&
-                            Object.keys(biosample.extra_properties).length ? (
-                                <JsonView inputJson={biosample.extra_properties} />
-                            ) : (
-                                EM_DASH
-                            )}
-                    </Descriptions.Item>
-                    <Descriptions.Item label="Available Experiments">
-                        {(biosample.experiments ?? [])
-                            .map((e, i) => (
-                                <Button
-                                    key={i}
-                                    onClick={() => handleExperimentClick(e.id)}
-                                >
-                                    {e.experiment_type}
-                                </Button>
-                            ))}
-                    </Descriptions.Item>
-                </Descriptions>
-            </Tabs.TabPane>
-        ))}
-    </Tabs>;
+    const columns = useMemo(
+        () => [
+            {
+                title: "Biosample",
+                dataIndex: "id",
+            },
+            {
+                title: "Sampled Tissue",
+                dataIndex: "sampled_tissue",
+                render: tissue => renderOntologyTerm(tissue),
+            },
+            {
+                title: "Experiments",
+                key: "experiments",
+                render: (_, {experiments}) => (
+                    <ExperimentsClickList experiments={experiments} handleExperimentClick={handleExperimentClick} />
+                ),
+            },
+        ],
+        [handleExperimentClick],
+    );
+
+    const onExpand = useCallback(
+        (e, biosample) => {
+            handleBiosampleClick(e ? biosample.id : undefined);
+        },
+        [handleBiosampleClick],
+    );
+
+    const expandedRowRender = useCallback(
+        (biosample) => (
+            <BiosampleDetail biosample={biosample} handleExperimentClick={handleExperimentClick} />
+        ),
+        [handleExperimentClick],
+    );
+
+    return (
+        <Table
+            bordered={true}
+            pagination={false}
+            columns={columns}
+            onExpand={onExpand}
+            expandedRowKeys={selectedRowKeys}
+            expandedRowRender={expandedRowRender}
+            dataSource={biosamples}
+            rowKey="id"
+        />
+    );
 };
-BiosampleTabs.propTypes = {
+Biosamples.propTypes = {
     individual: individualPropTypesShape,
     handleBiosampleClick: PropTypes.func,
     handleExperimentClick: PropTypes.func,
@@ -94,16 +179,13 @@ const IndividualBiosamples = ({individual, experimentsUrl}) => {
     const history = useHistory();
     const match = useRouteMatch();
 
-    const biosampleIDs = useMemo(
-        () => Array.from(new Set(
-            (individual?.phenopackets ?? [])
-                .flatMap((p) => p.biosamples)
-                .map(b => b.id),
-        )),
-        [individual]);
-
     const handleBiosampleClick = useCallback((bID) => {
-        history.push(`${match.path}/${bID}`);
+        console.log(match);
+        if (!bID) {
+            history.replace(match.url);
+            return;
+        }
+        history.replace(`${match.url}/${bID}`);
     }, [history, match]);
 
     const handleExperimentClick = useCallback((eid) => {
@@ -114,13 +196,19 @@ const IndividualBiosamples = ({individual, experimentsUrl}) => {
     return (
         <Switch>
             <Route path={`${match.path}/:selectedBiosample`}>
-                <BiosampleTabs
+                <Biosamples
                     individual={individual}
                     handleBiosampleClick={handleBiosampleClick}
                     handleExperimentClick={handleExperimentClick}
                 />
             </Route>
-            <Redirect from={match.path} to={`${match.path}/${biosampleIDs[0]}`} />
+            <Route path={match.path} exact={true}>
+                <Biosamples
+                    individual={individual}
+                    handleBiosampleClick={handleBiosampleClick}
+                    handleExperimentClick={handleExperimentClick}
+                />
+            </Route>
         </Switch>
     );
 };
