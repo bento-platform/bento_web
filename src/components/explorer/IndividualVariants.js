6f5893f94f12e7ba686a9b06ec3fd041f520485b
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2023-10-18 12:53:57 -0400

    GenomicInterpretation component

--- src/components/explorer/IndividualVariants.js
+++ src/components/explorer/IndividualVariants.js
@@ -9,6 +9,7 @@ import { individualPropTypesShape } from "../../propTypes";
 import { setIgvPosition } from "../../modules/explorer/actions";
 import { useDeduplicatedIndividualBiosamples, useIndividualInterpretations, useIndividualVariantInterpretations } from "./utils";
 import "./explorer.css";
+import g from "file-saver";
 
 // TODO: Only show variants from the relevant dataset, if specified;
 //  highlight those found in search results, if specified
@@ -61,42 +62,46 @@ SampleVariants.propTypes = {
     tracksUrl: PropTypes.string,
 };
 
+const GenomicInterpretation = ({genomicInterpretation, tracksUrl}) => {
+    const isBiosampleRelated = genomicInterpretation?.extra_properties?.related_type ?? "";
+    console.log(isBiosampleRelated);
+    return (
+        <div style={sampleStyle}>
+            <Descriptions>
+                <Descriptions.Item label={""}>
+                    
+                </Descriptions.Item>
+                <Descriptions.Item></Descriptions.Item>
+                <Descriptions.Item></Descriptions.Item>
+            </Descriptions>
+        </div>
+    );
+};
+
+
+GenomicInterpretation.propTypes = {
+    genomicInterpretation: PropTypes.object,
+    tracksUrl: PropTypes.string,
+};
+
 const IndividualVariants = ({individual, tracksUrl}) => {
-    // const biosamples = useDeduplicatedIndividualBiosamples(individual);
     const interpretations = useIndividualVariantInterpretations(individual);
 
-    const variantsMapped = useMemo(
-        () => Object.fromEntries(
-                interpretations.map(i => i.diagnosis)
-                    .flatMap(d => d.genomic_interpretations)
-                    .filter(gi => gi.hasOwnProperty("variant_interpretation"))
-                    .map(gi => [gi.subject_or_biosample_id, gi])
-        ),
-        [interpretations]
-    );
-    console.log(variantsMapped);
-
-    // const variantsMapped = useMemo(
-    //     () => Object.fromEntries(biosamples.map((biosample) => [
-    //         biosample.id,
-    //         (biosample.variants ?? []).map((v) => ({
-    //             id: v.hgvsAllele?.id,
-    //             hgvs: v.hgvsAllele?.hgvs,
-    //             geneContext: v.extra_properties?.gene_context ?? "",
-    //         })),
-    //     ])),
-    //     [biosamples],
-    // );
+    const genomicInterpretations = interpretations.map(i => i.diagnosis)
+            .flatMap(d => d.genomic_interpretations)
+            .filter(gi => gi.hasOwnProperty("variant_interpretation"))
+    console.log(genomicInterpretations);
 
     return (
       <div className="variantDescriptions">
-          {biosamples.length ? <Descriptions layout="horizontal" bordered={true} column={1} size="small">
-              {biosamples.map(({id}) => (
-                  <Descriptions.Item key={id} label={`Biosample ${id}`}>
-                      <SampleVariants variantsMapped={variantsMapped} biosampleID={id} tracksUrl={tracksUrl} />
-                  </Descriptions.Item>
-              ))}
-          </Descriptions> : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />}
+            {genomicInterpretations.length ?
+                (<Descriptions layout="horizontal" bordered={true} column={1} size="small">
+                    {genomicInterpretations.map(gi => 
+                        <GenomicInterpretation genomicInterpretation={gi} tracksUrl={tracksUrl} key={gi.id}/>
+                    )}
+                </Descriptions>) :
+                (<Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>)
+            }
       </div>
     );
 };
