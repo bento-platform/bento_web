1a7b9d6ae63c83832a34cf6c87372a378da6ecd5
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2023-10-25 20:58:09 +0000

    individual interpretations table refactor

--- src/components/explorer/IndividualInterpretations.js
+++ src/components/explorer/IndividualInterpretations.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import PropTypes from "prop-types";
 
 import { Link } from "react-router-dom";
-import { Button, Descriptions, Empty } from "antd";
+import { Button, Descriptions, Empty, Icon, Table, Typography } from "antd";
 
 import { useIndividualResources, useIndividualInterpretations } from "./utils";
 import "./explorer.css";
 import { individualPropTypesShape, resourcePropTypesShape } from "../../propTypes";
 import OntologyTerm from "./OntologyTerm";
+import { Route, Switch, useHistory, useParams, useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
 
 const createdAndUpdatedDescriptions = (data) => {
     const descriptions = [];
@@ -76,67 +77,148 @@ GenomicInterpretation.propTypes = {
 };
 
 
-const Diagnosis = ({diagnosis, resourcesTuple, variantsUrl, genesUrl}) => {
-    const genomicInterpretations = diagnosis?.genomic_interpretations ?? [];
-    return (
-        <Descriptions layout="horizontal" bordered={true} column={1} size="small">
-            <Descriptions.Item label="Disease">
-                <OntologyTerm resourcesTuple={resourcesTuple} term={diagnosis.disease_ontology}/>
-            </Descriptions.Item>
-            {genomicInterpretations.length ? (
-                <Descriptions.Item label="Genomic Interpretations">
-                    {genomicInterpretations.map(gi =>
-                        <GenomicInterpretation key={gi.id}
-                                               genomicInterpretation={gi}
-                                               variantsUrl={variantsUrl}/>,
-                    )}
-                </Descriptions.Item>
-            ) : null}
-        </Descriptions>
-    );
-};
-Diagnosis.propTypes = {
-    diagnosis: PropTypes.object,
-    resourcesTuple: resourcePropTypesShape,
-    variantsUrl: PropTypes.string,
-    genesUrl: PropTypes.string,
-};
+const INTERPRETATIONS_COLUMNS = [
+    {
+        title: "ID",
+        dataIndex: "id",
+    },
+    {
+        title: "Created",
+        dataIndex: "created",
+    },
+    {
+        title: "Updated",
+        dataIndex: "updated",
+    },
+    {
+        title: "Progress Status",
+        dataIndex: "progress_status",
+    },
+    {
+        title: "Summary",
+        dataIndex: "summary",
+    },
+];
 
-const Interpretation = ({interpretation, resourcesTuple, variantsUrl, genesUrl}) => {
-    return (<Descriptions layout="horizontal" bordered={true} column={1} size="small">
-        <Descriptions.Item label="ID">{interpretation.id}</Descriptions.Item>
-        {createdAndUpdatedDescriptions(interpretation)}
-        <Descriptions.Item label="Progress Status">{interpretation.progress_status}</Descriptions.Item>
-        <Descriptions.Item label="Summary">{interpretation.summary}</Descriptions.Item>
-        <Descriptions.Item label="Diagnosis">
-            <Diagnosis diagnosis={interpretation.diagnosis} resourcesTuple={resourcesTuple} variantsUrl={variantsUrl}/>
-        </Descriptions.Item>
-    </Descriptions>);
+const GENOMIC_INTERPRETATION_COLUMNS = [
+    {
+        title: "Subject or Biosample ID",
+        dataIndex: "subject_or_biosample_id",
+    },
+    {
+        title: "Interpretation Status",
+        dataIndex: "interpretation_status",
+    },
+    // TODO: use a Table here or Descriptions?
+]
+
+const InterpretationDetail = ({interpretation, resourcesTuple}) => {
+    const { diagnosis } = interpretation;
+
+    const sortedGenomicInterpretations = useMemo(
+        () => (diagnosis?.genomic_interpretations ?? [])
+                .sort((g1, g2) => g1.subject_or_biosample_id > g2.subject_or_biosample_id ? 1 : -1),
+        [diagnosis],
+    );
+
+    return (<div className="experiment_and_results">
+        <Typography.Title level={4}><Icon type="medicine-box" /> Diagnosis</Typography.Title>
+        {diagnosis ? <Descriptions layout="horizontal" bordered column={2} size="small">
+            <Descriptions.Item label="Disease">
+                <OntologyTerm
+                    resourcesTuple={resourcesTuple}
+                    term={diagnosis.disease_ontology}
+                />
+            </Descriptions.Item>
+        </Descriptions> : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>}
+
+        <Typography.Title level={4}><Icon type="experiment" /> Genomic Interpretations</Typography.Title>
+        {sortedGenomicInterpretations.length ? <Table
+            bordered={true}
+            size="small"
+            pagination={false}
+            columns={GENOMIC_INTERPRETATION_COLUMNS}
+            rowKey="subject_or_biosample_id"
+            dataSource={sortedGenomicInterpretations}
+        /> : null}
+    </div>)
 };
-Interpretation.propTypes = {
+InterpretationDetail.propTypes = {
     interpretation: PropTypes.object,
     resourcesTuple: PropTypes.array,
+};
+
+const Interpretations = ({individual, variantsUrl, genesUrl, handleInterpretationClick}) => {
+    const { selectedInterpretation } = useParams();
+    const selectedRowKeys = useMemo(
+        () => selectedInterpretation ? [selectedInterpretation] : [],
+        [selectedInterpretation],
+    );
+
+    const interpretationsData = useIndividualInterpretations(individual);
+    const resourcesTuple = useIndividualResources(individual);
+
+    const onExpand = useCallback(
+        (e, interpretation) => {
+            handleInterpretationClick(e ? interpretation.id : undefined);
+        },
+        [handleInterpretationClick],
+    );
+
+    const interpretationRowRender = useCallback(
+        (interpretation) => (
+            <InterpretationDetail interpretation={interpretation} resourcesTuple={resourcesTuple} />
+        ),
+        [resourcesTuple]
+    )
+    return (
+        <Table
+            bordered={true}
+            pagination={false}
+            size="middle"
+            columns={INTERPRETATIONS_COLUMNS}
+            onExpand={onExpand}
+            expandedRowKeys={selectedRowKeys}
+            expandedRowRender={interpretationRowRender}
+            dataSource={interpretationsData}
+            rowKey="id"
+        />
+    );
+};
+Interpretations.propTypes = {
+    individual: individualPropTypesShape,
     variantsUrl: PropTypes.string,
     genesUrl: PropTypes.string,
+    handleInterpretationClick: PropTypes.func,
 };
 
 const IndividualInterpretations = ({individual, variantsUrl, genesUrl}) => {
-    const interpretations = useIndividualInterpretations(individual);
-    const resourcesTuple = useIndividualResources(individual);
-    return (
-        <div>
-            {interpretations.length ?
-                (<Descriptions layout="horizontal" bordered={true} column={1} size="small" title="Interpretations">
-                    {interpretations.map(interp => <Descriptions.Item label={interp.id} key={interp.id}>
-                        <Interpretation interpretation={interp}
-                                        resourcesTuple={resourcesTuple}
-                                        variantsUrl={variantsUrl}
-                                        genesUrl={genesUrl}/>
-                    </Descriptions.Item>)}
-                </Descriptions>)
-                : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>}
-        </div>
+    const history = useHistory();
+    const match = useRouteMatch();
+
+    const handleInterpretationClick = useCallback((interpID) => {
+        if (!interpID) {
+            history.replace(match.url);
+            return;
+        }
+        history.replace(`${match.url}/${interpID}`)
+    }, [history, match]);
+
+    const interpretationsNode = (
+        <Interpretations
+            individual={individual}
+            variantsUrl={variantsUrl}
+            genesUrl={genesUrl}
+            handleInterpretationClick={handleInterpretationClick}
+        />
     );
+
+    return (
+        <Switch>
+            <Route path={`${match.path}/:selectedInterpretation`}>{interpretationsNode}</Route>
+            <Route path={match.path} exact={true}>{interpretationsNode}</Route>
+        </Switch>
+    )
 };
 
 IndividualInterpretations.propTypes = {
