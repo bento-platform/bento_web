89def62fa0eb41affeeef3a8b48727c5134d94a3
Julian <ornatus30@gmail.com>
2023-11-20 17:00:18 -0500

    Modify the statistic title in variants overview

--- src/components/overview/VariantsSummary.js
+++ src/components/overview/VariantsSummary.js
@@ -22,7 +22,10 @@ const VariantsSummary = ( ) => {
             <Row style={{ marginBottom: "24px" }} gutter={[0, 16]}>
                 <Col xl={2} lg={3} md={4} sm={5} xs={6}>
                     <Spin spinning={fetchingVariantsOverview}>
-                        <Statistic title="VCF Files" prefix={<Icon type="file" />} value={numVarFilesFromSampleIds} />
+                        <Statistic
+                            title="Samples"
+                            prefix={<Icon type="file" />}
+                            value={numVarFilesFromSampleIds} />
                     </Spin>
                 </Col>
             </Row>
