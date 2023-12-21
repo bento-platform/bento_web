19346ec4e115e7bb4bf10fbee182983e82159098
David Lougheed <david.lougheed@gmail.com>
2023-12-21 15:11:13 -0500

    fix: wait for reference service to load before querying reference genomes=

--- src/modules/reference/hooks.js
+++ src/modules/reference/hooks.js
@@ -5,8 +5,11 @@ import { fetchReferenceGenomesIfNeeded } from "./actions";
 
 export const useReferenceGenomes = () => {
     const dispatch = useDispatch();
+    const referenceService = useSelector((state) => state.services.itemsByKind.reference);
     useEffect(() => {
-        dispatch(fetchReferenceGenomesIfNeeded());
-    }, [dispatch]);
+        if (referenceService) {
+            dispatch(fetchReferenceGenomesIfNeeded());
+        }
+    }, [dispatch, referenceService]);
     return useSelector((state) => state.referenceGenomes);
 };
