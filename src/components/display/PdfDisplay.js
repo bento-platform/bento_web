da1030a83a84525a6f121e98d3b43be63ce370ea
v-rocheleau <victor.rocheleau@mcgill.ca>
2024-01-23 18:34:54 +0000

    fix: replace missing auth header hook with bento-auth-js

--- src/components/display/PdfDisplay.js
+++ src/components/display/PdfDisplay.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import { Document, Page, pdfjs } from "react-pdf";
 import { Button, Icon } from "antd";
 
-import { useAuthorizationHeader } from "../../lib/auth/utils";
+import { useAuthorizationHeader } from "bento-auth-js";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.js", import.meta.url).toString();
 
