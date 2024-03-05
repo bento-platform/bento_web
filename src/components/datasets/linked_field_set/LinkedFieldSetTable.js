d83430dc4d0956dc14b938aac665fe7655d110c3
David Lougheed <david.lougheed@gmail.com>
2024-03-05 10:20:56 -0500

    refact(manager): func LinkedFieldSetTable + no pagination

--- src/components/datasets/linked_field_set/LinkedFieldSetTable.js
+++ src/components/datasets/linked_field_set/LinkedFieldSetTable.js
@@ -1,28 +1,32 @@
-import React, {Component} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import {Table} from "antd";
 
-import {linkedFieldSetPropTypesShape} from "../../../propTypes";
+import {linkedFieldSetPropTypesShape} from "@/propTypes";
 
 const COLUMNS = [
     {dataIndex: "dataType", title: "Data Type"},
     {dataIndex: "field", title: "Field", render: f => <span style={{fontFamily: "monospace"}}>{f.join(".")}</span>},
 ];
 
-class LinkedFieldSetTable extends Component {
-    render() {
-        const data = Object.entries(this.props.linkedFieldSet.fields)
-            .map(([dataType, field]) => ({dataType, field}))
-            .sort((a, b) =>
-                a.dataType.localeCompare(b.dataType));
-        return <Table columns={COLUMNS}
-                      dataSource={data}
-                      rowKey="dataType"
-                      size={this.props.inModal ? "small" : "middle"}
-                      bordered={true} />;
-    }
-}
+const LinkedFieldSetTable = ({ linkedFieldSet, inModal }) => {
+    const data = Object.entries(linkedFieldSet.fields)
+        .map(([dataType, field]) => ({dataType, field}))
+        .sort((a, b) =>
+            a.dataType.localeCompare(b.dataType));
+
+    return (
+        <Table
+            columns={COLUMNS}
+            dataSource={data}
+            rowKey="dataType"
+            pagination={false}
+            size={inModal ? "small" : "middle"}
+            bordered={true}
+        />
+    );
+};
 
 LinkedFieldSetTable.propTypes = {
     linkedFieldSet: linkedFieldSetPropTypesShape,
