711aec3a00944561f8ecde148635edface570d4b
David Lougheed <david.lougheed@gmail.com>
2024-04-15 13:35:30 -0400

    feat(manager): permissions / ownership display in DRS detail

--- src/components/manager/drs/ManagerDRSContent.js
+++ src/components/manager/drs/ManagerDRSContent.js
@@ -1,41 +1,128 @@
 import React, { useCallback, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
+
 import { filesize } from "filesize";
 import { throttle } from "lodash";
 import { useAuthorizationHeader } from "bento-auth-js";
 
 import { Layout, Input, Table, Descriptions, message } from "antd";
 
+import { EM_DASH } from "@/constants";
 import { LAYOUT_CONTENT_STYLE } from "@/styles/layoutContent";
 
-import DownloadButton from "../../DownloadButton";
-import MonospaceText from "@/components/MonospaceText";
+import BooleanYesNo from "@/components/common/BooleanYesNo";
+import DownloadButton from "@/components/common/DownloadButton";
+import MonospaceText from "@/components/common/MonospaceText";
+import DatasetTitleDisplay from "../DatasetTitleDisplay";
+import ProjectTitleDisplay from "../ProjectTitleDisplay";
+
+const TABLE_NESTED_DESCRIPTIONS_STYLE = {
+    backgroundColor: "white",
+    borderRadius: 3,
+    maxWidth: 1400,
+};
+
+const DRSObjectDetail = ({ drsObject }) => {
+    const { id, description, checksums, access_methods: accessMethods, size, bento } = drsObject;
+    return (
+        <div style={TABLE_NESTED_DESCRIPTIONS_STYLE} className="table-nested-ant-descriptions">
+            <Descriptions bordered={true}>
+                <Descriptions.Item label="ID" span={2}>
+                    <MonospaceText>{id}</MonospaceText>
+                </Descriptions.Item>
+                <Descriptions.Item label="Size" span={1}>
+                    {filesize(size)}
+                </Descriptions.Item>
+                {description && (
+                    <Descriptions.Item label="Description" span={3}>
+                        {description}
+                    </Descriptions.Item>
+                )}
+                <Descriptions.Item label="Checksums" span={3}>
+                    {checksums.map(({ type, checksum }) => (
+                        <div
+                            key={type}
+                            style={{ display: "flex", gap: "0.8em", alignItems: "baseline" }}
+                        >
+                            <span style={{ fontWeight: "bold" }}>{type.toLocaleUpperCase()}:</span>
+                            <MonospaceText>{checksum}</MonospaceText>
+                        </div>
+                    ))}
+                </Descriptions.Item>
+                <Descriptions.Item label="Access Methods" span={2}>
+                    {accessMethods.map(({ type, access_url: url }, i) => (
+                        <div key={i} style={{ display: "flex", gap: "0.8em", alignItems: "baseline" }}>
+                            <span style={{ fontWeight: "bold" }}>{type.toLocaleUpperCase()}:</span>
+                            <MonospaceText>
+                                {["http", "https"].includes(type) ? (  // "http" for back-compat
+                                    <a href={url?.url} target="_blank" rel="noreferrer">
+                                        {url?.url}
+                                    </a>
+                                ) : (
+                                    url?.url
+                                )}
+                            </MonospaceText>
+                        </div>
+                    ))}
+                </Descriptions.Item>
+                <Descriptions.Item label="Public" span={1}>
+                    <BooleanYesNo value={bento?.public} />
+                </Descriptions.Item>
+                <Descriptions.Item label="Project" span={1}>
+                    <ProjectTitleDisplay projectID={bento?.project_id} link={true} />
+                </Descriptions.Item>
+                <Descriptions.Item label="Dataset" span={1}>
+                    <DatasetTitleDisplay datasetID={bento?.dataset_id} link={true} />
+                </Descriptions.Item>
+                <Descriptions.Item label="Data Type" span={1}>
+                    {bento?.data_type ?? EM_DASH}
+                </Descriptions.Item>
+            </Descriptions>
+        </div>
+    );
+};
+DRSObjectDetail.propTypes = {
+    drsObject: PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        size: PropTypes.number,
+        checksums: PropTypes.arrayOf(PropTypes.shape({
+            type: PropTypes.string.isRequired,
+            checksum: PropTypes.string.isRequired,
+        })),
+        access_methods: PropTypes.arrayOf(PropTypes.shape({
+            type: PropTypes.string.isRequired,
+            access_url: PropTypes.shape({ url: PropTypes.string }),
+        })),
+        bento: PropTypes.shape({
+            project_id: PropTypes.string,
+            dataset_id: PropTypes.string,
+            data_type: PropTypes.string,
+            public: PropTypes.bool,
+        }),
+    }),
+};
 
 const SEARCH_CONTAINER_STYLE = {
     maxWidth: 800,
     marginBottom: "1rem",
 };
 
-const TABLE_NESTED_DESCRIPTIONS_STYLE = {
-    backgroundColor: "white",
-    borderRadius: 3,
-};
-
 const DRS_COLUMNS = [
     {
         title: "URI",
         dataIndex: "self_uri",
-        key: "self_uri",
+        render: (selfUri) => <MonospaceText>{selfUri}</MonospaceText>,
     },
     {
         title: "Name",
         dataIndex: "name",
-        key: "name",
     },
     {
         title: "Size",
         dataIndex: "size",
-        key: "size",
         render: (size) => filesize(size),
     },
     {
@@ -49,8 +136,17 @@ const DRS_COLUMNS = [
     },
 ];
 
+// noinspection JSUnusedGlobalSymbols
+const DRS_TABLE_EXPANDABLE = {
+    expandedRowRender: (drsObject) => <DRSObjectDetail drsObject={drsObject} />,
+};
+
+// noinspection JSCheckFunctionSignatures
+const buildDRSSearchURL = (drsURL, q) =>
+    `${drsURL}/search?` + new URLSearchParams({ q, with_bento_properties: true });
+
 const ManagerDRSContent = () => {
-    const drsUrl = useSelector((state) => state.services.drsService?.url);
+    const drsURL = useSelector((state) => state.services.drsService?.url);
 
     const authHeader = useAuthorizationHeader();
 
@@ -61,7 +157,7 @@ const ManagerDRSContent = () => {
     const onSearch = useCallback(
         throttle(
             (v) => {
-                if (!drsUrl) return;
+                if (!drsURL) return;
 
                 // Extract value from either the native HTML event or the AntDesign event
                 const sv = (v.target?.value ?? v ?? "").trim();
@@ -73,7 +169,7 @@ const ManagerDRSContent = () => {
 
                 setLoading(true);
 
-                fetch(`${drsUrl}/search?` + new URLSearchParams({ q: sv }), { method: "GET", headers: authHeader })
+                fetch(buildDRSSearchURL(drsURL, sv), { method: "GET", headers: authHeader })
                     .then((r) => Promise.all([Promise.resolve(r.ok), r.json()]))
                     .then(([ok, data]) => {
                         if (ok) {
@@ -94,7 +190,7 @@ const ManagerDRSContent = () => {
             250,
             { leading: true, trailing: true },
         ),
-        [drsUrl, authHeader],
+        [drsURL, authHeader],
     );
 
     const tableLocale = useMemo(
@@ -110,8 +206,8 @@ const ManagerDRSContent = () => {
                 <div style={SEARCH_CONTAINER_STYLE}>
                     <Input.Search
                         placeholder="Search DRS objects by name."
-                        loading={loading || !drsUrl}
-                        disabled={!drsUrl}
+                        loading={loading || !drsURL}
+                        disabled={!drsURL}
                         onChange={onSearch}
                         onSearch={onSearch}
                         size="large"
@@ -124,50 +220,7 @@ const ManagerDRSContent = () => {
                     loading={loading}
                     bordered={true}
                     size="middle"
-                    expandedRowRender={({ id, description, checksums, access_methods: accessMethods, size }) => (
-                        <div style={TABLE_NESTED_DESCRIPTIONS_STYLE} className="table-nested-ant-descriptions">
-                            <Descriptions bordered={true}>
-                                <Descriptions.Item label="ID" span={2}>
-                                    <MonospaceText>{id}</MonospaceText>
-                                </Descriptions.Item>
-                                <Descriptions.Item label="Size" span={1}>
-                                    {filesize(size)}
-                                </Descriptions.Item>
-                                <Descriptions.Item label="Checksums" span={3}>
-                                    {checksums.map(({ type, checksum }) => (
-                                        <div
-                                            key={type}
-                                            style={{ display: "flex", gap: "0.8em", alignItems: "baseline" }}
-                                        >
-                                            <span style={{ fontWeight: "bold" }}>{type.toLocaleUpperCase()}:</span>
-                                            <MonospaceText>{checksum}</MonospaceText>
-                                        </div>
-                                    ))}
-                                </Descriptions.Item>
-                                <Descriptions.Item label="Access Methods" span={3}>
-                                    {accessMethods.map(({ type, access_url: url }, i) => (
-                                        <div key={i} style={{ display: "flex", gap: "0.8em", alignItems: "baseline" }}>
-                                            <span style={{ fontWeight: "bold" }}>{type.toLocaleUpperCase()}:</span>
-                                            <MonospaceText>
-                                                {["http", "https"].includes(type) ? (  // "http" for back-compat
-                                                    <a href={url?.url} target="_blank" rel="noreferrer">
-                                                        {url?.url}
-                                                    </a>
-                                                ) : (
-                                                    url?.url
-                                                )}
-                                            </MonospaceText>
-                                        </div>
-                                    ))}
-                                </Descriptions.Item>
-                                {description && (
-                                    <Descriptions.Item label="Description" span={3}>
-                                        {description}
-                                    </Descriptions.Item>
-                                )}
-                            </Descriptions>
-                        </div>
-                    )}
+                    expandable={DRS_TABLE_EXPANDABLE}
                     locale={tableLocale}
                 />
             </Layout.Content>
