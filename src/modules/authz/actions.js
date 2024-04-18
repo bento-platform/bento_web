8c588dfe3fb481e677f3ff0963c0f61c66ac70cb
David Lougheed <david.lougheed@gmail.com>
2024-04-18 14:43:00 -0400

    chore(manager): fetch grants/groups check for authz service / isFetching / data

--- src/modules/authz/actions.js
+++ src/modules/authz/actions.js
@@ -1,22 +1,26 @@
-import { createNetworkActionTypes, networkAction } from "../../utils/actions";
+import { createNetworkActionTypes, networkAction } from "@/utils/actions";
 
 export const FETCH_GRANTS = createNetworkActionTypes("FETCH_GRANTS");
-const fetchGrants = networkAction(() => (_dispatch, getState) => ({
+const _fetchGrants = networkAction(() => (_dispatch, getState) => ({
     types: FETCH_GRANTS,
     url: `${getState().services.itemsByKind.authorization.url}/grants/`,
 }));
-export const fetchGrantsIfNeeded = () => (dispatch, getState) => {
-    if (getState().grants.isFetching || getState().grants.data.length) return;
-    return dispatch(fetchGrants());
+export const fetchGrants = () => (dispatch, getState) => {
+    if (!getState().services.itemsByKind.authorization
+        || getState().grants.isFetching
+        || getState().grants.data.length) return Promise.resolve();
+    return dispatch(_fetchGrants());
 };
 
 
 export const FETCH_GROUPS = createNetworkActionTypes("FETCH_GROUPS");
-const fetchGroups = networkAction(() => (_dispatch, getState) => ({
+const _fetchGroups = networkAction(() => (_dispatch, getState) => ({
     types: FETCH_GROUPS,
     url: `${getState().services.itemsByKind.authorization.url}/groups/`,
 }));
-export const fetchGroupsIfNeeded = () => (dispatch, getState) => {
-    if (getState().groups.isFetching || getState().groups.data.length) return;
-    return dispatch(fetchGroups());
+export const fetchGroups = () => (dispatch, getState) => {
+    if (!getState().services.itemsByKind.authorization
+        || getState().groups.isFetching
+        || getState().groups.data.length) return Promise.resolve();
+    return dispatch(_fetchGroups());
 };
