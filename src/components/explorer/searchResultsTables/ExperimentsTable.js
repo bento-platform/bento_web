54a7a6fef96ed75951f50dda508ca94d8cc03c80
Julian <ornatus30@gmail.com>
2024-06-05 11:01:14 -0400

    use renamed hook

--- src/components/explorer/searchResultsTables/ExperimentsTable.js
+++ src/components/explorer/searchResultsTables/ExperimentsTable.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
 
-import { useSortedColumns, useDynamicFilterOptions } from "../hooks/explorerHooks";
+import { useSortedColumns, useDynamicTableFilterOptions } from "../hooks/explorerHooks";
 import { explorerIndividualUrl } from "../utils";
 
 import BiosampleIDCell from "./BiosampleIDCell";
@@ -37,42 +37,42 @@ const ExperimentsTable = ({ data, datasetID }) => {
 
     const columns = useMemo(() => {
         const baseColumns = [
-    {
-        title: "Experiment",
-        dataIndex: "experimentId",
-        render: (experimentId, record) => <ExperimentRender experimentId={experimentId} {...record} />,
-        sorter: (a, b) => a.experimentId.localeCompare(b.experimentId),
-        defaultSortOrder: "ascend",
-    },
-    {
-        title: "Individual",
-        dataIndex: "individual",
-        render: (individual) => <>{individual.id}</>,
-        sorter: (a, b) => a.individual.id.localeCompare(b.individual.id),
-        sortDirections: ["descend", "ascend", "descend"],
-    },
-    {
-        title: "Biosample",
-        dataIndex: "biosampleId",
-        render: (biosampleId, record) => (
-            <BiosampleIDCell biosample={biosampleId} individualID={record.individual.id} />
-        ),
-        sorter: (a, b) => a.biosampleId.localeCompare(b.biosampleId),
-        sortDirections: ["descend", "ascend", "descend"],
-    },
-    {
-        title: "Study Type",
-        dataIndex: "studyType",
-        render: (studyType) => <>{studyType}</>,
-        sorter: (a, b) => a.studyType.localeCompare(b.studyType),
-        sortDirections: ["descend", "ascend", "descend"],
-    },
-    {
-        title: "Experiment Type",
-        dataIndex: "experimentType",
-        render: (expType) => <>{expType}</>,
-        sorter: (a, b) => a.experimentType.localeCompare(b.experimentType),
-        sortDirections: ["descend", "ascend", "descend"],
+            {
+                title: "Experiment",
+                dataIndex: "experimentId",
+                render: (experimentId, record) => <ExperimentRender experimentId={experimentId} {...record} />,
+                sorter: (a, b) => a.experimentId.localeCompare(b.experimentId),
+                defaultSortOrder: "ascend",
+            },
+            {
+                title: "Individual",
+                dataIndex: "individual",
+                render: (individual) => <>{individual.id}</>,
+                sorter: (a, b) => a.individual.id.localeCompare(b.individual.id),
+                sortDirections: ["descend", "ascend", "descend"],
+            },
+            {
+                title: "Biosample",
+                dataIndex: "biosampleId",
+                render: (biosampleId, record) => (
+                    <BiosampleIDCell biosample={biosampleId} individualID={record.individual.id} />
+                ),
+                sorter: (a, b) => a.biosampleId.localeCompare(b.biosampleId),
+                sortDirections: ["descend", "ascend", "descend"],
+            },
+            {
+                title: "Study Type",
+                dataIndex: "studyType",
+                render: (studyType) => <>{studyType}</>,
+                sorter: (a, b) => a.studyType.localeCompare(b.studyType),
+                sortDirections: ["descend", "ascend", "descend"],
+            },
+            {
+                title: "Experiment Type",
+                dataIndex: "experimentType",
+                render: (expType) => <>{expType}</>,
+                sorter: (a, b) => a.experimentType.localeCompare(b.experimentType),
+                sortDirections: ["descend", "ascend", "descend"],
                 filters: experimentTypeFilters,
                 onFilter: (value, record) => record.experimentType === value,
             },
