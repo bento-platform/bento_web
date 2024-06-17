bba3ea4316c13be7ba5463fe7ff86cad0e6158ee
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2024-06-17 15:56:18 -0400

    Added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true,
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended", // Add this line
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "process": "readonly",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+        process: "readonly",
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true,
-            "modules": true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            modules: true,
         },
-        "sourceType": "module",
+        sourceType: "module",
     },
-    "plugins": [
+    plugins: [
         "react",
         "@typescript-eslint",
+        "prettier", // Add this line
     ],
-    "rules": {
-        "indent": [
+    rules: {
+        indent: [
             "error",
             4,
             {
-                "SwitchCase": 1,
-                "ignoredNodes": [
+                SwitchCase: 1,
+                ignoredNodes: [
                     "JSXElement",
                     "JSXElement > *",
                     "JSXAttribute",
@@ -46,52 +48,48 @@ module.exports = {
                     "JSXEmptyExpression",
                     "JSXSpreadChild",
                 ],
-                "ignoreComments": true,
-            }
+                ignoreComments: true,
+            },
         ],
         "react/jsx-indent-props": ["error", "first"],
-
-        // Don't need React for JSX anymore with new JSX transform
         "react/react-in-jsx-scope": "off",
         "react/jsx-uses-react": "off",
-
-        // Prevent some legacy HTML tags
-        "react/forbid-elements": ["error", {"forbid": ["b", "font"]}],
-
+        "react/forbid-elements": ["error", { forbid: ["b", "font"] }],
         "no-prototype-builtins": "off",
         "react/display-name": "off",
-
         "linebreak-style": ["error", "unix"],
-        "react/prop-types": ["error", {"ignore": ["form", "match", "history"]}],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
+        "react/prop-types": ["error", { ignore: ["form", "match", "history"] }],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
         "semi-spacing": ["error"],
         "no-var": ["error"],
         "prefer-const": ["error"],
-        "eqeqeq": ["error"],
-        "max-len": ["error", {"code": 120}],
+        eqeqeq: ["error"],
+        "max-len": ["error", { code: 120 }],
         "no-trailing-spaces": ["error"],
         "space-before-blocks": ["error", "always"],
         "eol-last": ["error", "always"],
         "no-restricted-globals": ["error", "event"],
         "brace-style": ["error", "1tbs"],
-        "camelcase": ["error", {"properties": "never"}],
+        camelcase: ["error", { properties: "never" }],
         "space-infix-ops": ["error"],
-        "keyword-spacing": ["error", {"before": true, "after": true}],
-        "comma-dangle": ["error", {
-            "arrays": "always-multiline",
-            "objects": "always-multiline",
-            "imports": "always-multiline",
-            "exports": "always-multiline",
-            "functions": "always-multiline",
-        }],
-
-        // @typescript-eslint tweaks
+        "keyword-spacing": ["error", { before: true, after: true }],
+        "comma-dangle": [
+            "error",
+            {
+                arrays: "always-multiline",
+                objects: "always-multiline",
+                imports: "always-multiline",
+                exports: "always-multiline",
+                functions: "always-multiline",
+            },
+        ],
         "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+        "prettier/prettier": "error", // Add this line
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    }
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
 };
