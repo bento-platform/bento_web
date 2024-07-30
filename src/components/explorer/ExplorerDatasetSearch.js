7e1faa01d7c8aab8347ac7a242775c15bcfd6ab4
Julian <ornatus30@gmail.com>
2024-07-30 18:28:07 -0400

    delete comment

--- src/components/explorer/ExplorerDatasetSearch.js
+++ src/components/explorer/ExplorerDatasetSearch.js
@@ -46,7 +46,6 @@ const ExplorerDatasetSearch = () => {
   const fetchingTextSearch = useSelector((state) => state.explorer.fetchingTextSearch || false);
   const searchResults = useSelector((state) => state.explorer.searchResultsByDatasetID[datasetID] || null);
 
-  /// traer aqui los resultados de advanced search
 
   useEffect(() => {
     console.debug("search results: ", searchResults);
