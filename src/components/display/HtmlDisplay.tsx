165b3c9e0a53d7c9f152079bd89003fceb00e7d5
David Lougheed <david.lougheed@gmail.com>
2024-10-28 15:50:42 -0400

    chore: comment height for HTML iframe

--- src/components/display/HtmlDisplay.tsx
+++ src/components/display/HtmlDisplay.tsx
@@ -59,7 +59,12 @@ const HtmlDisplay = ({ contents, loading }: BlobDisplayProps) => {
         sandbox="allow-downloads allow-scripts allow-top-navigation"
         style={{
           width: "90vw",
-          height: "calc(100vh - 40px - 32px - 2px - 100px)",
+          // Height of the iframe is 100% of the view height
+          // minus its border (2px)
+          // minus the padding of the parent modal (40px)
+          // minus the header of the modal (32px)
+          // minus a top and bottom outside margin set by the modal (50px top, matched bottom)
+          height: "calc(100vh - 2px - 40px - 32px - 100px)",
           border: "1px solid #C6C6C6",
           borderRadius: 3,
         }}
