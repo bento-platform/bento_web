bf2e5bb490964cd70d960e624bf0fcf05f444d7d
David Lougheed <david.lougheed@gmail.com>
2024-11-06 16:37:30 -0500

    refact: rewrite utils/requests as typescript

---
+++ src/utils/requests.ts
@@ -0,0 +1,22 @@
+import { JSONType } from "@/types/json";
+
+const SERIALIZED_TYPES = ["object", "array"];
+
+export const createFormData = (obj: Record<string | number, JSONType>) => {
+  const formData = new FormData();
+  Object.entries(obj).forEach(([k, v]) => {
+    if (v === undefined || v === null) return;
+    formData.append(k, SERIALIZED_TYPES.includes(typeof v) ? JSON.stringify(v) : v.toString());
+  });
+  return formData;
+};
+
+export const jsonRequest = (
+  body: JSONType,
+  method = "GET",
+  extraHeaders: Record<string, string> | undefined = undefined,
+): RequestInit => ({
+  method,
+  headers: { "Content-Type": "application/json", ...(extraHeaders ?? {}) },
+  body: JSON.stringify(body),
+});
