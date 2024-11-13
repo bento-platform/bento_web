ab30e04d02b0510e9f9e8d4cb4bd3041093627e5
David Lougheed <david.lougheed@gmail.com>
2024-11-13 12:23:12 -0500

    chore: comment createFormData

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -5,6 +5,8 @@ const SERIALIZED_TYPES = ["object", "array"];
 export const createFormData = (obj: Record<string | number, JSONType>) => {
   const formData = new FormData();
   Object.entries(obj).forEach(([k, v]) => {
+    // If values are "null-like", leave them out. It will be up to the receiver to (correctly) determine that a missing
+    // key means undefined/null.
     if (v === undefined || v === null) return;
     formData.append(k, SERIALIZED_TYPES.includes(typeof v) ? JSON.stringify(v) : v.toString());
   });
