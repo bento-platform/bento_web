0d391a6bcbbe0e17b132b1d11dc61d80f6b6dff2
Delia Bretan <delia_bretan@yahoo.com>
2025-05-08 13:49:19 -0400

    ignore validator error when creating discovery schema (temp workaround)

--- src/hooks.ts
+++ src/hooks.ts
@@ -10,6 +10,7 @@ import {
 } from "bento-auth-js";
 
 import { useService } from "@/modules/services/hooks";
+import { Exception } from "handlebars";
 
 // AUTHORIZATION:
 // Wrapper hooks for bento-auth-js permissions hooks, which expect a 'authzUrl' argument.
@@ -95,7 +96,14 @@ export const useJsonSchemaValidator = (schema: SchemaObject, schemaName: string,
   }, [schema, schemaName]);
   return useCallback(
     (rule: unknown, value: unknown) => {
-      const validator = ajv?.getSchema(schemaName);
+      var validator
+      try {
+        // TODO: why is this throwing?
+        validator = ajv?.getSchema(schemaName);
+      } catch (e) {
+        console.debug(e);
+        return Promise.resolve();
+      }
       if (!ajv || !validator) {
         return Promise.reject(new Error(`No JSON schema provided for ${schemaName}, cannot validate.`));
       }
