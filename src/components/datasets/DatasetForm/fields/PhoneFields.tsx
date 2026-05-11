c16dd52874e9d4b99c8e0e4dad459348a7a48943
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2026-05-11 16:13:58 -0400

    fix(dataset-form): remove spurious required rules from PhoneFields
    Phone is optional; antd required rules fired even when untouched.
    Zod still enforces both fields if a phone number is provided.

--- src/components/datasets/DatasetForm/fields/PhoneFields.tsx
+++ src/components/datasets/DatasetForm/fields/PhoneFields.tsx
@@ -2,10 +2,10 @@ import { Card, Form, InputNumber } from "antd";
 
 const PhoneFields = ({ namePrefix }: { namePrefix: (string | number)[] }) => (
   <Card size="small" title="Phone" style={{ marginBottom: 8 }}>
-    <Form.Item label="Country code" name={[...namePrefix, "country_code"]} rules={[{ required: true }]}>
+    <Form.Item label="Country code" name={[...namePrefix, "country_code"]}>
       <InputNumber style={{ width: "100%" }} placeholder="e.g. 1" />
     </Form.Item>
-    <Form.Item label="Number" name={[...namePrefix, "number"]} rules={[{ required: true }]}>
+    <Form.Item label="Number" name={[...namePrefix, "number"]}>
       <InputNumber style={{ width: "100%" }} placeholder="e.g. 5551234567" />
     </Form.Item>
     <Form.Item label="Extension" name={[...namePrefix, "extension"]}>
